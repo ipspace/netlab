2d1919813de912ebdfc230d5453ab15cfbd24d76
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-12-28 17:22:39 -0600

    Support SROS platforms that don't use connectors

--- tests/utils.py
+++ tests/utils.py
@@ -4,7 +4,7 @@
 
 import yaml
 
-def transformation_results_yaml(topology,ignore=('addressing','defaults','nodes_map','includes')):
+def transformation_results_yaml(topology,ignore=('addressing','defaults','includes')):
   """
   Return the results of topology transformation YAML format
 
@@ -18,7 +18,7 @@ def transformation_results_yaml(topology,ignore=('addressing','defaults','nodes_
   Temporary: replace node dict with node list
   """
   topology.nodes = [ node_data + { 'name': name} for name,node_data in topology.nodes.items() ]
-  
+
   """
   If we're using a dictionary extension that has to_yaml method use that,
   otherwise use pyyaml (hoping it won't generate extraneous attributes)
