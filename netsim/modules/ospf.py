a186682564243a78cdcb66e59bf37c3ff34dbdc9
Ivan Pepelnjak <ip@ipspace.net>
2022-01-04 16:07:48 +0100

    Report an error when trying to run OSPF over unnumbered multi-access interfaces (#111)

--- netsim/modules/ospf.py
+++ netsim/modules/ospf.py
@@ -7,6 +7,7 @@ from box import Box
 
 from . import _Module
 from . import bfd
+from .. import common
 
 class OSPF(_Module):
 
@@ -18,11 +19,17 @@ class OSPF(_Module):
     # We need to set ospf.unnumbered if we happen to have OSPF running over an unnumbered
     # link -- Arista EOS needs an extra nerd knob to make it work
     #
+    # An interface can be unnumbered if it has the 'unnumbered' flag set or if it
+    # has IPv4 enabled but no IPv4 address (ipv4: true)
+    #
     for l in node.links:                                         # Scan all links 
-      if 'unnumbered' in l:                                      # ... old style unnumbered link
+      is_unnumbered = \
+        'unnumbered' in l or \
+        'ipv4' in l and isinstance(l.ipv4,bool) and l.ipv4
+      if is_unnumbered:
         node.ospf.unnumbered = True
-        break                                                    # No need to go any further
-      elif 'ipv4' in l and isinstance(l.ipv4,bool) and l.ipv4:   # New-style unnumbered link: ipv4 set to True
-        node.ospf.unnumbered = True
-        break
-      
+        if len(l.get('neighbors',[])) != 1:
+          common.error(
+            f'OSPF does not work over multi-access unnumbered IPv4 interfaces: node {node.name} link {l.name}',
+            common.IncorrectValue,
+            'ospf')
