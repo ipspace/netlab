32c32ddbe06de3d115a500737a9b6019d7fd3893
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-01-05 11:53:04 -0600

    Merge with other IS-IS changes

--- netsim/modules/isis.py
+++ netsim/modules/isis.py
@@ -5,6 +5,7 @@ from box import Box
 
 from . import _Module
 from . import bfd
+from .. import common
 
 class ISIS(_Module):
 
@@ -13,9 +14,32 @@ class ISIS(_Module):
 
     bfd.multiprotocol_bfd_link_state(node,'isis')
 
-    # Determine the IS-IS network type for each interface, based on number of neighbors
-    # and whether the interface is passive
+    for af in ('ipv4','ipv6'):
+      is_unnumbered = False
+      for l in node.get('interfaces',[]):
+        is_unnumbered = is_unnumbered or \
+          'unnumbered' in l or \
+          (af in l and isinstance(l[af],bool) and l[af])
+
+      if is_unnumbered and not topology.defaults.devices[node.device].features.isis.unnumbered[af]:
+        common.error(
+          f'Device {node.device} used on node {node.name} cannot run IS-IS over {"unnumbered" if af == "ipv4" else "LLA"} {af} interfaces',
+          common.IncorrectValue,
+          'interfaces')
+
     for l in node.get('interfaces',[]):
-        if 'isis' in l:
-            l.isis.network_type_p2p = len(l.get('neighbors',[])) == 1
-            l.isis.passive = l.type == "stub" or l.get('role',"") in ["stub","passive"]
+      unnum_v4 = 'unnumbered' in l or ('ipv4' in l and isinstance(l.ipv4,bool) and l.ipv4)
+      if unnum_v4 and \
+          len(l.neighbors) > 1 and \
+          topology.defaults.devices[node.device].features.isis.unnumbered.ipv4 and \
+          not topology.defaults.devices[node.device].features.isis.unnumbered.network:
+        common.error(
+          f'Device {node.device} used on node {node.name} cannot run IS-IS over\n'+
+          f'.. unnumbered multi-access interfaces (link {l.name})',
+          common.IncorrectValue,
+          'interfaces')
+
+      # Determine the IS-IS network type for each interface, based on number of neighbors
+      # and whether the interface is passive
+      l.isis.network_type_p2p = len(l.get('neighbors',[])) == 1
+      l.isis.passive = l.type == "stub" or l.get('role',"") in ["stub","passive"]
