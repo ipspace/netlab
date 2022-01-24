774a4c9cb2a83bfc53f9259fba56df6eda365a32
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-01-24 12:06:40 -0600

    Cleanup code, update doc sample

--- netsim/extra/ebgp-local_as.py
+++ netsim/extra/ebgp-local_as.py
@@ -11,22 +11,20 @@
 # based on that attribute.
 #
 # Changes:
-# * Creates bgp.neighbors of type 'ebgp' with a 'local_as' attribute
+# * Creates additional bgp.neighbors of type 'ebgp' with a 'local_as' attribute
 #
 import typing, netaddr
 from box import Box
 
 """
-Similar to Module pre-default:
-* process AS list
-* build BGP groups with multi-AS support
+Adds a custom bgp.underlay_as node attribute
 """
 def init(topology: Box) -> None:
     # Allow users to specify a different AS to use for underlay peering (eBGP)
     topology.defaults.bgp.attributes.node.append('underlay_as')
 
-def ebgp_neighbor(n: Box, asn: int, intf: Box, extra_data: typing.Optional[dict] = None) -> Box:
-  ngb = Box(extra_data or {},default_box=True,box_dots=True)
+def ebgp_neighbor(n: Box, asn: int, intf: Box, extra_data: dict) -> Box:
+  ngb = Box(extra_data,default_box=True,box_dots=True)
   ngb.name = n.name
   ngb.type = 'ebgp'
   ngb["as"] = asn  # 'as' for backwards compatibility, would prefer 'peer_as'
@@ -40,7 +38,7 @@ def ebgp_neighbor(n: Box, asn: int, intf: Box, extra_data: typing.Optional[dict]
 
 
 """
-build_bgp_sessions: augment BGP neighbors with ebgp peers
+build_ebgp_sessions: augment BGP neighbors with ebgp peers
 * EBGP sessions are established whenever two nodes on the same link have
   different underlay AS
 """
@@ -55,14 +53,11 @@ def build_ebgp_sessions(node: Box, topology: Box) -> None:
       for ngb_ifdata in l.get("neighbors",[]):
         ngb_name = ngb_ifdata.node
         neighbor = topology.nodes[ngb_name]
-        if not "bgp" in neighbor or not 'underlay_as' in neighbor.bgp:
-          # print( f"ibgp-over-bgp: BGP not enabled for neighbor {ngb_name}" )
+        if not "bgp" in neighbor or not "underlay_as" in neighbor.bgp:
           continue
 
         peer_as = neighbor.bgp.underlay_as
 
-        # Iterate over both sets of AS; support at most 1 eBGP peering
-        # print( f"ibgp-over-ebgp: Checking eBGP peering between {node.name} and {ngb_name}: {list(neighbor.bgp.ibgp_over_ebgp['as'])}" )
         if node_as!=peer_as:
           extra_data = Box({})
           extra_data.ifindex = l.ifindex
@@ -71,7 +66,6 @@ def build_ebgp_sessions(node: Box, topology: Box) -> None:
           if "unnumbered" in l:
             extra_data.unnumbered = True
             extra_data.local_if = l.ifname
-          # print( f"ibgp-over-ebgp: Found eBGP peer {asn}-{asn2} to {ngb_name}" )
           node.bgp.neighbors.append( ebgp_neighbor(neighbor,peer_as,ngb_ifdata,extra_data) )
 
 def post_transform(topology: Box) -> None:
