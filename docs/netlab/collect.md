d24ae673c95cf1054517911f863f87614b69429f
Ivan Pepelnjak <ip@ipspace.net>
2022-03-07 18:09:23 +0100

    Add '--tar' and '--cleanup' options to "netlab collect"

--- docs/netlab/collect.md
+++ docs/netlab/collect.md
@@ -4,6 +4,8 @@
 
 A single configuration file in the output directory is created for most network devices; multiple files stored in host-specific subdirectory are collected from Cumulus VX.
 
+After collecting the device configurations, you can save them in a tar archive with the `--tar` option and clean up the working directory with `--cleanup` option.
+
 ```{tip}
 **netlab collect** command does not need a topology file (so you don't have to specify one even if you're using a non-default topology name). It's just a thin wrapper around an Ansible playbook which uses Ansible inventory created by **netlab create** or **netlab up** command.
 ```
@@ -11,15 +13,19 @@ A single configuration file in the output directory is created for most network 
 ## Usage
 
 ```text
-usage: netlab collect [-h] [-v] [-o [OUTPUT]]
+usage: netlab collect [-h] [-v] [-q] [-o [OUTPUT]] [--tar TAR] [--cleanup]
 
 Collect device configurations
 
 optional arguments:
   -h, --help            show this help message and exit
   -v, --verbose         Verbose logging
+  -q, --quiet           Run Ansible playbook and tar with minimum output
   -o [OUTPUT], --output [OUTPUT]
                         Output directory (default: config)
+  --tar TAR             Create configuration tarball
+  --cleanup             Clean up config directory and modified configuration file after
+                        creating tarball
 
 All other arguments are passed directly to ansible-playbook
 ```
--- netsim/cli/collect.py
+++ netsim/cli/collect.py
@@ -6,10 +6,13 @@
 import typing
 import os
 import argparse
+import shutil
+import subprocess
 
-from . import common_parse_args
+from . import common_parse_args,fs_cleanup
 from . import ansible
-from ..common import set_logging_flags
+from . import external_commands
+from .. import common
 
 #
 # CLI parser for 'netlab collect' command
@@ -26,23 +29,71 @@ def initial_config_parse(args: typing.List[str]) -> typing.Tuple[argparse.Namesp
     action='store_true',
     help='Verbose logging')
   parser.add_argument(
+    '-q','--quiet',
+    dest='quiet',
+    action='store_true',
+    help='Run Ansible playbook and tar with minimum output')
+  parser.add_argument(
     '-o','--output',
     dest='output',
     action='store',
     nargs='?',
     default='config',
     help='Output directory (default: config)')
+  parser.add_argument(
+    '--tar',
+    dest='tar',
+    action='store',
+    help='Create configuration tarball')
+  parser.add_argument(
+    '--cleanup',
+    dest='cleanup',
+    action='store_true',
+    help='Clean up config directory and modified configuration file after creating tarball')
   return parser.parse_known_args(args)
+
+def get_tarball_file(tarball: str) -> str:
+  if not '.tar' in tarball:
+    tarball = tarball + '.tar.gz'
+  return tarball
 
 def run(cli_args: typing.List[str]) -> None:
   (args,rest) = initial_config_parse(cli_args)
+  common.set_logging_flags(args)
+
+  fs_cleanup([ args.output ])
+  try:
+    os.mkdir(args.output)
+  except Exception as ex:
+    common.fatal(f"Cannot create output directory {{args.output}}: {{ex}}")
 
   if args.verbose:
     rest = ['-v'] + rest
 
   rest = ['-e','target='+args.output ] + rest
 
+  if args.tar and not args.quiet:
+    external_commands.print_step(1,"Collecting device configurations")
+
   if args.verbose:
     print("Ansible playbook args: %s" % rest)
 
+  if args.quiet:
+    os.environ["ANSIBLE_STDOUT_CALLBACK"] = "dense"
+
   ansible.playbook('collect-configs.ansible',rest)
+
+  if args.tar:
+    tarball = get_tarball_file(args.tar)
+    if not args.quiet:
+      external_commands.print_step(2,"Creating tarball",spacing = True)
+    try:
+      subprocess.check_call(['tar','cfz' if args.quiet else 'cvfz',tarball,args.output])
+    except Exception as ex:
+      common.fatal(f"Cannot start tar: {ex}")
+
+    if args.cleanup:
+      if not args.quiet:
+        external_commands.print_step(3,"Cleanup config directory",spacing = True)
+      fs_cleanup([ args.output ],args.verbose)
+      print("... done")
