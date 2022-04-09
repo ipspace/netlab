95f7f3ad8b32530faf03df6ed9e2937d3d7b156e
Stefano Sasso <852093+ssasso@users.noreply.github.com>
2022-04-09 18:49:01 +0200

    Support output in Graphite JSON format (#241)

--- docs/outputs/index.md
+++ docs/outputs/index.md
@@ -1,10 +1,10 @@
 # Output Formats
 
-**netlab create** command can call one or more output modules to create files that can be used with Vagrant, Ansible, containerlab, or graphviz. The output modules are specified with one or more `-o` parameters. When no `-o` parameter is specific, **netlab create** calls *provider* and *ansible* output modules.
+**netlab create** command can call one or more output modules to create files that can be used with Vagrant, Ansible, containerlab, graphviz or *graphite* topology. The output modules are specified with one or more `-o` parameters. When no `-o` parameter is specific, **netlab create** calls *provider* and *ansible* output modules.
 
 Each `-o` parameter specifies an output module, formatting modifiers, and output filename in the **format:modifiers=file(s)** format:
 
-* **format** is the desired output module. It can be one of *provider*, *ansible*, *graph*, *yaml* or *json*.
+* **format** is the desired output module. It can be one of *provider*, *ansible*, *graph*, *yaml*, *json* or *graphite*.
 * Some output modules use optional formatting modifiers -- you can specify Ansible inventory format, graph type, or parts of the transformed data structure that you want to see in YAML or JSON format
 * All output formats support optional destination file name. Default file name is either hard-coded in the module or specified in **defaults.outputs** part of lab topology.
 
@@ -19,4 +19,5 @@ The following output modules are included in **netsim-tools** distribution; you 
    graph.md
    yaml-or-json.md
    devices.md
+   graphite.md
 ```
---
+++ netsim/outputs/graphite.py
@@ -0,0 +1,141 @@
+# Why use Graphite (https://github.com/netreplica/graphite) only with ContainerLab? :-)
+#
+# With this output module it would be possible to export a topology file to be used within Graphite.
+# 
+# Graphite container can be launched with:
+#
+# docker run -d \
+#  -v "$(pwd)/graphite-default.json":/var/www/localhost/htdocs/default/default.json \
+#  -p 8080:80 \
+#  --name graphite \
+#  netreplica/graphite:webssh2
+#
+
+from array import array
+import typing
+
+import json
+import os
+from box import Box
+
+from .. import common
+from .. import data
+from . import _TopologyOutput
+
+DEFAULT_NODE_ICON = "router"
+HOST_BRIDGE_NODE_NAME = "({type}:{index})"
+
+def nodes_items(topology: Box) -> list:
+    r = []
+    for name,n in topology.nodes.items():
+        node_icon = ( data.get_from_box(n,'graphite.icon') or 
+            data.get_from_box(topology,f'defaults.devices.{n.device}.graphite.icon') or 
+            DEFAULT_NODE_ICON )
+        graph_level = data.get_from_box(n,'graphite.level') or 1
+        node_group = "tier-1"
+        node_as = n.get('bgp', {}).get('as')
+        if node_as:
+            node_group = "as{}".format(node_as)
+        r.append(
+            {
+                'name': name,
+                'kind': n.device,
+                "ipv4_address": n.mgmt.ipv4,
+                "group": node_group,
+                "labels": {
+                    "graph-level": graph_level,
+                    "graph-icon": node_icon,
+                },
+            }
+        )
+    # Special Case:
+    # Create a fake node to identify a host bridge, in case there are 1 or more than 2 nodes attached to it.
+    for l in topology.links:
+        if (l.type == "lan" and l.node_count != 2) or l.type == "stub":
+            # Create fake node
+            # Inherit graph level and group from first node (l.interfaces[0].node)
+            node_item = topology.nodes[l.interfaces[0].node]
+            graph_level = data.get_from_box(node_item,'graphite.level') or 1
+            node_group = "tier-1"
+            node_as = node_item.get('bgp', {}).get('as')
+            if node_as:
+                node_group = "as{}".format(node_as)
+            r.append(
+                {
+                    'name': HOST_BRIDGE_NODE_NAME.format(type=l.type, index=l.linkindex),
+                    'kind': "(host bridge: {})".format(l.bridge),
+                    "ipv4_address": '',
+                    "group": node_group,
+                    "labels": {
+                        "graph-level": graph_level,
+                        "graph-icon": 'expand',
+                    },
+                }
+            )
+    return r
+
+def get_lan_intf_name(topology: Box, node_name: str, bridge_name: str) -> str:
+    for intf in topology.nodes[node_name].interfaces:
+        if intf.get('bridge','') == bridge_name:
+            return intf.ifname
+    return "<unknown>"
+
+def links_items(topology: Box) -> list:
+    r = []
+    for l in topology.links:
+        # P2P Links
+        if l.type == "p2p":
+            r.append(
+                {
+                    "source": l.left.node,
+                    "source_endpoint": l.left.ifname,
+                    "target": l.right.node,
+                    "target_endpoint": l.right.ifname
+                }
+            )
+        elif l.type == "lan" or l.type == "stub":
+            # If LAN link have only two interfaces in it, treat it like a P2P
+            # - Else, in nodes_items create a fake item that represents the host bridge, and attach the devices to it.
+            if l.type == "lan" and l.node_count == 2:
+                r.append(
+                    {
+                        "source": l.interfaces[0].node,
+                        "source_endpoint": get_lan_intf_name(topology, l.interfaces[0].node, l.bridge),
+                        "target": l.interfaces[1].node,
+                        "target_endpoint": get_lan_intf_name(topology, l.interfaces[1].node, l.bridge),
+                    }
+                )
+            else:
+                bridge_node_name = HOST_BRIDGE_NODE_NAME.format(type=l.type, index=l.linkindex)
+                for bridge_intf in l.interfaces:
+                    # Create an attachment
+                    r.append(
+                        {
+                            "source": bridge_node_name,
+                            "source_endpoint": "",
+                            "target": bridge_intf.node,
+                            "target_endpoint": get_lan_intf_name(topology, bridge_intf.node, l.bridge),
+                        }
+                    )
+    return r
+
+class Graphite(_TopologyOutput):
+
+  def write(self, topology: Box) -> None:
+    outfile = self.settings.filename or 'graphite-default.json'
+
+    graphite_json = {
+        'nodes': nodes_items(topology),
+        'links': links_items(topology),
+    }
+
+    output = common.open_output_file(outfile)
+
+    output.write(json.dumps(graphite_json, indent=2,sort_keys=True))
+    output.write("\n")
+
+    if outfile != '-':
+      common.close_output_file(output)
+      print("Created Graphite topology file in %s" % outfile)
+    else:
+      output.write("\n")
