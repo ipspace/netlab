68ef67e0703ea4ead74a2eb42fa4589bfe6171c5
Ivan Pepelnjak <ip@ipspace.net>
2022-04-25 08:23:58 +0200

    Bug fix: links using libvirt UDP tunnels must not have a bridge attribute

--- netsim/cli/down.py
+++ netsim/cli/down.py
@@ -70,6 +70,7 @@ def run(cli_args: typing.List[str]) -> None:
   external_commands.run_probes(settings,topology.provider,1)
 
   provider = providers._Provider.load(topology.provider,topology.defaults.providers[topology.provider])
+  provider.call('pre_output_transform',topology)
 
   if hasattr(provider,'pre_stop_lab') and callable(provider.pre_stop_lab):
     provider.pre_stop_lab(topology)
