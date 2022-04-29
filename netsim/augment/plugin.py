e36d72216b1cc61594e64304438df918581eb30a
Ivan Pepelnjak <ip@ipspace.net>
2022-04-29 10:45:30 +0200

    Bug fix: load more than one plugin from a subdirectory

--- netsim/augment/plugin.py
+++ netsim/augment/plugin.py
@@ -5,44 +5,49 @@ plugin - implement custom topology transformation plugins
 import os
 import sys
 import typing
+import importlib
+import importlib.util
 
 from box import Box
-from importlib import import_module
 from .. import common
 
 def load_plugin_from_path(path: str, plugin: str) -> typing.Optional[object]:
   module_path = path+'/'+plugin
-  module_name = module_path
+  if not os.path.exists(module_path):
+    if os.path.exists(module_path+'.py'):
+      module_path = module_path+'.py'
+    else:
+      return None
+
   module: typing.Optional[object] = None
-  is_package = os.path.isdir(module_path)
   config_name = None
 
-  if is_package:
-    sys.path = [ module_path ] + sys.path
-    module_name = module_path + '/plugin.py'
+  if os.path.isdir(module_path):
+    module_path = module_path + '/plugin.py'
     config_name = plugin
-    plugin = 'plugin'
-  else:
-    sys.path = [ path ] + sys.path
-    if os.path.exists(module_name+'.py'):
-      module_name = module_name + '.py'
+    if not os.path.exists(module_path):
+      return None
 
-  if os.path.exists(module_name):
-    try:
-      module = import_module(plugin)
-    except:
-      print("Cannot load plugin %s\n%s" % (module_name,str(sys.exc_info()[1])))
-      common.fatal('Aborting the transformation process','plugin')
+  module_name = plugin.replace('.py','')
+  module_name = f'netlab.plugin.{module_name}'
 
-    if is_package:
-      setattr(module,'config_name',config_name)
-  else:
-    module = None
+  try:
+    modspec  = importlib.util.spec_from_file_location(module_name,module_path)
+    assert(isinstance(modspec,importlib.machinery.ModuleSpec))
+    pymodule = importlib.util.module_from_spec(modspec)
+    sys.modules[module_name] = pymodule
+    assert(isinstance(modspec.loader,importlib.abc.Loader))
+    modspec.loader.exec_module(pymodule)
+  except:
+    print(f"Cannot load plugin {module_name} from {module_path}\n{str(sys.exc_info()[1])}")
+    common.fatal('Aborting the transformation process','plugin')
 
-  sys.path = sys.path[1:]
+  if config_name:
+    setattr(pymodule,'config_name',config_name)
+
   if common.VERBOSE:
-    print("loaded plugin %s" % module)
-  return module
+    print(f"loaded plugin {module_name}")
+  return pymodule
 
 def init(topology: Box) -> None:
   if not 'plugin' in topology:
@@ -51,7 +56,7 @@ def init(topology: Box) -> None:
   topology.Plugin = []
   for pname in topology.plugin:
     plugin = None
-    search_path = ('.',common.netsim_package_path+'/extra',pname)
+    search_path = ('.',common.netsim_package_path+'/extra')
     for path in search_path:
       if not plugin:
         plugin = load_plugin_from_path(path,pname)
@@ -59,6 +64,9 @@ def init(topology: Box) -> None:
       topology.Plugin.append(plugin)
     else:
       common.error(f"Cannot find plugin {pname} in {search_path}",common.IncorrectValue,'plugin')
+
+  if common.DEBUG:
+    print(f'plug INIT: {topology.Plugin}')
 
 def execute(action: str, topology: Box) -> None:
   if not 'Plugin' in topology:
