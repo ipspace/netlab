85d1c68aa370735ea6d4fdba97dc35d69379597c
Ivan Pepelnjak <ip@ipspace.net>
2022-08-18 17:33:34 +0200

    EVPN VLAN-Aware Bundle Service functionality + Arista EOS implementation

--- docs/module/evpn.md
+++ docs/module/evpn.md
@@ -2,7 +2,11 @@
 
 This configuration module configures the BGP EVPN address family on Nokia SR OS, Nokia SR Linux, Arista EOS and FRR. The module depends on the BGP module.
 
-The current implementation of EVPN module supports VLAN-based Service Interface (bridging of a single VLAN within an EVPN Instance) with VXLAN-based transport. 
+The current implementation of EVPN module supports:
+
+* VXLAN-based transport
+* VLAN-Based Service (bridging of a single VLAN within an EVPN Instance)
+* VLAN-Aware Bundle Service (bridging of multiple related VLANs inside a single EVPN Instance)
 
 The EVI (EVPN Instance) value can be set with **evpn.evi** VLAN attribute, RD and RT values (when missing) are computed from EVI or VLAN ID using the rules from Section 7.9 and 7.10 of RFC 7432.
 
@@ -19,8 +23,8 @@ The following table describes per-platform support of individual VXLAN features:
 
 | Operating system   | VXLAN<br>transport | VLAN-based<br>service | VLAN Bundle<br>service | Asymmetric<br>IRB | Symmetric<br>IRB |
 | ------------------ | :-: | :-: | :-: | :-: |
-| Arista EOS         | ✅  | ✅  |  ❌  |  ❌  |  ❌  |
-| Nokia SR Linux     | ✅  | ✅  | ✅  |  ❌  |  ❌  |
+| Arista EOS         | ✅  | ✅  | ✅  |  ❌  |  ❌  |
+| Nokia SR Linux     | ✅  | ✅  |  ❌  |  ❌  |  ❌  |
 | Nokia SR OS        |  ❌  |  ❌  |  ❌  |  ❌  |  ❌  |
 | FRR                |  ❌  |  ❌  |  ❌  |  ❌  |  ❌  |
 
@@ -47,10 +51,12 @@ EVPN module supports these global/node parameters:
 * **evpn.session**: A list of BGP session types on which the EVPN address family is enabled (default: `ibgp`)
 * **evpn.use_ibgp**: Nokia-only parameter that selects IBGP-over-EBGP (when set to `true`, default behavior) or EBGP-only (when set to `false`) design. Setting this parameter on Nokia devices also disables IBGP IPv4/IPv6 address family.
 
-Other EVPN-related parameters are set on **vlans** dictionary. You can set the following parameters for every VLAN:
+Other EVPN-related parameters are set on **vlans** dictionary. You can set the following parameters for every VLAN using VLAN-Based Service:
 
 * **evpn.evi**: EVPN Instance identifier. Default: `vlan-id`.
 * **evpn.rd**: EVPN Instance route distinguisher (not checked at the moment). Default: `router-id:vlan-id`
 * **evpn.import** and **evpn.export**: Import and export route targets (not checked at the moment). Default: `bgp-as:vlan-id`
 
 The default EVI/RD/RT values are set for [VXLAN-enabled VLANs](vxlan.md#selecting-vxlan-enabled-vlans).
+
+VLAN-Aware Bundle Service uses VRF configuration (and thus requires [VRF configuration module](vrf.md)). All VLANs belonging to a single VRF are configured as a VLAN bundle. [RD and RT values assigned by VRF module](vrf.md#rd-and-rt-values) are used to configure the VLAN bundle; you can set **evpn.evi** VRF parameter to set the EVPN Instance identifier. The default value of VRF EVPN Instance identifier is the VLAN ID of the first VLAN in that VRF.
--- netsim/modules/evpn.py
+++ netsim/modules/evpn.py
@@ -5,6 +5,76 @@ from box import Box
 from .. import common
 from .. import data
 
+def enable_evpn_af(node: Box, topology: Box) -> None:
+  # evpn.use_ibgp was the old way of saying 'run EVPN over IBGP'
+  #
+  # This parameter has been replaced with evpn.session parameter and will eventually
+  # disappear. In the meantime, 'evpn.use_ibgp' results in 'evpn.session' being
+  # set to 'ibgp'
+  if 'evpn' in node and 'use_ibgp' in node.evpn:
+    data.must_be_bool(
+      node,'evpn.use_ibgp',f'nodes.{node.name}.evpn.use_ibgp',
+      module='evpn')
+    node.evpn.session = ['ibgp'] if node.evpn.use_ibgp else ['ebgp']
+
+  bgp_session = data.get_from_box(node,'evpn.session') or []
+
+  # Enable EVPN AF on all BGP neighbors with the correct session type
+  # that also use EVPN module
+  #
+  for bn in node.bgp.get('neighbors',[]):
+    if bn.type in bgp_session and 'evpn' in topology.nodes[bn.name].get('module'):
+      bn.evpn = True
+
+def vlan_based_service(vlan: Box, vname: str, node: Box, topology: Box) -> None:
+  evpn  = vlan.evpn
+  epath = f'nodes.{node.name}.vlans.{vname}.evpn'
+  evpn.evi = evpn.evi or vlan.id                                    # Default EVI value: VLAN ID
+  data.must_be_int(
+    evpn,'evi',epath,
+    module='evpn',
+    min_value=1,max_value=65535)                                    # Check EVI data type in range
+  if not 'rd' in evpn:                                              # Default RD value
+    evpn.rd = f'{node.bgp.router_id}:{vlan.id}'
+  for rt in ('import','export'):                                    # Default RT value
+    if not rt in evpn:                                              # ... BGP ASN:vlan ID
+      evpn[rt] = [ f"{node.bgp['as']}:{vlan.id}" ]
+
+def vlan_aware_bundle_service(vlan: Box, vname: str, node: Box, topology: Box) -> None:
+  vrf_name = vlan.vrf
+  if not vrf_name in topology.vrfs:
+    common.error(
+      f'VXLAN-enabled VLAN {vname} on node {node.name} that is part of VLAN bundle must belong to a global VRF',
+      common.IncorrectValue,
+      'evpn')
+    return
+
+  if not vname in topology.vlans:
+    common.error(
+      f'VLAN {vname} on node {node.name} that is part of VLAN bundle must be a global VLAN',
+      common.IncorrectValue,
+      'evpn')
+    return
+
+  if 'evpn' in node.vlans[vname]:                                   # VLAN that is part of a bundle cannot have EVI/RT/RD attributes
+    common.error(
+      f'VLAN {vname} on node {node.name} is part of a VLAN bundle {vrf_name} and cannot have EVPN-related attributes',
+      common.IncorrectValue,
+      'evpn')
+    return
+
+  g_evpn = topology.vrfs[vrf_name].evpn
+  if not 'evi' in g_evpn:                                           # If needed, set EVI attribute for the global VRF
+    g_evpn.evi = topology.vlans[vname].id                           # ... to VLAN ID
+
+  evpn = node.vrfs[vrf_name].evpn                                   # Now set VRF EVPN parameters for node VRF
+  evpn.evi = g_evpn.evi                                             # Copy EVI from global VRF
+  for k in ('vlans','vlan_ids'):
+    if not k in evpn:                                               # Is this the first EVPN-enabled VLAN in this VRF?
+      evpn[k] = []                                                  # ... create an empty list of VLANs
+  evpn.vlans.append(vname)                                          # Finally, add VLAN name to the list of MAC VRF VLANs
+  evpn.vlan_ids.append(topology.vlans[vname].id)                    # ... and a VLAN ID to list of EVPN-enabled VLAN tags
+
 class EVPN(_Module):
 
   def module_init(self, topology: Box) -> None:
@@ -22,46 +92,17 @@ class EVPN(_Module):
   Add 'evi' (EVPN Instance),'rd' and 'rt' attributes to VLANs that have a 'vni' attribute
   """
   def node_post_transform(self, node: Box, topology: Box) -> None:
-
-    # evpn.use_ibgp was the old way of saying 'run EVPN over IBGP'
-    #
-    # This parameter has been replaced with evpn.session parameter and will eventually
-    # disappear. In the meantime, 'evpn.use_ibgp' results in 'evpn.session' being
-    # set to 'ibgp'
-    if 'evpn' in node and 'use_ibgp' in node.evpn:
-      data.must_be_bool(
-        node,'evpn.use_ibgp',f'nodes.{node.name}.evpn.use_ibgp',
-        module='evpn')
-      node.evpn.session = ['ibgp'] if node.evpn.use_ibgp else ['ebgp']
-
-    bgp_session = data.get_from_box(node,'evpn.session') or []
+    enable_evpn_af(node,topology)
 
     vlan_list = data.get_from_box(node,'vxlan.vlans') or []       # Get the list of VXLAN-enabled VLANs
     if not vlan_list:
       return                                                      # This could be a route reflector running EVPN
 
-    # Enable EVPN AF on all BGP neighbors with the correct session type
-    # that also use EVPN module
-    #
-    for bn in node.bgp.get('neighbors',[]):
-      if bn.type in bgp_session and 'evpn' in topology.nodes[bn.name].get('module'):
-        bn.evpn = True
+    _routing.router_id(node,'bgp',topology.pools)                 # Make sure we have a usable router ID
 
-    rid = None
     for vname in vlan_list:
-      evpn  = node.vlans[vname].evpn
-      epath = f'nodes.{node.name}.vlans.{vname}.evpn'
-
-      evpn.evi = evpn.evi or node.vlans[vname].id
-      data.must_be_int(
-        evpn,'evi',epath,
-        module='evpn',
-        min_value=1,max_value=65535)
-      if not 'rd' in evpn:
-        if not rid:
-          _routing.router_id(node,'bgp',topology.pools)
-          rid = node.bgp.router_id
-        evpn.rd = f'{rid}:{node.vlans[vname].id}'
-      for rt in ('import','export'):
-        if not rt in evpn:
-          evpn[rt] = [ f"{node.bgp['as']}:{node.vlans[vname].id}" ]
+      vlan = node.vlans[vname]
+      if not 'vrf' in vlan:                                               # VLAN-Based Service
+        vlan_based_service(vlan,vname,node,topology)
+      else:                                                               # VLAN-Aware Bundle Service or IRB
+        vlan_aware_bundle_service(vlan,vname,node,topology)
