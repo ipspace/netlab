6eefddd21395118c3e1190bd2cdc8225d3ecf233
J vanBemmel <jvb127@gmail.com>
2022-08-27 00:50:21 -0500

    Add install script for grpc packages (#360)

---
+++ netsim/install/grpc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -z "$FLAG_YES" ]]; then
+  # Remove implied default of Y - ghostinthenet 20220418
+  read -p "Are you sure you want to proceed [y/n] " -n 1 -r
+  echo
+  if ! [[ $REPLY =~ [Yy] ]]; then
+    echo "Aborting..."
+    exit 1
+  fi
+  FLAG_YES="Y"
+fi
+
+# Install Ansible grpc plug-in and its dependencies from github repo
+ansible-galaxy collection install git+https://github.com/nokia/ansible-networking-collections.git#/grpc/
+
+# grpc sources contain generated pb2 file which is not compatible with newer versions of protobuf
+python3 -m pip install grpcio protobuf==3.20.1 --upgrade
+
+exit $?
