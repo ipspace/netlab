f841371741b8b5540622d269e12da6a50d223b08
Ivan Pepelnjak <ip@ipspace.net>
2022-08-28 07:51:20 +0200

    Bump netsim-tools stub version

--- legacy/setup.py
+++ legacy/setup.py
@@ -6,12 +6,12 @@ sys.path.append('..')
 
 setup(
   name="netsim-tools",
-  version="1.3",
+  version="1.3-post1",
   packages=[],
   author="Ivan Pepelnjak",
   author_email="ip@ipspace.net",
   description="CLI-based Virtual Networking Lab Abstraction Layer",
-  install_requires=["networklab"],
+  install_requires=["networklab>=1.3"],
   classifiers=[
     "Topic :: Utilities",
     "Programming Language :: Python",
