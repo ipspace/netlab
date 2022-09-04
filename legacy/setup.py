c7818758747197e0620096272987fd61105fcb41
Ivan Pepelnjak <ip@ipspace.net>
2022-09-04 17:22:17 +0200

    Hotfix: Release 1.3-post1

--- legacy/setup.py
+++ legacy/setup.py
@@ -6,12 +6,12 @@ sys.path.append('..')
 
 setup(
   name="netsim-tools",
-  version="1.3-post1",
+  version="1.3-post2",
   packages=[],
   author="Ivan Pepelnjak",
   author_email="ip@ipspace.net",
   description="CLI-based Virtual Networking Lab Abstraction Layer",
-  install_requires=["networklab>=1.3"],
+  install_requires=["networklab>=1.3.post1"],
   classifiers=[
     "Topic :: Utilities",
     "Programming Language :: Python",
