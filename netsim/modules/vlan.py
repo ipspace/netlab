52bc330725aa93ee9f7bdd0ea0435f20daa655ad
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-09-14 18:21:45 -0500

    Only add link_ifindex in case of self links, other cases could be added but IP addressing logic becomes affected

--- netsim/modules/vlan.py
+++ netsim/modules/vlan.py
@@ -479,7 +479,8 @@ def create_vlan_links(link: Box, v_attr: Box, topology: Box) -> None:
         if 'vlan' in intf and vname in intf.vlan.get('trunk',{}):
           intf_data = Box(intf.vlan.trunk[vname] or {},default_box=True,box_dots=True)
           intf_data.node = intf.node
-          intf_data.link_ifindex = link.interfaces.index(intf) # Add index of this subinterface in link.interfaces
+          if len([ i.node for i in link.interfaces if i.node == intf.node ]) > 1:
+            intf_data.link_ifindex = link.interfaces.index(intf) # For self loops, add index of this subinterface in link.interfaces
           intf_data.vlan.access = vname
           intf_node = topology.nodes[intf.node]
 
@@ -720,16 +721,15 @@ def find_parent_interface(intf: Box, node: Box, topology: Box) -> typing.Optiona
 
   link = link_list[0]
 
-  # There is a problem with self-looped links - there will be 2 matching interfaces here
-  #
-  # intf_list = [ intf for intf in link.interfaces if intf.node == node.name]
-  # if not intf_list:
-  #   return None
+  # There is a problem with self-looped links - there will be 2 matching interfaces below
+  if 'link_ifindex' in intf:    # To solve, use the link_ifindex populated upon creating of the subif
+    link_intf = link.interfaces[ intf.link_ifindex ]    
+  else:
+    intf_list = [ intf for intf in link.interfaces if intf.node == node.name]
+    if not intf_list:
+      return None
 
-  # link_intf = intf_list[0]  # Only takes the first one, in case there are two
-
-  # Instead, use the link_ifindex populated upon creating of the subif
-  link_intf = link.interfaces[ intf.link_ifindex ]
+    link_intf = intf_list[0]  # Only takes the first one, in case there are two
 
   node_iflist = [ intf for intf in node.interfaces if intf.ifindex == link_intf.ifindex]
   if not node_iflist:
