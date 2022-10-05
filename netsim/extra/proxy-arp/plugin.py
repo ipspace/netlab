b482008fa7e952ac0fa931313ef0283bf1923f21
J vanBemmel <jvb127@gmail.com>
2022-10-05 01:21:50 -0500

    Adds a plugin that enables proxy ARP on all interfaces in selected VRFs (#546)
    Co-authored-by: Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>

---
+++ netsim/extra/proxy-arp/plugin.py
@@ -0,0 +1,30 @@
+import typing, netaddr
+from box import Box
+
+from netsim import api
+
+"""
+Attribute 'proxy_arp' is enabled per VRF; not currently enforced
+"""
+# def init(topology: Box) -> None:
+
+"""
+Add 'proxy_arp' flag and trigger custom config file for any interfaces that are
+part of a VRF with proxy ARP enabled
+"""
+def post_transform(topology: Box) -> None:
+
+  if not topology.get('vrfs',[]):
+    return
+
+  target_vrfs = [ v for v,d in topology.vrfs.items() if 'proxy_arp' in d ]
+
+  config_name = api.get_config_name(globals())
+
+  # Iterate over node[x].interfaces that are part of a VRF with proxy_arp enabled
+  for n, ndata in topology.nodes.items():
+    for i in ndata.interfaces:
+      if i.type in ['lan','p2p','svi'] and 'vrf' in i and i.vrf in target_vrfs \
+        and ('ipv4' in i or 'ipv6' in i):
+          i.proxy_arp = True
+          api.node_config(ndata,config_name)
