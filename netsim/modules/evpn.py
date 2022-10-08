7fd9dea8f658f7c7dd36c39d3aa6e77c1b81c3cd
Ivan Pepelnjak <ip@ipspace.net>
2022-10-08 07:22:38 +0200

    Refactoring EVPN configuration module (#555)
    * Build evpn.vrfs and evpn.vlans lists based on vni/transit_vni
    * Validate contents of global- and node EVPN lists
    * Assign provisional RD and final RT to VLAN-based EVPN service
      (results in network-wide consistent RT)
    * Add evpn.evi (set to vrf.id) to IRB and bundle VRFs
    * Process evpn.transit_vni only with VXLAN transport, report error otherwise
    * Trim final EVPN lists on every node and assign local RDs to VLANs and VRFs
    
    Also:
    * Document interactions between VLAN, VRF, VXLAN, EVPN modules
    * Migrated reference checks needed for VXLAN and EVPN modules into _dataplane module
    * Integration test cases: VXLAN bridging with explicit evpn.vlans attribute
    * Rename start_transit_vlan to _start_transit_vlan (internal attribute)
    
    Co-authored-by: J vanBemmel <jvb127@gmail.com>

--- netsim/modules/evpn.py
+++ netsim/modules/evpn.py
@@ -6,6 +6,41 @@ from .. import common
 from .. import data
 from ..augment import devices
 
+"""
+validate_evpn_list
+
+* If missing, create evpn.vlans list with all VLANs with 'vni' attribute
+* Validate that the evpn.vlans list is a list with valid local or global VLAN names
+* If missing, create evpn.vrfs list with all VRFs with 'evpn.transit_vni' attribute
+* Validate that the evpn.vrfs list is a list with valid local or global VRF names
+
+The heavy lifting is done in a shared _dataplane function
+"""
+def validate_evpn_lists(toponode: Box, obj_path: str, topology: Box, create: bool = False) -> None:
+  _dataplane.validate_object_reference_list(
+    parent=toponode if not toponode is topology else None,
+    parent_path=obj_path,
+    topology=topology, 
+    list_name='evpn.vlans',
+    reference_dictionary='vlans',
+    reference_name='VLAN',
+    create_default=create,
+    merge_topology=False,
+    default_filter=lambda v: 'vni' in v and v.vni,
+    module='evpn')
+
+  _dataplane.validate_object_reference_list(
+    parent=toponode if not toponode is topology else None,
+    parent_path=obj_path,
+    topology=topology, 
+    list_name='evpn.vrfs',
+    reference_dictionary='vrfs',
+    reference_name='VLAN',
+    create_default=create,
+    merge_topology=False,
+    default_filter=lambda v: False if not isinstance(v,Box) else data.get_from_box(v,'evpn.transit_vni'),
+    module='evpn')
+
 def enable_evpn_af(node: Box, topology: Box) -> None:
   bgp_session = data.get_from_box(node,'evpn.session') or []
 
@@ -16,52 +51,62 @@ def enable_evpn_af(node: Box, topology: Box) -> None:
     if bn.type in bgp_session and 'evpn' in topology.nodes[bn.name].get('module'):
       bn.evpn = True
 
-def vlan_based_service(vlan: Box, vname: str, node: Box, topology: Box) -> None:
+def get_usable_evpn_asn(topology: Box) -> int:
+  asn = data.get_from_box(topology,'evpn.as') or \
+        data.get_from_box(topology,'vrf.as') or \
+        data.get_from_box(topology,'bgp.as')
+
+  if asn and data.is_true_int(asn):
+    return asn
+
+  common.error(
+    f'Cannot get a usable global AS number to use in EVPN route targets',
+    common.IncorrectValue,
+    'evpn')
+  return 0
+
+def vlan_based_service(vlan: Box, vname: str, topology: Box) -> None:
   evpn  = vlan.evpn
-  epath = f'nodes.{node.name}.vlans.{vname}.evpn'
+  epath = f'vlans.{vname}.evpn'
   evpn.evi = evpn.evi or vlan.id                                    # Default EVI value: VLAN ID
+  asn = get_usable_evpn_asn(topology)
   data.must_be_int(
     evpn,'evi',epath,
     module='evpn',
     min_value=1,max_value=65535)                                    # Check EVI data type in range
-  if not 'rd' in evpn:                                              # Default RD value
-    evpn.rd = f'{node.bgp.router_id}:{vlan.id}'
   for rt in ('import','export'):                                    # Default RT value
     if not rt in evpn:                                              # ... BGP ASN:vlan ID
-      evpn[rt] = [ f"{node.bgp['as']}:{vlan.id}" ]
+      evpn[rt] = [ f"{asn}:{evpn.evi}" ]
 
-def vlan_aware_bundle_service(vlan: Box, vname: str, node: Box, topology: Box) -> None:
+def vlan_aware_bundle_service(vlan: Box, vname: str, topology: Box) -> None:
   vrf_name = vlan.vrf
   if not vrf_name in topology.vrfs:
     common.error(
-      f'VXLAN-enabled VLAN {vname} on node {node.name} that is part of VLAN bundle must belong to a global VRF',
+      f'VXLAN-enabled VLAN {vname} that is part of VLAN bundle must belong to a global VRF',
       common.IncorrectValue,
       'evpn')
     return
 
-  if not vname in topology.vlans:
+  if 'evpn' in vlan:                                                # VLAN that is part of a bundle cannot have EVI/RT/RD attributes
     common.error(
-      f'VLAN {vname} on node {node.name} that is part of VLAN bundle must be a global VLAN',
+      f'VLAN {vname} is part of a VLAN bundle {vrf_name} and cannot have EVPN-related attributes',
       common.IncorrectValue,
       'evpn')
     return
 
-  if 'evpn' in node.vlans[vname]:                                   # VLAN that is part of a bundle cannot have EVI/RT/RD attributes
-    common.error(
-      f'VLAN {vname} on node {node.name} is part of a VLAN bundle {vrf_name} and cannot have EVPN-related attributes',
-      common.IncorrectValue,
-      'evpn')
-    return
+  vlan.evpn.bundle = vlan.vrf                                       # Set a 'we are in a bundle' flag
+  vrf = topology.vrfs[vrf_name]
+  data.must_be_dict(
+         parent=vrf,
+         key='evpn',
+         path=f'vrfs.{vrf_name}',
+         create_empty=True)
 
-  if not topology.vrfs[vrf_name].evpn:
-    topology.vrfs[vrf_name].evpn = {}                               # Make sure the global VRF EVPN attribute is a dictionary
-  g_evpn = topology.vrfs[vrf_name].evpn
-  if not 'evi' in g_evpn:                                           # If needed, set EVI attribute for the global VRF
-    g_evpn.evi = topology.vlans[vname].id                           # ... to first VLAN ID encountered (lowest when auto-assigned)
+  evpn = vrf.evpn
+  if not 'evi' in evpn:                                             # If needed, set EVI attribute for the global VRF
+    evpn.evi = vrf.id                                               # ... to the VRF ID
 
-  data.must_be_dict(node.vrfs[vrf_name],'evpn',f'nodes.{node.name}.vrfs.{vrf_name}',create_empty=True)
-  evpn = node.vrfs[vrf_name].evpn                                   # Now set VRF EVPN parameters for node VRF
-  evpn.evi = g_evpn.evi                                             # Copy EVI from global VRF
+  vlan.evpn.evi = evpn.evi
   for k in ('vlans','vlan_ids'):
     if not k in evpn:                                               # Is this the first EVPN-enabled VLAN in this VRF?
       evpn[k] = []                                                  # ... create an empty list of VLANs
@@ -94,7 +139,7 @@ def register_static_transit_vni(topology: Box) -> None:
           'evpn')
 
 """
-Set transit VNI values for symmetrical IRB VRFs
+Set transit VNI values for symmetrical IRB VRFs (REFACTOR to use _dataplane)
 """
 def get_next_vni(start_vni: int, used_vni_list: typing.List[int]) -> int:
   while True:
@@ -106,11 +151,25 @@ def vrf_transit_vni(topology: Box) -> None:
   if not 'vrfs' in topology:
     return
 
-  vni_list: typing.List[int] = []
+  vni_list: typing.List[int] = []                               # List of static transit VNIs
+  vni_error = False                                             # "A horrible error" flag that causes abort after the first loop
+  vni_count = 0                                                 # Number of VRFs with evpn.transit_vni
+  evpn_transport = data.get_from_box(topology,'evpn.transport') or 'vxlan'
+
   for vrf_name,vrf_data in topology.vrfs.items():               # First pass: build a list of statically configured VNIs
     if vrf_data is None:                                        # Skip empty VRF definitions
       continue
-    vni = data.get_from_box(vrf_data,'evpn.transit_vni')
+    vni = data.get_from_box(vrf_data,'evpn.transit_vni')        # transit_vni makes no sense with MPLS transport
+    if vni and evpn_transport != 'vxlan':
+      common.error(
+        f'evpn.transit_vni in VRF {vrf_name} is not allowed with mpls evpn.transport',
+        common.IncorrectValue,
+        'evpn')
+      vni_error = True
+      continue
+
+    if not vni is None:
+      vni_count = vni_count + 1                                 # Count number of VRFs with evpn.transit_vni attribute
     if not data.is_true_int(vni):                               # Skip non-integer values, no need to check them at this time
       continue
     if vni in vni_list:
@@ -126,6 +185,11 @@ def vrf_transit_vni(topology: Box) -> None:
         'evpn')
       continue  
     vni_list.append( vni )                                      # Insert it to detect duplicates elsewhere
+
+  if vni_error:                                                 # Found serious errors, makes no sense to continue
+    return
+  if not vni_count:                                             # No VRF found with evpn.transit_vni, no need to waste further CPU cycles
+    return
 
   vni_start = topology.defaults.evpn.start_transit_vni
   for vrf_name,vrf_data in topology.vrfs.items():               # Second pass: set transit VNI values for VRFs with "transit_vni: True"
@@ -165,24 +229,78 @@ def vrf_transit_vni(topology: Box) -> None:
       continue
     vrf_data.evpn.transit_vni = foreign_vni
 
-def vrf_irb_setup(node: Box, topology: Box) -> None:
-  features = devices.get_device_features(node,topology.defaults)
-  for vrf_name,vrf_data in node.get('vrfs',{}).items():
-    if not 'af' in vrf_data or not 'evpn' in vrf_data:          # VRF without EVPN data or L3 information is definitely not doing IRB
+def vrf_irb_setup(topology: Box) -> None:
+  for vrf_name,vrf_data in topology.get('vrfs',{}).items():
+    if not 'evpn' in vrf_data:                                  # VRF without EVPN data is definitely not doing IRB
       continue
-    if not vrf_name in topology.get('vrfs',{}):                 # Makes no sense to configure IRB for local VRF
+    
+    if not 'evi' in vrf_data.evpn:
+      vrf_data.evpn.evi = vrf_data.id
+
+"""
+Copy global evpn.vlans, evpn.vrfs, and evpn.transport into node evpn data structures.
+"""
+def copy_global_evpn_lists(node: Box, topology: Box) -> None:
+  g_evpn = topology.get('evpn',{})
+  for k in ('vlans','vrfs','transport'):
+    if k in g_evpn and not k in node.evpn:
+      node.evpn[k] = g_evpn[k]
+
+"""
+Trim node evpn.vlans and evpn.vrfs lists to refer only to on-node VLANs and VRFs
+"""
+def trim_node_evpn_lists(node: Box) -> None:
+  if not 'evpn' in node:                                        # No on-node EVPN structures, get out of here
+    return
+
+  for ref_list in ('vlans','vrfs'):
+    if ref_list in node.evpn:                                   # Filter EVPN list to refer only to on-node objects
+      node.evpn[ref_list] = [ obj_name for obj_name in node.get(ref_list,{}).keys() if obj_name in node.evpn[ref_list] ]
+
+"""
+Set local evpn.rd for evpn-enabled VLANs and VRFs
+"""
+def set_local_evpn_rd(node: Box) -> None:
+  for obj in ('vlans','vrfs'):
+    if not obj in node:
       continue
 
-    g_vrf = topology.vrfs[vrf_name]                             # Pointer to global VRF data, will come useful in a second
-    if data.get_from_box(g_vrf,'evpn.transit_vni'):             # Transit VNI in global VRF => symmetrical IRB
+    for o_name,o_data in node[obj].items():                     # Iterate over VLANs/VRFs
+      if not 'evpn' in o_data:
+        continue                                                # Skip non-EVPN objects
+      if not 'evi' in o_data.evpn:
+        common.error(
+          f'{obj}.{o_name} on node {node.name} has EVPN attributes but no evpn.evi ({o_data.evpn})',
+          common.IncorrectValue,
+          'evpn')
+        continue
+      #
+      # Set local RD unless a static RD was set
+      if not 'rd' in o_data.evpn:
+        o_data.evpn.rd = f'{node.bgp.router_id}:{o_data.evpn.evi}'
+
+"""
+Check whether VXLAN IRB mode is supported by the device
+"""
+def check_node_vrf_irb(node: Box, topology: Box) -> None:
+  features = devices.get_device_features(node,topology.defaults)
+  evpn_transport = data.get_from_box(node,'evpn.transport') or 'vxlan'
+  if evpn_transport != 'vxlan':                                 # Perform IRB checks only for VXLAN transport
+    return
+
+  for vrf_name,vrf_data in node.get('vrfs',{}).items():
+    if not vrf_data.get('af',None):                             # Cannot do IRB without L3 addresses ;)
+      continue
+
+    if data.get_from_box(vrf_data,'evpn.transit_vni'):          
       if not features.evpn.irb:                                 # ... does this device support IRB?
         common.error(
           f'VRF {vrf_name} on {node.name} uses symmetrical EVPN IRB which is not supported by {node.device} device',
           common.IncorrectValue,
           'evpn')
         continue
-      vrf_data.evpn.transit_vni = g_vrf.evpn.transit_vni        # Make transit VNI is copied into the local VRF
-      vrf_data.pop('ospf',None)                                 # ... and remove OSPF from EVPN IRB VRF
+
+      vrf_data.pop('ospf',None)                                 # Remove OSPF from EVPN IRB VRF
     else:
       if not features.evpn.asymmetrical_irb:                    # ... does this device asymmetrical IRB -- is it supported?
         common.error(
@@ -198,33 +316,42 @@ class EVPN(_Module):
 
   def module_pre_transform(self, topology: Box) -> None:
     register_static_transit_vni(topology)
+    data.must_be_string(
+           parent=topology,
+           key='evpn.transport',
+           path='topology',
+           valid_values=['vxlan','mpls'],
+           module='evpn')
 
   def module_post_node_transform(self, topology: Box) -> None:
+    validate_evpn_lists(topology,'topology',topology,create=True)
+    for n in topology.nodes.values():
+      validate_evpn_lists(n,f'nodes.{n.name}',topology,create=False)
+
     vrf_transit_vni(topology)
+    vlan_bundle = data.get_from_box(topology,'evpn.vlan_bundle_service')
+
+    for vname in data.get_from_box(topology,'evpn.vlans') or []:
+      vlan = topology.vlans[vname]
+      if not 'vrf' in vlan or not vlan_bundle:
+        vlan_based_service(vlan,vname,topology)
+      else:
+        vlan_aware_bundle_service(vlan,vname,topology)
+
+    vrf_irb_setup(topology)
 
   """
   Node post-transform: runs after VXLAN module
 
-  Add 'evi' (EVPN Instance),'rd' and 'rt' attributes to VLANs that have a 'vni' attribute
+  * Enable EVPN address family
+  * Build node-specific evpn.vlans and epvn.vrfs lists
+  * Calculate final RD values
+
   """
   def node_post_transform(self, node: Box, topology: Box) -> None:
     enable_evpn_af(node,topology)
-
-    vlan_list = data.get_from_box(node,'vxlan.vlans') or []       # Get the list of VXLAN-enabled VLANs
-    if not vlan_list:
-      return                                                      # This could be a route reflector running EVPN
-
     _routing.router_id(node,'bgp',topology.pools)                 # Make sure we have a usable router ID
-
-    for vname in vlan_list:
-      vlan = node.vlans[vname]
-      #
-      # VLAN based service is used for VLANs that are not in a VRF or when the EVPN VLAN-Aware Bundle 
-      # Service is disabled (default)
-      #
-      if not 'vrf' in vlan or not data.get_from_box(node,'evpn.vlan_bundle_service'):
-        vlan_based_service(vlan,vname,node,topology)
-      else:
-        vlan_aware_bundle_service(vlan,vname,node,topology)
-
-    vrf_irb_setup(node,topology)
+    check_node_vrf_irb(node,topology)
+    copy_global_evpn_lists(node,topology)
+    trim_node_evpn_lists(node)
+    set_local_evpn_rd(node)
