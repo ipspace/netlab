b569f70c7c570708ab80e52a0941c387f045825c
Ivan Pepelnjak <ip@ipspace.net>
2022-10-19 19:21:37 +0200

    Use common validation routines to validate top-level topology attributes (#593)
    * Use common routines in augment.topology.check_global_elements
    * Move augment.topology.check_global_elements in front of augment.nodes.validate
    
    Also:
    * Move the 'which module defines this extra element' check into common validation code
    * Add validation debugging
    * Further cleanup of module validation code (next step: group data)
    * Split 'wrong global/node attribute' and 'wrong group data module attribute' test cases
    
    Fixes #583

--- netsim/augment/topology.py
+++ netsim/augment/topology.py
@@ -12,7 +12,7 @@ from box import Box
 
 from .. import common
 from .. import data
-from ..data.validate import must_be_list
+from ..data.validate import must_be_list,validate_attributes
 
 #
 # Extend link/node/global attribute lists with extra attributes
@@ -78,35 +78,16 @@ def check_required_elements(topology: Box) -> None:
   topology.defaults.name = topology.name
 
 def check_global_elements(topology: Box) -> None:
-  topo_elements = topology.defaults.attributes.get('global',[]) + topology.defaults.attributes.get('internal',[])
-  module_list   = [ modname for modname in topology.defaults.keys() if 'supported_on' in topology.defaults[modname] ]
-  extra_modattr: dict = {}
-  for m,x_list in [ (modname,topology.defaults[modname].attributes.extra['global']) \
-                    for modname in topology.defaults.keys() \
-                      if data.get_from_box(topology.defaults[modname],'attributes.extra.global') ]:
-    for attr in x_list:
-      extra_modattr[attr] = m
-
-  if topology.get('module'):
-    topo_elements = topo_elements + topology.module
-
-  for k in topology.keys():
-    if not k in topo_elements:
-      if k in module_list:
-        common.error(
-          f'Add module {k} to global- or node configuration modules to use {k}.something global attribute',
-          category=common.IncorrectValue,
-          module="topology")
-      elif k in extra_modattr:
-        common.error(
-          f'Global attribute {k} is defined by module {extra_modattr[k]} which is not used in your topology',
-          category=common.IncorrectValue,
-          module="topology")
-      else:
-        common.error(
-          f'Unknown top-level element {k} -- typo?',
-          category=common.IncorrectValue,
-          module="topology")
+  providers = list(topology.defaults.providers.keys())
+  validate_attributes(
+    data=topology,                                  # Validate node data
+    topology=topology,
+    data_path='topology',                           # Topology path to node name
+    data_name=f'topology',
+    attr_list=['global','internal'],                # We're checking global (+ internal) attributes
+    modules=topology.get('module',[]),              # ... against topology modules
+    module='topology',                              # Function is called from 'nodes' module
+    extra_attributes = providers)                   # Allow provider-specific settings (not checked at the moment)
 
 #
 # Find virtualization provider, set provider and defaults.provider to that value
