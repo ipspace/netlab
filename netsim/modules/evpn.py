2ab3490940c908af4339c40d8101f04a7074c133
J vanBemmel <jvb127@gmail.com>
2022-10-31 13:27:27 -0500

    Remove OSPF in both symmetric and asymmetric cases

--- netsim/modules/evpn.py
+++ netsim/modules/evpn.py
@@ -299,8 +299,6 @@ def check_node_vrf_irb(node: Box, topology: Box) -> None:
           common.IncorrectValue,
           'evpn')
         continue
-
-      vrf_data.pop('ospf',None)                                 # Remove OSPF from EVPN IRB VRF
     else:
       if not features.evpn.asymmetrical_irb:                    # ... does this device asymmetrical IRB -- is it supported?
         common.error(
@@ -308,6 +306,8 @@ def check_node_vrf_irb(node: Box, topology: Box) -> None:
           common.IncorrectValue,
           'evpn')
         continue
+
+    vrf_data.pop('ospf',None)                                 # Remove OSPF from EVPN IRB VRF, in both cases
 
 class EVPN(_Module):
 
