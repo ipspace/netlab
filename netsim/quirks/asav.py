038b1f9c898e3ec009196fcf5f0b107a9d7fda77
Willi Meißner <willi.meissner@tu-dresden.de>
2022-11-09 18:05:24 +0100

    fix mypy errors

--- netsim/quirks/asav.py
+++ netsim/quirks/asav.py
@@ -9,10 +9,10 @@ from ..data import get_from_box
 
 def check_isis_p2p_interfaces(node: Box, topology: Box) -> None:
   for intf in node.interfaces:
-    for neighbor in get_from_box(intf, 'neighbors'):
+    for neighbor in intf['neighbors']:
       remote_node = get_from_box(neighbor, 'node')
       remote_interface_name = get_from_box(neighbor, 'ifname')
-      remote_interfaces = get_from_box(topology, f'nodes.{remote_node}.interfaces')
+      remote_interfaces = topology[f'nodes.{remote_node}.interfaces']
       for rintf in remote_interfaces:
         if get_from_box(rintf, 'ifname') == remote_interface_name:
           if get_from_box(rintf, 'isis.network_type') != None:
@@ -24,6 +24,8 @@ def check_isis_p2p_interfaces(node: Box, topology: Box) -> None:
             )
 
 class ASA(_Quirks):
+
+  @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
     mods = node.get('module', [])
     if 'isis' in mods:
