e0a1d569bae853864df436a4408b9a3b44ff6db9
Willi Meißner <willi.meissner@tu-dresden.de>
2022-11-09 18:23:47 +0100

    use Box syntax for better readablity and remove unnecessary `get_from_box` calls

--- netsim/quirks/asav.py
+++ netsim/quirks/asav.py
@@ -9,16 +9,15 @@ from ..data import get_from_box
 
 def check_isis_p2p_interfaces(node: Box, topology: Box) -> None:
   for intf in node.interfaces:
-    for neighbor in intf['neighbors']:
-      remote_node = get_from_box(neighbor, 'node')
-      remote_interface_name = get_from_box(neighbor, 'ifname')
-      remote_interfaces = topology[f'nodes.{remote_node}.interfaces']
+    for neighbor in intf.neighbors:
+      remote_node = neighbor.node
+      remote_interfaces = topology.nodes[remote_node].interfaces
       for rintf in remote_interfaces:
-        if get_from_box(rintf, 'ifname') == remote_interface_name:
+        if rintf.ifname == neighbor.ifname:
           if get_from_box(rintf, 'isis.network_type') != None:
             common.error(
                 f'Cisco ASA does not support P2P IS-IS links.'
-                f'Problematic Interface: {remote_node} {remote_interface_name}',
+                f'Problematic Interface: {remote_node} {neighbor.ifname}',
                 common.IncorrectType,
                 'quirks',
             )
