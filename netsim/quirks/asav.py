776d0b78a2763f1259502e24c4e323702a3ed88e
J vanBemmel <jvb127@gmail.com>
2022-11-10 07:16:34 -0600

    Proposal: check specifically for 'point-to-point' (?)

--- netsim/quirks/asav.py
+++ netsim/quirks/asav.py
@@ -16,7 +16,7 @@ def check_isis_p2p_interfaces(node: Box, topology: Box) -> None:
       remote_interfaces = topology.nodes[remote_node].interfaces
       for rintf in remote_interfaces:
         if rintf.ifname == neighbor.ifname:
-          if get_from_box(rintf, 'isis.network_type') != None:
+          if get_from_box(rintf, 'isis.network_type') == "point-to-point":
             common.error(
                 f'Cisco ASA does not support P2P IS-IS links.'
                 f'Problematic Interface: {remote_node} {neighbor.ifname}',
