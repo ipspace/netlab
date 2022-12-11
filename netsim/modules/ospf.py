73a09eee2adceecd045140102e99d29f07ba5f58
Ivan Pepelnjak <ip@ipspace.net>
2022-12-11 17:27:49 +0100

    IGP BFD status should not be present on interfaces without IGP
    The sequence of operations used in IGP modules resulted in igp.bfd being
    set on an interface even when the IGP itself was disabled with 'igp: false'.
    This commit fixes #679:
    
    * Changes the BFD IGP code to skip interfaces with IGP disabled
    * Disables IGP BFD status if the node is not using BFD module
    * Evaluates BFD state early in IGP data transformation to ensure
      the 'igp: false' flag is not lost
    
    Other fixes:
    
    * Moved OSPF and IS-IS unnumbered processing into a function to make the
      code more readable
    * Fix EIGRP code (it was passing OSPF or IS-IS as parameters to _routing
      functions)

--- netsim/modules/ospf.py
+++ netsim/modules/ospf.py
@@ -10,14 +10,57 @@ from . import bfd
 from .. import common
 from ..augment import devices
 
+# We need to set ospf.unnumbered if we happen to have OSPF running over an unnumbered
+# link -- Arista EOS needs an extra nerd knob to make it work
+#
+# An interface can be unnumbered if it has the 'unnumbered' flag set or if it
+# has IPv4 enabled but no IPv4 address (ipv4: true)
+#
+# While doing that, we also check for the number of neighbors on unnumbered interfaces
+# and report an error if there are none (in which case the interface should not be unnumbered)
+# or more than one (in which case OSPF won't work)
+#
+def ospf_unnumbered(node: Box, features: Box) -> bool:
+  OK = True
+
+  for l in node.get('interfaces',[]):
+    is_unnumbered = \
+      'unnumbered' in l or \
+      ('ipv4' in l and isinstance(l.ipv4,bool) and l.ipv4)
+    if is_unnumbered and 'ospf' in l:
+      node.ospf.unnumbered = True
+      if len(l.get('neighbors',[])) > 1:
+        common.error(
+          f'OSPF does not work over multi-access unnumbered IPv4 interfaces: node {node.name} link {l.name}',
+          common.IncorrectValue,
+          'ospf')
+        OK = False
+      elif not len(l.get('neighbors',[])):
+        common.error(
+          f'Configuring OSPF on an unnumbered stub interface makes no sense: node {node.name} link {l.name}',
+          common.IncorrectValue,
+          'ospf')
+        OK = False
+
+  if 'unnumbered' in node.ospf:
+    if not features.ospf.unnumbered:
+      common.error(
+        f'Device {node.device} used on node {node.name} cannot run OSPF over unnumbered interface',
+        common.IncorrectValue,
+        'interfaces')
+      OK = False
+
+  return OK
+
 class OSPF(_Module):
 
   def node_post_transform(self, node: Box, topology: Box) -> None:
     features = devices.get_device_features(node,topology.defaults)
 
     _routing.router_id(node,'ospf',topology.pools)
+    bfd.bfd_link_state(node,'ospf')
     #
-    # Initial cleanup
+    # Cleanup routing protocol from external/disabled interfaces
     for intf in node.get('interfaces',[]):
       if not _routing.external(intf,'ospf'):                # Remove external interfaces from OSPF process
         _routing.passive(intf,'ospf')                       # Set passive flag on other OSPF interfaces
@@ -25,39 +68,8 @@ class OSPF(_Module):
         if err:
           common.error(f'{err}\n... node {node.name} link {intf}')
 
-    # We need to set ospf.unnumbered if we happen to have OSPF running over an unnumbered
-    # link -- Arista EOS needs an extra nerd knob to make it work
-    #
-    # An interface can be unnumbered if it has the 'unnumbered' flag set or if it
-    # has IPv4 enabled but no IPv4 address (ipv4: true)
-    #
-    # While doing that, we also check for the number of neighbors on unnumbered interfaces
-    # and report an error if there are none (in which case the interface should not be unnumbered)
-    # or more than one (in which case OSPF won't work)
-    #
-    for l in node.get('interfaces',[]):
-      is_unnumbered = \
-        'unnumbered' in l or \
-        ('ipv4' in l and isinstance(l.ipv4,bool) and l.ipv4)
-      if is_unnumbered and 'ospf' in l:
-        node.ospf.unnumbered = True
-        if len(l.get('neighbors',[])) > 1:
-          common.error(
-            f'OSPF does not work over multi-access unnumbered IPv4 interfaces: node {node.name} link {l.name}',
-            common.IncorrectValue,
-            'ospf')
-        elif not len(l.get('neighbors',[])):
-          common.error(
-            f'Configuring OSPF on an unnumbered stub interface makes no sense: node {node.name} link {l.name}',
-            common.IncorrectValue,
-            'ospf')
-
-    if 'unnumbered' in node.ospf:
-      if not features.ospf.unnumbered:
-        common.error(
-          f'Device {node.device} used on node {node.name} cannot run OSPF over unnumbered interface',
-          common.IncorrectValue,
-          'interfaces')
+    if not ospf_unnumbered(node,features):
+      return
 
     #
     # Final steps:
@@ -69,6 +81,5 @@ class OSPF(_Module):
     _routing.remove_unaddressed_intf(node,'ospf')
     _routing.build_vrf_interface_list(node,'ospf',topology)
     _routing.routing_af(node,'ospf')
-    bfd.bfd_link_state(node,'ospf')
     _routing.remove_vrf_routing_blocks(node,'ospf')
     _routing.remove_unused_igp(node,'ospf')
