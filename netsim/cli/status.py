0df3986a66856cb73713fefeb7683e8ad8788321
Ivan Pepelnjak <ip@ipspace.net>
2023-03-28 16:22:14 +0200

    Implement netlab status show

--- netsim/cli/status.py
+++ netsim/cli/status.py
@@ -20,24 +20,28 @@ def status_parse(args: typing.List[str]) -> argparse.Namespace:
   parser = argparse.ArgumentParser(
     prog='netlab status',
     description='Display lab status')
-  parser.add_argument(
-    '-v','--verbose',
-    dest='verbose',
-    action='count',
-    default=0,
-    help='Verbose logging')
+#  parser.add_argument(
+#    '-v','--verbose',
+#    dest='verbose',
+#    action='count',
+#    default=0,
+#    help='Verbose logging')
   parser.add_argument(
     dest='action',
     action='store',
-    choices=['active','show','cleanup','reset'],
+    choices=['list','show','cleanup','reset'],
     help='Lab status action')
+  parser.add_argument(
+    dest='instance',
+    action='store',
+    nargs="*",
+    help='Display or cleanup specific lab instance(s)')
   if args:
     return parser.parse_args(args)
   else:
-    return parser.parse_args(['active'])
+    return parser.parse_args(['list'])
 
-def display_active_labs(topology: Box,args: argparse.Namespace) -> None:
-  lab_states = status.read_status(topology)
+def display_active_labs(topology: Box,args: argparse.Namespace,lab_states: Box) -> None:
   if not lab_states:
     print('No netlab-managed labs')
     return
@@ -58,19 +62,35 @@ def display_active_labs(topology: Box,args: argparse.Namespace) -> None:
     if result.stdout:
       print(f'{pdata.act_title}\n{"=" * 80}\n{result.stdout}\n')
 
+def show_lab_instance(lab_state: Box) -> None:
+  print(f'Lab {lab_state.id} in {lab_state.dir}')
+  print(f'  status: {lab_state.status}')
+  print(f'  provider(s): {",".join(lab_state.providers)}')
+  if lab_state.log:
+    newline = "\n"
+    print(f'\nLog:\n{"=" * 80}\n{newline.join(lab_state.log)}{newline}')
+
+def show_lab(topology: Box,args: argparse.Namespace,lab_states: Box) -> None:
+  for iid in args.instance:
+    if not iid in lab_states:
+      print(f'Unknown lab instance {iid}, skipping\n')
+      continue
+    show_lab_instance(lab_states[iid])
+
 action_map = {
-  'active': display_active_labs,
-#  'show': show_lab,
+  'list': display_active_labs,
+  'show': show_lab,
 #  'cleanup': cleanup_lab,
 #  'reset': reset_lab_status
 }
 
 def run(cli_args: typing.List[str]) -> None:
   topology = read_topology.load("package:cli/empty.yml","","package:topology-defaults.yml")
+  lab_states = status.read_status(topology)
   args = status_parse(cli_args)
   common.set_logging_flags(args)
   if args.action in action_map:
-    action_map[args.action](topology,args)
+    action_map[args.action](topology,args,lab_states)
   else:
     common.fatal(f'Unknown action {args.action}')
     return
