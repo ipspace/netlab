4c559cf5e97d535674c535bd3fc9e839a2cfbaed
Ivan Pepelnjak <ip@ipspace.net>
2023-07-08 08:40:53 +0200

    Add HTML support to 'report' output module

--- docs/outputs/report.md
+++ docs/outputs/report.md
@@ -4,6 +4,15 @@ The *report* output module uses its parameter as the name of a Jinja2 formatting
 
 The *report* output module tries to use the **defaults.outputs.report.*rname*** topology setting (*rname* is the report name). If that fails, it tries to read the Jinja2 template from **_rname_.j2** file in **reports** subdirectory of current directory, user _netlab_ directory (`~/.netlab`), system _netlab_ directory (`/etc/netlab`) and _netlab_ package directory.
 
+## Built-In Reports
+
 _netlab_ ships with the following built-in reports:
 
-* **addressing** -- Node/interface addressing report
+* **addressing** -- Node/interface addressing report in text format
+* **addressing.html** -- Node/interface addressing report in HTML format
+
+## Generating HTML reports
+
+If a report name includes `.html`, _netlab_ assumes the template generates HTML markup and adds HTML wrapper generated from `page.html.j2` to the generated text. The `page.html.j2` template included with _netlab_ contains the `head` and `body` HTML tags and a simple CSS style definition.
+
+If you want to customize the HTML reports, add `page.html.j2` to  one of the user directories the _reports_ module searches when trying to locate the template file (see above). Your HTML wrapper might include inline CSS (using the `style` tag) or a link to an external stylesheet.
--- netsim/outputs/report.py
+++ netsim/outputs/report.py
@@ -14,9 +14,51 @@ from ..utils import log,templates,strings
 
 from . import _TopologyOutput
 
+EXTRA_PATH: typing.List[str] = []
+
+def render_from_settings(settings: Box, fmt: str, topo: Box) -> str:
+  global EXTRA_PATH
+  try:
+    return templates.render_template(
+      data=topo.to_dict(),
+      j2_text = settings[fmt],
+      path="reports",
+      extra_path=EXTRA_PATH)+"\n"
+  except Exception as ex:
+    log.fatal(
+      text=f"Error rendering topology format {fmt}\n{strings.extra_data_printout(str(ex))}",
+      module='report')
+    return ""
+
+def render_from_file(fmt: str, topo: Box) -> str:
+  global EXTRA_PATH
+  try:
+    return templates.render_template(
+        data=topo.to_dict(),
+        j2_file=fmt+".j2",
+        path="reports",
+        extra_path=EXTRA_PATH)+"\n"
+  except jinja2.exceptions.TemplateNotFound:
+    log.error(
+      text=f'Cannot find "{fmt}.j2" in any of the report directories',
+      category=log.IncorrectValue,
+      module='report',
+      hint='source')
+    return ""
+  except Exception as ex:
+    log.fatal(
+      text=f"Error rendering {fmt}\n{strings.extra_data_printout(str(ex))}",
+      module="report")
+    return ""
+
+def render(fmt: str, settings: Box, topo: Box) -> str:
+  return render_from_settings(settings,fmt,topo) if fmt in settings else render_from_file(fmt,topo)
+
 class REPORT(_TopologyOutput):
 
   def write(self, topo: Box) -> None:
+    global EXTRA_PATH
+    EXTRA_PATH = _files.get_search_path("reports")
     outfile = self.settings.filename or '-'
     modname = type(self).__name__
 
@@ -25,39 +67,13 @@ class REPORT(_TopologyOutput):
       if len(self.filenames) > 1:
         log.error('Extra output filename(s) ignored: %s' % str(self.filenames[1:]),log.IncorrectValue,modname)
 
-    output = _files.open_output_file(outfile)
-
-    extra_path = _files.get_search_path("reports")
+    r_collect = ""
     for fmt in self.format:
-      if fmt in self.settings:
-        try:
-          output.write(
-            templates.render_template(
-              data=topo.to_dict(),
-              j2_text = self.settings[fmt],
-              path="reports",
-              extra_path=extra_path)+"\n")
-          output.write("\n")
-        except Exception as ex:
-          log.error(
-            text=f"Error rendering topology format {fmt}\n{strings.extra_data_printout(str(ex))}",
-            category=log.IncorrectValue)
-        continue
+      r_text = render(fmt,self.settings,topo)
+      if '.html' in fmt:
+        self.settings.html = r_text
+        r_text = render('page.html',self.settings,self.settings)
+      r_collect += r_text
 
-      try:
-        output.write(
-          templates.render_template(
-            data=topo.to_dict(),
-            j2_file=fmt+".j2",
-            path="reports",
-            extra_path=extra_path)+"\n")
-      except jinja2.exceptions.TemplateNotFound:
-        log.error(
-          text=f'Cannot find "{fmt}" in any of the report directories',
-          category=log.IncorrectValue,
-          module='report',
-          hint='source')
-      except Exception as ex:
-        log.error(
-          text=f"Error rendering {fmt}\n{strings.extra_data_printout(str(ex))}",
-          category=log.IncorrectValue)
+    if r_collect:
+      _files.create_file_from_text(outfile,r_collect)
