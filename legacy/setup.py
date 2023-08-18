35fabcff923dc2d798f115323cf55414567d238b
Ivan Pepelnjak <ip@ipspace.net>
2023-08-18 07:54:05 +0200

    Release 1.6.1

--- legacy/setup.py
+++ legacy/setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 sys.path.append('..')
 
-version="1.6.0"
+version="1.6.1"
 
 long_description = (Path(__file__).parent / "README.md").read_text()
 
--- netsim/__init__.py
+++ netsim/__init__.py
@@ -2,7 +2,7 @@
 
 import sys
 
-__version__ = "1.6.1-dev1"
+__version__ = "1.6.1"
 
 try:
   import box
