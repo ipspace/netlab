8253fd8282a8e9e74df4c75a28ca9b045621da16
Ivan Pepelnjak <ip@ipspace.net>
2023-09-12 18:42:32 +0200

    Make 'netlab test' command more resilient
    * Report errors after test crashes
    * Try to do forced cleanup
    * Report an explicit success
    * Support any test topology in templates/tests directory

--- netsim/cli/test.py
+++ netsim/cli/test.py
@@ -13,15 +13,17 @@ import shutil
 from box import Box
 from pathlib import Path
 
-from .. import utils
-from ..utils import read as _read
+from ..utils import log,read as _read, files as _files
 from . import external_commands
 
 #
 # CLI parser for 'netlab config' command
 #
 def test_parse(args: typing.List[str], settings: Box) -> argparse.Namespace:
-  choices = ( p for p in settings.providers.keys() if 'start' in settings.providers[p] )
+  c_path  = _files.get_traversable_path('package:templates/tests')   # Directory containing test scenarios
+  c_list  = _files.get_globbed_files(c_path,'*.yml')                  # Find all test scenarios
+  choices = [ Path(fn).stem for fn in c_list ]
+
   parser = argparse.ArgumentParser(
     prog='netlab test',
     description='Test virtual lab installation')
@@ -41,7 +43,7 @@ def test_parse(args: typing.List[str], settings: Box) -> argparse.Namespace:
     dest='provider',
     action='store',
     choices=list(choices),
-    help='Run tests for the specified provider')
+    help='Run tests for the specified provider/environment')
 
   return parser.parse_args(args)
 
@@ -56,16 +58,27 @@ def copy_topology(args: argparse.Namespace) -> None:
   try:
     shutil.copy(src_topo,"topology.yml")
   except:
-    utils.log.fatal("Cannot copy test topology file %s into %s" % (src_topo,args.workdir))
+    log.fatal("Cannot copy test topology file %s into %s" % (src_topo,args.workdir))
   if args.verbose:
     print("... done, moving on\n")
 
-def create_configs() -> None:
-  external_commands.print_step(2,"creating configuration files")
-  if not external_commands.run_command(["netlab","create"],True):
-    utils.log.fatal("netlab create failed, aborting...","netlab test")
+def cleanup_force() -> None:
+  print('''
+==============================================================================
+The test has failed. We will try to clean up the test directory and remove it,
+but there's no guarantee that the cleanup process will succeed, in which case
+please remove the test directory manually.
 
-def cleanup_working_directory(args: argparse.Namespace) -> None:
+You might want to copy the error messages generated during the test before
+proceeding. The cleanup process will start once you press RETURN.
+==============================================================================
+''')
+  input('Press RETURN to continue -> ')
+  external_commands.run_command('netlab down --cleanup --force')
+
+def cleanup_working_directory(args: argparse.Namespace, force_cleanup: bool) -> None:
+  if force_cleanup:
+    cleanup_force()
   if args.verbose:
     print("\nCleaning up -- removing %s and its contents" % args.workdir)
   os.chdir("..")
@@ -86,26 +99,32 @@ def cleanup_working_directory(args: argparse.Namespace) -> None:
 def run(cli_args: typing.List[str]) -> None:
   settings = _read.read_yaml('package:topology-defaults.yml')
   if not cli_args:
-    utils.log.fatal("Specify the virtualization environment to test or use -h to get help","test")
+    log.fatal("Specify the virtualization environment to test or use -h to get help","test")
 
   if not settings:
-    utils.log.fatal("Cannot read the global defaults","test")
+    log.fatal("Cannot read the global defaults","test")
 
   args = test_parse(cli_args,settings)
   if os.path.exists(args.workdir):
-    utils.log.fatal("Directory %s already exists, aborting" % args.workdir,"test")
+    log.fatal("Directory %s already exists, aborting" % args.workdir,"test")
 
   if args.verbose:
-    utils.log.set_logging_flags(args)
+    log.set_logging_flags(args)
 
   external_commands.run_probes(settings,args.provider,1)
   copy_topology(args)
-  create_configs()
+  force_cleanup = False
   if not external_commands.run_command('netlab up'):
-    utils.log.fatal('netlab up failed, aborting','test')
+    log.error('netlab up failed, aborting',log.FatalError,'test')
+    force_cleanup = True
   elif not external_commands.run_command('netlab down'):
-    utils.log.fatal('netlab down failed','test')
-#  external_commands.start_lab(settings,args.provider,3)
-#  external_commands.deploy_configs(4)
-#  external_commands.stop_lab(settings,args.provider,5)
-  cleanup_working_directory(args)
+    log.error('netlab down failed',log.FatalError,'test')
+    force_cleanup = True
+  else:
+    print('''
+==============================================================================
+All tests succeeded. You're good to go ;))
+==============================================================================
+''')
+
+  cleanup_working_directory(args,force_cleanup)
