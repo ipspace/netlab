1e6476c8b7491bb51856cfdab906838e27eb6d48
Ivan Pepelnjak <ip@ipspace.net>
2023-10-03 18:56:28 +0200

    Implement per-AF activation on multihop EBGP sessions

--- netsim/extra/ebgp.multihop/plugin.py
+++ netsim/extra/ebgp.multihop/plugin.py
@@ -5,18 +5,6 @@ from netsim.augment import devices
 from netsim.augment import links
 from netsim.data.validate import validate_attributes
 
-'''
-check_device_support -- check whether the device used in a multihop EBGP session supports it
-'''
-def check_device_support(ndata: Box, topology: Box) -> None:
-  features = devices.get_device_features(ndata,topology.defaults)
-  if features.bgp.get('multihop',None):
-    return
-  
-  log.error(
-    f'EBGP multihop is not supported by node {ndata.name} (device {ndata.device})',
-    log.IncorrectValue,'ebgp.multihop')
-
 def pre_transform(topology: Box) -> None:
   config_name  = api.get_config_name(globals())        # Get the plugin configuration name
   session_idx  = data.find_in_list(['ebgp.utils','bgp.session'],topology.plugin)
@@ -31,8 +19,8 @@ def pre_link_transform(topology: Box) -> None:
   if not 'multihop' in topology.get('bgp',{}):
     return
 
-  sessions = links.adjust_link_list(topology.bgp.multihop,topology.nodes,'bgp.multihop[{link_cnt}]')
-  topology.bgp.multihop = sessions
+  sessions = links.adjust_link_list(topology.bgp.multihop.sessions,topology.nodes,'bgp.multihop[{link_cnt}]')
+  topology.bgp.multihop.sessions = sessions
   for s in sessions:
     for attr in list(s.keys()):                       # Change session attributes into BGP link attributes
       if attr in ['interfaces','_linkname']:          # Skip internal attributes
@@ -83,9 +71,63 @@ def pre_link_transform(topology: Box) -> None:
       intf.bgp.multihop = 255
       intf._bgp_session = True
 
-
   topology.links.extend(sessions)
 
+'''
+check_device_support -- check whether the device used in a multihop EBGP session supports it
+'''
+def check_multihop_support(ndata: Box, topology: Box) -> None:
+  features = devices.get_device_features(ndata,topology.defaults)
+  if features.bgp.get('multihop',None):
+    return
+  
+  log.error(
+    f'EBGP multihop is not supported by node {ndata.name} (device {ndata.device})',
+    log.IncorrectValue,'ebgp.multihop')
+
+'''
+check_af_activation_support -- check whether the device used in a multihop EBGP session supports per-af activation
+'''
+def check_af_activation_support(ndata: Box, topology: Box) -> None:
+  features = devices.get_device_features(ndata,topology.defaults)
+  if features.bgp.get('activate_af',None):
+    return
+  
+  log.error(
+    f'Selective BGP address family activation is not supported by node {ndata.name} (device {ndata.device})',
+    log.IncorrectValue,'ebgp.multihop')
+
+'''
+augment_af_activation -- change the address families active on EBGP multihop sessions
+'''
+def augment_af_activation(ndata: Box, topology: Box) -> None:
+  af_set = ndata.get('bgp.multihop.activate',None) or topology.get('bgp.multihop.activate',None)
+  af_list = topology.defaults.bgp.attributes['global'].multihop.activate.ipv4.valid_values
+
+  if not af_set:
+    return
+
+  check_af_activation_support(ndata,topology)
+  for ngb in ndata.get('bgp.neighbors',[]):
+    if not 'multihop' in ngb:                                     # Skip regular neighbors
+      continue
+    for af in ['ipv4','ipv6']:                                    # A neighbor could be an IPv4 or IPv6 neighbor
+      if not af in ngb:                                           # Skip the irrelevant transport AF
+        continue
+      for bgp_af in af_list:                                      # Iterate over all potential address famiilies
+        chg = ngb.activate if bgp_af in ['ipv4','ipv6'] else ngb  # Find the object to change (neighbor or activate dictionary)
+        if bgp_af in af_set[af]:                                  # Is the AF active on this transport EBGP multhop session?
+          chg[bgp_af] = True                                      # Yes, turn it on
+        else:
+          chg.pop(bgp_af,None)                                    # Otherwise remove it
+
+'''
+post_transform processing:
+
+* Remove fake interfaces and links
+* Check device (= template) support
+* Augment address family activation
+'''
 def post_transform(topology: Box) -> None:
   config_name = api.get_config_name(globals())        # Get the plugin configuration name
   for ndata in topology.nodes.values():
@@ -93,7 +135,8 @@ def post_transform(topology: Box) -> None:
     ndata.interfaces = [ intf for intf in ndata.interfaces if not intf.get('_bgp_session',None) ]
 
     if len(ndata.interfaces) != intf_count:           # Did the interface count change?
-      check_device_support(ndata,topology)
+      check_multihop_support(ndata,topology)
       api.node_config(ndata,config_name)              # We must have some multihop sessions, add extra config
+      augment_af_activation(ndata,topology)
 
   topology.links = [ link for link in topology.links if not link.get('_bgp_session',None) ]
