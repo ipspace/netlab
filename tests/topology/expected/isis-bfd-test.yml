ipspace/netlab

commit 442f4bd60c4d7d83173fc14619f6f31e9fbb81fd

Add typing tests of 'extra' plugins to the CI/CD tests

.github/workflows/tests.yml
@@ -25,10 +25,10 @@ jobs:
     - name: Run Mypy
       run: |
         mypy -p netsim
-    - name: Run transformation tests
+    - name: Run typing and transformation tests
       run: |
         cd tests
-        PYTHONPATH="../" pytest
+        ./run-tests.sh
     - name: Check integration tests
       if: ${{ github.event_name == 'pull_request' }}
       run: |

netsim/extra/ebgp.multihop/plugin.py
@@ -13,7 +13,7 @@ def pre_transform(topology: Box) -> None:
   session_idx  = data.find_in_list(['ebgp.utils','bgp.session'],topology.plugin)
   multihop_idx = data.find_in_list([ _config_name ],topology.plugin)
 
-  if session_idx is not None and session_idx > multihop_idx:
+  if session_idx is not None and multihop_idx is not None and session_idx > multihop_idx:
     log.error(
       'ebgp.multihop plugin must be included after bgp.session/ebgp.utils plugin',
       log.IncorrectValue,'ebgp.multihop')
@@ -66,7 +66,7 @@ def pre_link_transform(topology: Box) -> None:
       if not 'loopback' in node:
         log.error(
           'Cannot establish EBGP multihop session from node {intf.name}: node has no loopback interface',
-          'ebgp.multihop',log.IncorrectValue)
+          log.IncorrectValue,'ebgp.multihop')
         continue
       for af in ['ipv4','ipv6']:
         if af in node.loopback:

tests/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIRNAME=`dirname $0`
-echo "Executing tests in $DIRNAME"
+echo "Executing CI/CD tests in $DIRNAME"
 cd $DIRNAME
 PYTHONPATH="../" python3 -m pytest -vvv
 #
@@ -9,3 +9,6 @@ PYTHONPATH="../" python3 -m pytest -vvv
 #
 rm -fr *files
 cd ..; python3 -m mypy --no-incremental -p netsim
+for file in netsim/extra/*/plugin.py; do
+  python3 -m mypy $file
+done

tests/run-typing.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+DIRNAME=`dirname $0`
+echo "Executing typing tests in $DIRNAME"
+cd $DIRNAME
+cd ..; python3 -m mypy --no-incremental -p netsim
+for file in netsim/extra/*/plugin.py; do
+  python3 -m mypy $file
+done

commit 2c1876db3f531fd486d76e7ba4923f9520e46a35

Fix the CI/CD tests (#909)
442f4bd6 broke the CI/CD tests -- the errors were reported but the script did not
exit with the error status, so CI/CD pipeline passed. When 48ba624b broke the
Arista cEOS test cases the CI/CD tests failed to catch the error.

This commit rewrites the CI/CD tests to fail when needed and adds the fixed
Arista cEOS tests cases.

.github/workflows/tests.yml
@@ -24,11 +24,15 @@ jobs:
         pip install -r requirements.txt -r requirements-dev.txt
     - name: Run Mypy
       run: |
+        set -e
         mypy -p netsim
-    - name: Run typing and transformation tests
+        for file in netsim/extra/*/plugin.py; do
+          python3 -m mypy $file
+        done
+    - name: Run transformation tests
       run: |
         cd tests
-        ./run-tests.sh
+        PYTHONPATH="../" pytest
     - name: Check integration tests
       if: ${{ github.event_name == 'pull_request' }}
       run: |

tests/topology/expected/device-global-attr.yml
@@ -75,6 +75,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/isis-bfd-test.yml
@@ -233,6 +233,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -281,6 +282,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -335,6 +337,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -384,6 +387,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -597,6 +601,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/libvirt-clab-complex.yml
@@ -363,6 +363,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
       ports:

tests/topology/expected/vlan-access-links.yml
@@ -172,6 +172,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -254,6 +255,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vlan-access-neighbors.yml
@@ -87,6 +87,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -144,6 +145,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -181,6 +183,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vlan-access-single.yml
@@ -199,6 +199,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -294,6 +295,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -379,6 +381,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vlan-routed-vrf.yml
@@ -664,6 +664,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vrf-igp.yml
@@ -143,6 +143,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -312,6 +313,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -481,6 +483,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -519,6 +522,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vrf-links.yml
@@ -123,6 +123,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -294,6 +295,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -425,6 +427,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vrf.yml
@@ -99,6 +99,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -258,6 +259,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -351,6 +353,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos

tests/topology/expected/vxlan-router-stick.yml
@@ -76,6 +76,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -287,6 +288,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos
@@ -379,6 +381,7 @@ nodes:
     box: ceos:4.26.4M
     clab:
       env:
+        CLAB_MGMT_VRF: management
         INTFTYPE: et
       kind: ceos
     device: eos