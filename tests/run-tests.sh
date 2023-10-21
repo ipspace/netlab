423f3d9735518b1cb762e6a072d3c565924a91d0
Ivan Pepelnjak <ip@ipspace.net>
2023-10-21 16:10:45 +0200

    Stop mypy tests on first error

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -8,6 +8,7 @@ PYTHONPATH="../" python3 -m pytest -vvv
 # (until we fix that)
 #
 rm -fr *files
+set -e
 cd ..; python3 -m mypy --no-incremental -p netsim
 for file in netsim/extra/*/plugin.py; do
   python3 -m mypy $file
--- tests/run-typing.sh
+++ tests/run-typing.sh
@@ -2,6 +2,7 @@
 DIRNAME=`dirname $0`
 echo "Executing typing tests in $DIRNAME"
 cd $DIRNAME
+set -e
 cd ..; python3 -m mypy --no-incremental -p netsim
 for file in netsim/extra/*.*/*.py; do
   python3 -m mypy $file
