9f6b1d12de65de0917f8aeb403befdf807f32518
Ivan Pepelnjak <ip@ipspace.net>
2024-03-13 08:50:02 +0100

    Cleanup the 'using netlab from Git repo' setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 me=${BASH_SOURCE:-$_}
-echo "Set up paths for netsim-tools downloaded from Github"
-echo
+echo "Adding netlab Git repository to the search path"
 dir=$(pwd)
 if [ -z "$me" ]; then
   echo "Cannot figure out who I am. Aborting..."
 else
   cd $(dirname "$me")
   pwd=$(pwd)
-  export PATH=$pwd:$pwd/netsim/ansible:$PATH
-  echo "added $pwd and netsim/ansible directories to PATH"
+  export PATH=$pwd:$PATH
+  echo "... added $pwd to PATH"
   cd "$dir"
 fi
