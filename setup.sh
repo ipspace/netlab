e89cf322b75a6cb0dbf356927aa7db3b27078968
Ivan Pepelnjak <ip@ipspace.net>
2024-03-30 15:14:14 +0100

    Set PYTHONPATH when running netlab from a Git repo

--- setup.sh
+++ setup.sh
@@ -4,10 +4,20 @@ echo "Adding netlab Git repository to the search path"
 dir=$(pwd)
 if [ -z "$me" ]; then
   echo "Cannot figure out who I am. Aborting..."
-else
-  cd $(dirname "$me")
-  pwd=$(pwd)
-  export PATH=$pwd:$PATH
-  echo "... added $pwd to PATH"
-  cd "$dir"
+  exit 1
 fi
+
+cd $(dirname "$me")
+pwd=$(pwd)
+export PATH=$pwd:$PATH
+echo "... added $pwd to PATH"
+
+if [ -z "$PYTHONPATH" ]; then
+  export PYTHONPATH=$pwd
+  echo "... created PYTHONPATH"
+else
+  export PYTHONPATH=$pwd:$PYTHONPATH
+  echo "... added $pwd to PYTHONPATH"
+fi
+
+cd "$dir"
