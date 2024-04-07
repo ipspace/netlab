5068bf3bd561b2b172aa79ab11971b470d3d5e76
CD/CD tests <ip@ipspace.net>
2024-04-07 15:00:12 +0000

    Reduced RA timer on Nexus OS

---
+++ redo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+. vars.sh
+export NETLAB_DEVICE=$1
+shift
+export NETLAB_PROVIDER=$1
+shift
+export MODULE=$1
+shift
+if [[ -z $1 ]]; then
+  echo "Usage: redo.sh device provider suite test-pattern"
+  exit
+fi
+LOG_PATH=$CICD_LOG_PATH/$NETLAB_DEVICE/$NETLAB_PROVIDER/$MODULE
+for limit in $@; do
+  pushd $(realpath "$CICD_TEST_PATH") >/dev/null
+  echo "Starting device $NETLAB_DEVICE provider $NETLAB_PROVIDER module $MODULE limit $limit logging on $LOG_PATH"
+  ./device-module-test $MODULE --workdir /tmp/netlab_cicd --logdir "$LOG_PATH" --batch --redo $limit
+  popd >/dev/null
+done
