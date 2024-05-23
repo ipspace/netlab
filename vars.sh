df4c15dcd0f9c9d285279318a0e11ee0e2abf7f4
Ivan Pepelnjak <ip@ipspace.net>
2024-05-23 19:21:03 +0200

    Remove old test scripts

--- vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-export CICD_TEST_PATH=~/net101/tools/tests/integration/
-export CICD_HOME=~/netlab_cicd
-export CICD_LOG_PATH=$CICD_HOME
-export NETLAB_PLUGIN=multilab
-export NETLAB_MULTILAB_ID=17
-export PATH=.:$PATH
