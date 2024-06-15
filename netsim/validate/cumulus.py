f021474e2b36659ec820ef06eadb90e92fb6ecd5
Ivan Pepelnjak <ip@ipspace.net>
2024-06-15 18:04:11 +0200

    Add more validation functions to Cumulus Linux validation plugin

--- netsim/validate/cumulus.py
+++ netsim/validate/cumulus.py
@@ -10,3 +10,4 @@ Import:
 from box import Box
 from netsim.validate.bgp.frr import *
 from netsim.validate.linux import *
+from netsim.validate.ospf.frr import show_ospf_neighbor,valid_ospf_neighbor,show_ospf_prefix,valid_ospf_prefix
