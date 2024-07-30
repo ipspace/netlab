ff1ad6ee9981b68098ae59d9d9da5981d3b70b21
Ivan Pepelnjak <ip@ipspace.net>
2024-07-30 09:04:54 +0200

    Bug fix: Check SR Linux device type when using MPLS module

--- netsim/devices/srlinux.py
+++ netsim/devices/srlinux.py
@@ -35,9 +35,17 @@ class SRLINUX(_Quirks):
       for c,vals in topology.get('bgp.community',[]).items():
         if 'extended' not in vals:
            log.error(
-                      f'SR Linux on ({node.name}) does not support filtering out extended communities for BGP. {c}:{vals}\n',
-                      Warning,
-                      'quirks')
+              f'SR Linux on ({node.name}) does not support filtering out extended communities for BGP. {c}:{vals}\n',
+              Warning,
+              'quirks')
+           
+    if 'mpls' in mods:
+      dt = node.clab.type
+      if dt not in ['ixr6','ixr10','ixr6e','ixr10e']:
+        log.error(
+          f'SR Linux device type must be set to ixr6/ixr10 for MPLS to work (node {node.name})',
+          log.IncorrectValue,
+          'quirks')
 
   def check_config_sw(self, node: Box, topology: Box) -> None:
     need_ansible_collection(node,'nokia.grpc')
