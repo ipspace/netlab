f8856a956490e6d68b67a1bcfc2421c0ad18b25a
Ivan Pepelnjak <ip@ipspace.net>
2024-08-26 08:39:00 +0200

    Add '--all' option to 'netlab inspect' to add global Ansible vars to node data

--- netsim/cli/inspect.py
+++ netsim/cli/inspect.py
@@ -15,7 +15,7 @@ from ..outputs import _TopologyOutput
 from ..outputs import common as outputs_common
 from ..utils import strings,log
 from ..data.types import must_be_id
-from ..data import global_vars
+from ..data import global_vars,get_empty_box
 
 #
 # CLI parser for 'netlab inspect' command
@@ -36,6 +36,10 @@ def inspect_parse(args: typing.List[str]) -> argparse.Namespace:
     '--node',
     dest='node', action='store',
     help='Display data for selected node(s)')
+  parser.add_argument(
+    '--all',
+    dest='all', action='store_true',
+    help='Add global Ansible variables to node data')
   parser.add_argument(
     '--format',
     dest='format', action='store',
@@ -62,16 +66,30 @@ def fmt_value(v: typing.Union[Box,BoxList], fmt: str) -> str:
   value = v.to_yaml() if fmt == 'yaml' else v.to_json()
   return value.strip('\n')
 
+"""
+Read Ansible variables for the 'all' group (paths, pools, prefixes)
+"""
+def read_all_group_vars() -> Box:
+  fname = 'group_vars/all/topology.yml'
+  try:
+    return Box().from_yaml(filename='group_vars/all/topology.yml')
+  except Exception as ex:
+    log.fatal(f"Cannot read variables for 'all' group: {str(ex)}")    
+
 def inspect_node(topology: Box, node_list: list, args: argparse.Namespace) -> None:
   o_format = args.format or 'yaml'
   hdr_row: list = []
   data_row: list = []
 
+  global_data = read_all_group_vars() if args.all else get_empty_box()
+    
   for node in node_list:
     node_data = outputs_common.adjust_inventory_host(
                   node=topology.nodes[node],
                   defaults=topology.defaults,
                   group_vars=True)
+    if global_data:
+      node_data = global_data + node_data
     if len(node_list) == 1:
       inspect_value(node_data,args)
       return
