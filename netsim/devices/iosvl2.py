740ef21d908dd9527315a7d91fd64c2a762e145b
Ivan Pepelnjak <ip@ipspace.net>
2024-09-01 16:55:45 +0200

    Cisco IOSvL2 cannot use VLANs 1002..1005

--- netsim/devices/iosvl2.py
+++ netsim/devices/iosvl2.py
@@ -1,7 +1,27 @@
 #
 # Cisco IOSvL2 quirks
 #
+from box import Box
+
+from ..utils import log
+from ..modules import _routing
+from ..augment import devices
+
 from .iosv import IOS as _IOS
 
+def check_reserved_vlans(node: Box, topology: Box) -> None:
+  for vname,vdata in node.get('vlans',{}).items():
+    if vdata.id in range(1002,1006):
+      log.error(
+        f'Cannot use VLAN ID {vdata.id} (VLAN {vname}) on Cisco IOSvL2 for historic reasons',
+        category=log.IncorrectValue,
+        module='quirks')
+
 class IOSvL2(_IOS):
-  pass
+  @classmethod
+  def device_quirks(self, node: Box, topology: Box) -> None:
+    super().device_quirks(node,topology)
+    mods = node.get('module',[])
+
+    if 'vlan' in mods:
+      check_reserved_vlans(node,topology)
