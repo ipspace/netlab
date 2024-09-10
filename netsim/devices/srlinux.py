77752dcf9f7840777de2d456c84d48b7bd3f9a87
Ivan Pepelnjak <ip@ipspace.net>
2024-09-10 08:53:06 +0200

    SR Linux: Check for the license needed to run IXR6/IXR10 containers (#1309)
    Trying to implement the "Principle of Least Astonishment", netlab checks whether
    you have the necessary license to run IXR6/IXR10 containers.
    
    https://containerlab.dev/manual/kinds/srl/#types

--- netsim/devices/srlinux.py
+++ netsim/devices/srlinux.py
@@ -13,6 +13,13 @@ class SRLINUX(_Quirks):
 
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
+    dt = node.clab.type
+    if dt in ['ixr6','ixr10','ixr6e','ixr10e'] and not node.clab.get('license',None):
+      log.error(
+        f'You need a valid SR Linux license to run {dt} container on node {node.name}',
+        log.MissingValue,
+        'quirks')
+
     mods = node.get('module',[])
     if 'vrf' in mods:
         for _,vrf in node.get('vrfs', {}).items():
@@ -40,7 +47,6 @@ class SRLINUX(_Quirks):
               'quirks')
 
     if 'mpls' in mods or 'sr' in mods:
-      dt = node.clab.type
       if dt not in ['ixr6','ixr10','ixr6e','ixr10e']:
         log.error(
           f'SR Linux device type must be set to ixr6/ixr10 for MPLS to work (node {node.name})',
