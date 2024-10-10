347315d293324c27b599630c9859011d9827483a
Jeroen van Bemmel <jvb127@gmail.com>
2024-10-09 20:44:34 -0500

    Add lag.ifindex

--- netsim/modules/lag.py
+++ netsim/modules/lag.py
@@ -3,14 +3,25 @@ import netaddr
 import re
 
 from box import Box, BoxList
-from . import _Module
+from . import _Module, _dataplane
 from .. import data
 from ..utils import log
 from ..augment import devices, links
 
-GROUPNAME = re.compile(r'^links\[(?P<group>\w+)\]\[\d+\]')
+GROUPNAME = re.compile(r'^links\[(?P<group>[\w\-_]+)\]\[\d+\]')
+
+ID_SET = 'lag_id'
+
+def populate_lag_id_set(topology: Box) -> None:
+  _dataplane.create_id_set(ID_SET)
+  LAG_IDS = { l.lag.ifindex for l in topology.links if 'lag' in l and 'ifindex' in l.lag }
+  _dataplane.extend_id_set(ID_SET,LAG_IDS)
+  _dataplane.set_id_counter(ID_SET,topology.defaults.lag.start_lag_id,100)
 
 class LAG(_Module):
+
+  def module_pre_transform(self, topology: Box) -> None:
+    populate_lag_id_set(topology)
 
   """
   link_pre_transform: Process LAG links and add member links to the topology
@@ -62,14 +73,17 @@ class LAG(_Module):
         parent = data.get_box(link)
         parent._linkname = group_name
         parent.linkindex = len(topology.links) + 1
-        parent.interfaces = link.interfaces + []    # Deep copy
+        parent.interfaces = link.interfaces + []    # Deep copy, assumes all links have same 2 nodes
         parent.lag._parent = True                   # Set flag for filtering
+        if 'ifindex' not in parent.lag:             # Use given lag.ifindex, if any
+          parent.lag.ifindex = _dataplane.get_next_id(ID_SET)
         topology.links.append( parent )
         if log.debug_active('lag'):
           print(f'LAG link_pre_transform created virtual parent {parent}')
       else:
         parent = parent[0]
-        # parent.interfaces.extend( link.interfaces )  # results in duplicate bond interfaces?
+        # For future mc-lag: add any new nodes
+        # parent.interfaces.extend( [ n for n in link.interfaces if n not in parent.interfaces ] )
 
       # Modify the LAG member link
       link.type = "p2p"                   # Change type back to p2p
