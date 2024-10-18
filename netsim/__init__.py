b2d534b8a19deaa7e4dac1d5e48e55066e67b8a8
Ivan Pepelnjak <ip@ipspace.net>
2024-10-18 08:30:08 +0200

    Bug fix: netsim requires python-box release 7.2.0 or greater
    python-box release 7.2.0 correctly handles box.get('a.b.c') calls
    where box.a is not a dictionary. Earlier python-box releases crash.

--- netsim/__init__.py
+++ netsim/__init__.py
@@ -7,8 +7,8 @@ __version__ = "1.9.2-dev1"
 try:
   import box
 
-  if box.__version__ < '7.0':                 # netlab needs Python Box version 7.0 or higher
-    print("FATAL ERROR: python-box version 7.0 or higher required, use 'pip3 install --upgrade python-box' to install")
+  if box.__version__ < '7.2.0':                 # netlab needs Python Box version 7.0 or higher
+    print("FATAL ERROR: python-box version 7.2.0 or higher required, use 'pip3 install --upgrade python-box' to install")
     sys.exit(1)
 except:                                       # box is not installed, something else is bound to fail ;)
   pass
