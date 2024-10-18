faacdf55805dc50fbd678da386fdd66a8f971e76
Ivan Pepelnjak <ip@ipspace.net>
2024-10-18 09:16:54 +0200

    Bug fix: abort on fatal initialization errors
    * The netsim initialization module failed to abort after reporting
      incorrect python-box version because sys.exit was executed
      inside the try block
    * Netlab reported a failure to import netsim but then tried to
      continue like nothing happened

--- netsim/__init__.py
+++ netsim/__init__.py
@@ -4,11 +4,17 @@ import sys
 
 __version__ = "1.9.2-dev1"
 
+abort = False
+
 try:
   import box
 
   if box.__version__ < '7.2.0':                 # netlab needs Python Box version 7.0 or higher
     print("FATAL ERROR: python-box version 7.2.0 or higher required, use 'pip3 install --upgrade python-box' to install")
-    sys.exit(1)
+    abort = True
+
 except:                                       # box is not installed, something else is bound to fail ;)
   pass
+
+if abort:
+  sys.exit(1)
