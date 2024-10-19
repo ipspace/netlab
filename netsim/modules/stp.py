2353757f50e1a4e94442226995753e2cde8399a3
Jeroen van Bemmel <jvb127@gmail.com>
2024-10-19 13:36:39 -0500

    * Cumulus uses max port priority of 240 in increments of 16 * Check for proper bridge priority
    * Cumulus fails to enable a port in the 03-vlan-multi-link.yml scenario, but it also fails without the new STP module.
      It works with FRR (and its multi-bridge STP); troubleshooting shows a complex mix of STP/RSTP packets and no resolution on Cumulus.
      Suggest to declare this 'FFS'

--- netsim/modules/stp.py
+++ netsim/modules/stp.py
@@ -33,3 +33,31 @@ class STP(_Module):
         f'node {node.name} (device {node.device}) does not support per-VLAN STP (PVRST)',
         log.IncorrectValue,
         'stp')
+
+  # Check max port_priority values
+  def node_post_transform(self, node: Box, topology: Box) -> None:
+    if not 'stp' in node.get('module',[]):
+      return
+    features = devices.get_device_features(node,topology.defaults)
+
+    priority = node.get('stp.priority',0)
+    if priority and (priority % 4096):
+        log.error(
+            f'node {node.name} (device {node.device}) stp.priority: {priority} must be a multiple of 4096',
+            log.IncorrectValue,
+            'stp')
+
+    port_priority = features.get('stp.port_priority', { 'max': 255 } )
+    for intf in node.get('interfaces',[]):
+      if 'stp' in intf:
+        val = intf.get('stp.port_priority',0)
+        if val > port_priority.max:
+          log.error(
+            f'node {node.name} (device {node.device}) only supports stp.port_priority up to {port_priority.max}, found {val}',
+            log.IncorrectValue,
+            'stp')
+        elif 'multiple' in port_priority and (val % port_priority.multiple):
+          log.error(
+            f'node {node.name} (device {node.device}) stp.port_priority {val} must be a multiple of {port_priority.multiple}',
+            log.IncorrectValue,
+            'stp')
