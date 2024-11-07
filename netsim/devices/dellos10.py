5e702c97dc526a31ca49a530082fb0129cb00f1a
Ivan Pepelnjak <ip@ipspace.net>
2024-11-07 16:18:48 +0100

    Fix the VRF check :(

--- netsim/devices/dellos10.py
+++ netsim/devices/dellos10.py
@@ -1,15 +1,14 @@
 #
 # Dell OS10 quirks
 #
-from box import Box
+from box import Box,BoxList
 
 from . import _Quirks,need_ansible_collection,report_quirk
 from ..utils import log
 from ..augment import devices
 
-def check_vlan_ospf(node: Box, vname: str) -> None:
+def check_vlan_ospf(node: Box, iflist: BoxList, vname: str) -> None:
   name = node.name
-  iflist = node.get('interfaces',[])
   err_data = []
   for intf in iflist:
     if 'ospf' not in intf or intf.type != 'svi':
@@ -27,9 +26,9 @@ class OS10(_Quirks):
 
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
-    check_vlan_ospf(node,'default')
+    check_vlan_ospf(node,node.interfaces,'default')
     for vname,vdata in node.get('vrfs',{}).items():
-      check_vlan_ospf(node,vname)
+      check_vlan_ospf(node,vdata.get('ospf.interfaces',[]),vname)
 
   def check_config_sw(self, node: Box, topology: Box) -> None:
     need_ansible_collection(node,'dellemc.os10')
