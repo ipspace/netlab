2ff7704534ba4eb0d20becc8f200923ae9e12a89
Ivan Pepelnjak <ip@ipspace.net>
2024-11-07 15:15:40 +0100

    Make quirk errors configurable
    The generic report_quirk function provides an abstraction on top of
    log.error that checks whether the specified quirk should be reported,
    giving the user means of turning off device-specific warnings.
    
    Implemented for OSPF on DellOS10 SVI as a proof-of-concept
    
    Fixes #1501

--- netsim/devices/dellos10.py
+++ netsim/devices/dellos10.py
@@ -3,11 +3,13 @@
 #
 from box import Box
 
-from . import _Quirks,need_ansible_collection
+from . import _Quirks,need_ansible_collection,report_quirk
 from ..utils import log
 from ..augment import devices
 
-def check_vlan_ospf(name: str, iflist: list, vname: str) -> None:
+def check_vlan_ospf(node: Box, vname: str) -> None:
+  name = node.name
+  iflist = node.get('interfaces',[])
   err_data = []
   for intf in iflist:
     if 'ospf' not in intf or intf.type != 'svi':
@@ -15,19 +17,19 @@ def check_vlan_ospf(name: str, iflist: list, vname: str) -> None:
     err_data.append(f'Interface {intf.ifname} VRF {vname}')
 
   if err_data:
-    log.error(
-      f'Dell OS10 (node {name}) cannot run OSPF on virtual-network (SVI) interfaces',
-      category=log.IncorrectValue,
+    report_quirk(
+      f'node {name} cannot run OSPF on virtual-network (SVI) interfaces',
+      quirk='svi_ospf',
       more_data=err_data,
-      module='ospf')
+      node=node)
 
 class OS10(_Quirks):
 
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
-    check_vlan_ospf(node.name,node.get('interfaces',[]),'default')
+    check_vlan_ospf(node,'default')
     for vname,vdata in node.get('vrfs',{}).items():
-      check_vlan_ospf(node.name,vdata.get('ospf.interfaces',[]),vname)
+      check_vlan_ospf(node,vname)
 
   def check_config_sw(self, node: Box, topology: Box) -> None:
     need_ansible_collection(node,'dellemc.os10')
