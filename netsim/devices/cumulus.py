3ffcacb7eedb8a642d5f5f44da20c65a1721d5cd
Jeroen van Bemmel <jvb127@gmail.com>
2024-11-10 20:21:01 -0600

    Much improved, port priority tests still fail

--- netsim/devices/cumulus.py
+++ netsim/devices/cumulus.py
@@ -3,7 +3,7 @@
 #
 from box import Box
 
-from . import _Quirks
+from . import _Quirks, report_quirk
 from ..utils import log
 from ..augment import devices
 
@@ -15,6 +15,25 @@ def check_ospf_vrf_default(node: Box) -> None:
         category=log.IncorrectType,
         module='quirks')
 
+def nvue_check_stp_features(node: Box, topology: Box) -> None:
+  err_data = []
+  stp_protocol = topology.get('stp.protocol','stp')
+  for i in node.interfaces:
+    if 'stp' not in i:
+      continue
+    if 'port_priority' in i.stp:  # AttributeNotImplemented("mstpctl-treeportprio") in NVUE
+      if 'vlan' not in i:
+        err_data.append(f'Non-VLAN interface {i.ifname}')
+      elif stp_protocol!='pvrst':
+        err_data.append(f'VLAN interface without PVRST {i.ifname}')
+  
+  if err_data:
+    report_quirk(
+      f'node {node.name} does not support STP port_priority on non-VLAN interfaces or without PVRST ({stp_protocol})',
+      quirk='stp_port_priority',
+      more_data=err_data,
+      node=node)
+
 class Cumulus(_Quirks):
 
   @classmethod
@@ -22,3 +41,8 @@ class Cumulus(_Quirks):
     mods = node.get('module',[])
     if 'ospf' in mods and 'vrfs' in node:
       check_ospf_vrf_default(node)
+    
+    # NVUE specific quirks
+    if node.device == "cumulus_nvue":
+      if 'stp' in mods:
+        nvue_check_stp_features(node,topology)
