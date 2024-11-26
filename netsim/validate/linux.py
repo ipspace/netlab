d7aac3ab4039e6ed7859dd9e78a1c3462dfb0894
J vanBemmel <jvb127@gmail.com>
2024-11-26 07:09:42 -0600

    Update linux.py

--- netsim/validate/linux.py
+++ netsim/validate/linux.py
@@ -19,7 +19,7 @@ def exec_ping(
   cmd = f'ping -c {count} -W 1 -A'
   if af == 'ipv6':
     cmd += ' -6'
-  else:
+  elif af == 'ipv4':
     cmd += ' -4'
   if pkt_len:
     cmd += f' -s {pkt_len}'
