32f8b7916ce4865b22e8861e8e37ca7479db6fba
Ivan Pepelnjak <ip@ipspace.net>
2024-11-28 11:53:36 +0100

    Add yamllint to local/CI-CD tests
    Also: fix all errors in existing YAML files

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -16,3 +16,12 @@ cd ..; python3 -m mypy -p netsim
 for file in netsim/extra/*/plugin.py; do
   python3 -m mypy $file
 done
+if [ `which yamllint` ]; then
+  echo
+  echo Executing yamllint in netsim and tests directories
+  yamllint --no-warnings netsim/*yml netsim/**/*yml tests/**/*yml
+else
+  echo
+  echo Install yamllint with pip3 install -r requirements-dev.txt
+  exit 1
+fi
