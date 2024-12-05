d25518c7b6586339cfe034fd5700e4212213f60d
Jeroen van Bemmel <jvb127@gmail.com>
2024-12-05 13:59:42 -0600

    * Support VLAN lists and groups too

---
+++ netsim/extra/repeater/plugin.py
@@ -0,0 +1,75 @@
+import typing
+from box import Box
+from netsim import api,data
+from netsim.utils import log,strings
+from netsim.augment import links
+
+def clone_interfaces( link_data: Box, nodename: str, clones: list ) -> list:
+
+  """
+  update_ifindex - update any custom ifindex on the given interface
+  """
+  def update_ifindex(intf:Box,c:int) -> Box:
+    ifindex = intf.get('ifindex',None)
+    if ifindex is not None:
+      intf.ifindex = ifindex + c + 1
+    return intf
+  
+  ifs = []
+  if nodename in [ i.node for i in link_data.interfaces ]:
+     link_data.pop('_linkname',None)
+     for c,clone in enumerate(clones):
+       l = data.get_box(link_data)
+       l.interfaces = [ { 'node': clone } ] + [ update_ifindex(i,c) for i in link_data.interfaces if i.node!=nodename ]
+       ifs.append(l)
+  return ifs
+
+def update_vlan_access_links(topology: Box, nodename: str, clones: list) -> None:
+  for vname,vdata in topology.vlans.items():                                # Iterate over global VLANs
+    if not isinstance(vdata,Box):                                           # VLAN not yet a dictionary?
+      continue                                                              # ... no problem, skip it
+    if not 'links' in vdata:                                                # No VLAN links?
+      continue                                                              # ... no problem, move on
+
+    for cnt,l in enumerate(vdata.links):                                    # So far so good, now iterate over the links
+      link_data = links.adjust_link_object(                                 # Create link data from link definition
+                    l=l,
+                    linkname=f'vlans.{vname}.links[{cnt+1}]',
+                    nodes=topology.nodes)
+      if link_data is not None:
+        ifs = clone_interfaces(link_data,nodename,clones)
+        vdata.links += ifs
+
+def repeat_node( node: Box, topology: Box ) -> None:
+  count = node.pop('repeat',None)
+  clones = []
+  for c in range(2,count+2):      # Existing node is '1'
+    clone = data.get_box(node)
+    clone.name = strings.eval_format(topology.defaults.repeater.node_name_pattern, node + { 'id': c } )
+    clone.interfaces = []         # Start clean, remove reference to original node
+    if 'id' in node:
+      clone.id = node.id + c      # Update any explicit node ID sequentially
+    topology.nodes += { clone.name: clone }
+    clones.append( clone.name )
+
+  for link in list(topology.get('links',[])):
+    link_data = links.adjust_link_object(link,f'repeater{c}',topology.nodes)        
+    if 'lag' in link_data:
+      log.error("LAG links not yet supported by repeater plugin, ignoring...",
+                category=Warning, module='repeater')
+      continue  
+    ifs = clone_interfaces(link_data,node.name,clones)
+    topology.links += ifs
+
+  if 'groups' in topology:
+    for group in topology.groups.values():
+      if node.name in group.members:
+        group.members.extend( clones )
+
+  if 'vlans' in topology:
+    update_vlan_access_links(topology,node.name,clones)
+
+def topology_expand(topology: Box) -> None:
+  for node in list(topology.nodes.values()):
+    if 'repeat' in node:
+      repeat_node( node, topology )
