8cec816d2c13fc4e3f5df4cf103e17a89ae9a30c
J vanBemmel <jvb127@gmail.com>
2024-12-11 20:18:09 -0600

    bonding: Restore p2p link

--- netsim/extra/bonding/plugin.py
+++ netsim/extra/bonding/plugin.py
@@ -9,27 +9,22 @@ _config_name = 'bonding'
 '''
 add_bond_interfaces - append interface data to node.interfaces for bonding template to read and implement
 '''
-def add_bond_interfaces(node: Box, bonds: dict[int,list[Box]]) -> None:
+def add_bond_interfaces(node: Box, bonds: dict[int,Box]) -> None:
   last_linkindex = node.interfaces[-1].linkindex
-  for c,(ifindex,members) in enumerate(bonds.items()):
+  for c,(ifindex,bond) in enumerate(bonds.items()):
     bond_if = {
       'linkindex': last_linkindex + 1 + c,
       'type': 'bond',
       'ifname': f'bond{ifindex}',  # XXX hardcoded, could make this a device template attribute
       'name': f'bond {ifindex}',
-      'bonding': { 'ifindex': ifindex, 'members': [ m.ifname for m in members ] },
+      'bonding': { 'ifindex': ifindex, 'members': [ m.ifname for m in bond['members'] ] },
+      'interfaces': bond['interfaces'],
       'ifindex': 50000 + c,
       'virtual_interface': True
     }
-    c = c + 1
-    vlans = [ m.vlan for m in members if 'vlan' in m ]
-    if vlans:
-      bond_if.vlan = vlans[0]   # Take the first VLAN, hopefully consistent
-    else:
-      for af in ['ipv4','ipv6']:
-        ips = [ m[af] for m in members if af in m ]
-        if ips:
-          bond_if[af] = ips[0]  # Take the first one, if any
+    for af in ['ipv4','ipv6']:
+      if af in bond:
+        bond_if[af] = bond[af]     # Take the first one, if any
     node.interfaces.append(bond_if)
 
 '''
@@ -42,26 +37,30 @@ def post_transform(topology: Box) -> None:
   for node in topology.nodes.values():
     features = devices.get_device_features(node,topology.defaults)
     if 'bonding' in features:
-      bonds : dict[int,list[Box]] = {}
+      bonds : dict[int,Box] = {}
       for intf in node.get('interfaces',[]):
         bond_ifindex = intf.get('bonding.ifindex',None)
         if not bond_ifindex:
           continue
 
-        if 'virtual_interface' in intf or len(intf.get('neighbors',[]))!=1:
-          log.error( f"{intf.name}: 'bonding.ifindex' can only be applied to interfaces on physical p2p links",
+        link = topology.links[ intf.linkindex-1 ]
+        if 'virtual_interface' in intf or link.node_count!=2:
+          log.error( f"{intf.name}: 'bonding.ifindex' can only be applied to interfaces on direct p2p links",
                      category=log.IncorrectAttr,module=_config_name)
           continue
 
         clone = data.get_box(intf)
         if bond_ifindex in bonds:
-          bonds[ bond_ifindex ].append( clone )
+          bonds[bond_ifindex]['members'].append( clone )
         else:
-          bonds[ bond_ifindex ] = [ clone ]
+          bonds[bond_ifindex] = { 'interfaces': intf.interfaces, 'members': [ clone ] }
+          for att in ['ipv4','ipv6']:    # Move any ips
+            if att in intf:
+              bonds[bond_ifindex][att] = intf.pop(att,None)
 
-        for att in ['ipv4','ipv6','vlan']:      # Remove any ips or vlan
-          intf.pop(att,None)
-        intf.prefix = False                     # L2 interface
+        intf.neighbors = [ { 'ifname': i.ifname, 'node': i.node } for i in link.interfaces if i.node!=node.name ]
+        intf.type = 'p2p'
+        intf.prefix = False              # L2 p2p interface
 
       if bonds:
         add_bond_interfaces(node,bonds)
