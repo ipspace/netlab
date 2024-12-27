c53d3ffab749d92c7df594ce028c6f005dcc8191
Stefano Sasso <stefano.sasso@hpe.com>
2024-12-27 13:59:02 +0100

    AOS-CX: support for LAG and MCLAG

--- netsim/devices/arubacx.py
+++ netsim/devices/arubacx.py
@@ -47,6 +47,37 @@ class ARUBACX(_Quirks):
           f'ArubaCX MPLS data plane works only on physical devices (using the external provider)',
           log.IncorrectType,
           'quirks')
+    
+    # LAG + VSX quirks
+    ## on VSX, you **must** configure the switch role as primary or secondary.
+    ### The roles do not indicate which device is forwarding traffic at a given time as VSX is an active-active forwarding solution.
+    ### The roles are used to determine which device stays active when there is a VSX split.
+    ### -- HERE we will assume the first device found for a specific peer-link is the primary, and the second one is the secondary.
+    if 'lag' in mods:
+      # find my MC-LAG
+      for intf in node.get('interfaces', []):
+        pgroup = intf.get('lag', {}).get('mlag', {}).get('peergroup', False)
+        if not pgroup:
+          continue
+        log.info(f'ArubaCX VSX: Found MLAG ID {pgroup} on node {node.name}')
+        # search for another ArubaCX peer with same MLAG ID and _vsx_role set to 'primary'
+        for n,nattrs in topology.nodes.items():
+          if n == node.name:
+            continue
+          if nattrs.device != 'arubacx':
+            continue
+          for n_intf in nattrs.get('interfaces', []):
+            n_pgroup = n_intf.get('lag', {}).get('mlag', {}).get('peergroup', False)
+            if not n_pgroup:
+              continue
+            if n_pgroup == pgroup:
+              # I have found my peer. Verify if it's the primary.
+              _vsx_role = n_intf.get('lag', {}).get('mlag', {}).get('_vsx_role', '')
+              my_role = 'primary'
+              if _vsx_role == 'primary':
+                my_role = 'secondary'
+              log.info(f'  - MLAG Peer is node {n} (ID {pgroup}) - which has role: "{_vsx_role}" - setting my role to "{my_role}"')
+              intf.lag.mlag._vsx_role = my_role
 
   def check_config_sw(self, node: Box, topology: Box) -> None:
     need_ansible_collection(node,'arubanetworks.aoscx')
