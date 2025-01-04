c4fb94c39fb5a09b5f790c46e21a264ba4567e33
Jeroen van Bemmel <jvb127@gmail.com>
2025-01-04 06:20:41 -0600

    * Fix per-VLAN bridge priority configuration * FRR tagged STP BDPUs are incompatible with peers that run PVRST and expect untagged STP

--- netsim/devices/frr.py
+++ netsim/devices/frr.py
@@ -8,21 +8,21 @@ from ..utils import log
 
 """
 Because FRR uses a 'bridge per VLAN' model, STP BPDUs are sent as tagged packets, not untagged.
-This makes FRR incompatible with basic STP in a VLAN trunking scenario
+This makes FRR incompatible with STP in a VLAN trunking scenario, as its peers expect standard 
+STP BPDUs to be sent untagged
 """
 def check_stp_on_trunks(node: Box, topology: Box) -> None:
-  if node.get('stp.protocol',"stp")=="stp":     # Are we being asked to run standard STP with untagged BPDUs?
-    err_data = []
-    for intf in node.interfaces:
-      if intf.type=='vlan_member':
-        err_data.append(f'Interface {intf.ifname}')
+  err_data = []
+  for intf in node.interfaces:
+    if intf.type=='vlan_member':
+      err_data.append(f'Interface {intf.ifname}')
 
-    if err_data:
-      report_quirk(
-        f'FRR node {node.name} runs STP per VLAN, it cannot run regular untagged STP on VLAN trunks',
-        quirk='stp_on_trunks',
-        more_data=err_data,
-        node=node)
+  if err_data:
+    report_quirk(
+      f'FRR node {node.name} runs STP per VLAN, it sends tagged standard STP BDPUs which is incompatible',
+      quirk='stp_on_trunks',
+      more_data=err_data,
+      node=node)
 
 class FRR(_Quirks):
 
