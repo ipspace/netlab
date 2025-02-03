e7a894aeb8fb19b951f25c83d9f69ecf8d857dcd
Jeroen van Bemmel <jvb127@gmail.com>
2025-02-03 00:38:36 -0600

    Fix test 63 - this one requires the native interface to be a subinterface

--- netsim/devices/cumulus_nvue.py
+++ netsim/devices/cumulus_nvue.py
@@ -60,27 +60,6 @@ def nvue_check_ospfv3(node: Box) -> None:
       hint='ospfv3')   
 
 """
-Checks whether this node uses OSPF passive interfaces inside a vrf. NVUE does not support these correctly, because the scripts
-at /usr/lib/python3/dist-packages/nos/funits/cue_frr_v1/templates/frr_ospf_intf.conf.j2 use 'passive-interface' instead of
-'ip ospf passive' at the interface level (which is VRF aware)
-"""
-def nvue_check_ospf_passive_in_vrf(node: Box) -> None:
-  err_data = []
-  for vrf,vdata in node.get('vrfs',{}).items():
-    for i in vdata.get('ospf.interfaces',[]):
-      if 'passive' in i.ospf and i.ospf.passive is True:
-        err_data.append(f'Interface {i.ifname} inside VRF {i.vrf} using ospf.passive=True')
-        i.ospf.passive = False
-
-  if err_data:
-    report_quirk(
-      f'Node {node.name} uses passive OSPF interfaces inside a VRF, which NVUE does not support; auto-converted to False',
-      quirk='vrf_ospf_passive',
-      category=Warning,
-      more_data=err_data,
-      node=node)
-
-"""
 In case of multiple loopbacks, merges OSPF settings into 1 if compatible; else throws an error
 """
 def nvue_merge_ospf_loopbacks(node: Box) -> None:
@@ -139,31 +118,6 @@ def nvue_check_ospf_passive_in_vrf(node: Box) -> None:
       more_data=err_data,
       node=node)
 
-"""
-In case of multiple loopbacks, merges OSPF settings into 1 if compatible; else throws an error
-"""
-def nvue_merge_ospf_loopbacks(node: Box) -> None:
-  err_data = []
-  for i in node.get('interfaces',[]):
-    if i.type!='loopback' or 'ospf' not in i:
-      continue
-    if 'ospf' in node.loopback and 'vrf' not in i:
-      if i.ospf == node.loopback.ospf or (i.ospf == node.loopback.ospf+{'passive': False}):
-        i.pop('ospf',None)
-        report_quirk(
-          f'Node {node.name} uses a secondary loopback with OSPF { i.ifname }, merged with primary loopback',
-          quirk='loopback_merge_ospf',
-          category=Warning,
-          node=node)
-        continue
-    err_data.append(f'Secondary loopback {i.ifname}')
-
-  if err_data:
-    report_quirk(
-      f'Node {node.name} uses secondary loopback(s) with OSPF configuration that differs from the primary loopback, or is in a VRF',
-      quirk='secondary_loopback_ospf',
-      more_data=err_data,
-      node=node)
 
 class Cumulus_Nvue(_Quirks):
 
