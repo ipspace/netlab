eeb0ffb4e0073f2e26d48109c27df0d5826adaac
Jeroen van Bemmel <jvb127@gmail.com>
2025-02-06 10:26:09 -0600

    Refactored to create a separate routed sub_interface (vlan_member) for the native vlan on mixed trunks

--- netsim/devices/cumulus_nvue.py
+++ netsim/devices/cumulus_nvue.py
@@ -8,6 +8,7 @@ from . import _Quirks, report_quirk
 from .cumulus import check_ospf_vrf_default
 from ..utils import log
 from ..augment import devices
+from .. import data
 
 def nvue_check_stp_features(node: Box, topology: Box) -> None:
   err_data = []
@@ -60,6 +61,60 @@ def nvue_check_ospfv3(node: Box) -> None:
       hint='ospfv3')
 
 """
+Checks for mixed trunk interfaces with native vlan, and creates a separate subinterface (vlan_member) for the native VLAN.
+This is needed because the parent interface gets added to the VLAN-aware bridge, with the native vlan set as 'untagged'
+"""
+def nvue_create_native_subifs(node: Box, topology: Box) -> None:
+  for i in list(node.interfaces):
+    if '_vlan_native' not in i:
+      continue
+    native_vlan = topology.vlans[ i._vlan_native ]
+    for j in list(node.interfaces):
+      if j.get('parent_ifindex',None)!=i.ifindex:
+        continue
+      if j.get('vlan.mode','irb') in ['bridge','irb']:               # Are we dealing with a mixed trunk?
+        native_subif = data.get_empty_box()
+        native_subif.ifname = f'{i.ifname}.{ native_vlan.id }'
+        native_subif.name = f'[SubIf native VLAN {i._vlan_native}] ' + i.name
+        native_subif.parent_ifindex = i.ifindex
+        native_subif.parent_ifname = i.ifname
+        native_subif.type = "vlan_member"
+        native_subif.virtual_interface = True
+        native_subif.ifindex = len(node.interfaces) + 1
+        native_subif.vlan.name = i._vlan_native
+        native_subif.vlan.access_id = native_vlan.id
+        native_subif.vlan.mode = 'route'
+
+        skip = [ 'bridge_group','subif_index','linkindex' ] + list(native_subif.keys())
+        for att,value in { k:v for k,v in i.items() if k not in skip }.items():
+          native_subif[att] = value
+          i.pop(att,None)
+
+        node.interfaces.append(native_subif)
+        break
+
+"""
+Checks whether this node uses OSPF passive interfaces inside a vrf. NVUE does not support these correctly, because the scripts
+at /usr/lib/python3/dist-packages/nos/funits/cue_frr_v1/templates/frr_ospf_intf.conf.j2 use 'passive-interface' instead of
+'ip ospf passive' at the interface level (which is VRF aware)
+"""
+def nvue_check_ospf_passive_in_vrf(node: Box) -> None:
+  err_data = []
+  for vrf,vdata in node.get('vrfs',{}).items():
+    for i in vdata.get('ospf.interfaces',[]):
+      if 'passive' in i.ospf and i.ospf.passive is True:
+        err_data.append(f'Interface {i.ifname} inside VRF {i.vrf} using ospf.passive=True')
+        i.ospf.passive = False
+
+  if err_data:
+    report_quirk(
+      f'Node {node.name} uses passive OSPF interfaces inside a VRF, which NVUE does not support; auto-converted to False',
+      quirk='vrf_ospf_passive',
+      category=Warning,
+      more_data=err_data,
+      node=node)
+
+"""
 In case of multiple loopbacks, merges OSPF settings into 1 if compatible; else throws an error
 """
 def nvue_merge_ospf_loopbacks(node: Box) -> None:
@@ -97,28 +152,6 @@ def mark_shared_mlag_vtep(node: Box, topology: Box) -> None:
       node.vxlan._shared_vtep = n.name
       return
 
-"""
-Checks whether this node uses OSPF passive interfaces inside a vrf. NVUE does not support these correctly, because the scripts
-at /usr/lib/python3/dist-packages/nos/funits/cue_frr_v1/templates/frr_ospf_intf.conf.j2 use 'passive-interface' instead of
-'ip ospf passive' at the interface level (which is VRF aware)
-"""
-def nvue_check_ospf_passive_in_vrf(node: Box) -> None:
-  err_data = []
-  for vrf,vdata in node.get('vrfs',{}).items():
-    for i in vdata.get('ospf.interfaces',[]):
-      if 'passive' in i.ospf and i.ospf.passive is True:
-        err_data.append(f'Interface {i.ifname} inside VRF {i.vrf} using ospf.passive=True')
-        i.ospf.passive = False
-
-  if err_data:
-    report_quirk(
-      f'Node {node.name} uses passive OSPF interfaces inside a VRF, which NVUE does not support; auto-converted to False',
-      quirk='vrf_ospf_passive',
-      category=Warning,
-      more_data=err_data,
-      node=node)
-
-
 class Cumulus_Nvue(_Quirks):
 
   @classmethod
@@ -141,6 +174,7 @@ class Cumulus_Nvue(_Quirks):
 
     if 'vxlan' in mods:
       mark_shared_mlag_vtep(node,topology)
+    nvue_create_native_subifs(node,topology)
 
     if devices.get_provider(node,topology) == 'clab':
       log.error(
