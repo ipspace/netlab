8e15c4bc21f2b6205d723102f5074453c933acdb
Jeroen van Bemmel <jvb127@gmail.com>
2025-02-10 19:38:11 -0600

    Set an '_mlag: True' flag on the VTEP loopback, as hint to further scripting

--- netsim/extra/mlag.vtep/plugin.py
+++ netsim/extra/mlag.vtep/plugin.py
@@ -36,9 +36,10 @@ def pre_link_transform(topology: Box) -> None:
         continue                                    # Skip nodes for which the plugin is disabled
       if 'vxlan' in node.module:
           vtep_loopback = data.get_empty_box()
-          vtep_loopback.type = 'loopback'
+          vtep_loopback.type = 'loopback'           # Assign same static IP to both nodes
           vtep_loopback.interfaces = [ { 'node': node_name, 'ipv4': str(vtep_a.network)+"/32" } ]
           vtep_loopback._linkname = f"MLAG VTEP VXLAN interface shared between {' - '.join(peers)}"
           vtep_loopback.vxlan.vtep = True
+          vtep_loopback.vxlan._mlag = True
           vtep_loopback.linkindex = len(topology.links)+1
           topology.links.append(vtep_loopback)
