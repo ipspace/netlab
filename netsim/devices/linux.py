e40b0c522a12b94b6bf0ca244cfce8c3cd3be598
Ivan Pepelnjak <ip@ipspace.net>
2025-04-02 16:52:53 +0200

    VLAN support on Linux containers (#2103)
    * VLAN support on Linux containers
    
    Features:
    * VLAN access and trunk links
    * SVI (IRB) interfaces
    * Works on containers (with 'vanilla' configuration) only
    
    Does not work:
    * IRB on containers (Linux cannot be a router)
    * The weird VLAN 1 trunk scenario
    
    * Move Linux static route configuration into 'routing' config template
    
    Also: remove the "we don't need routing module for static routes"
    quirk from Linux

--- netsim/devices/linux.py
+++ netsim/devices/linux.py
@@ -8,40 +8,31 @@ from ._common import check_indirect_static_routes
 from ..utils import log
 from ..augment import devices
 
-"""
-The "routing" module is used to configure static routes pointing to the default
-gateway. Linux configuration creates static routes as part of the initial
-configuration template, so it makes no sense to keep the "routing" module active
-on a Linux host if it's only used for static routes.
-"""
-def check_routing_module(node: Box) -> None:
-  if 'routing' not in node.get('module',[]):      # The node is not using the routing module, nothing to do
-    return
+def check_clab_modules(node: Box, topology: Box) -> None:
+  if 'dhcp' in node.get('module',[]):
+    report_quirk(
+      text=f"netlab does not support DHCP functionality in Linux containers (node {node.name})",
+      node=node,
+      more_hints=[ "Use 'cumulus' for DHCP client or 'dnsmasq' for DHCP server" ],
+      category=log.IncorrectType,
+      quirk='clab_dhcp_client')
 
-  if 'routing' not in node:                       # No routing data in the node
-    return
-  
-  if 'routing' in node and list(node.routing.keys()) != [ 'static' ]:
-    return                                        # There's other routing information, not just static routes
-
-  node.module.remove('routing')                   # Remove the routing module from the node
-  if not node.module:
-    node.pop('module',None)
+def check_vm_modules(node: Box, topology: Box) -> None:
+  if 'vlan' in node.get('module',[]):
+    report_quirk(
+      text=f"netlab does not support VLANs in Linux virtual machines (node {node.name})",
+      node=node,
+      more_hints=[ "Use 'frr' device instead" ],
+      category=log.IncorrectType,
+      quirk='vm_vlan')
 
 class Linux(_Quirks):
 
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
     check_indirect_static_routes(node)
-    check_routing_module(node)
 
-    if devices.get_provider(node,topology) != 'clab':
-      return
-
-    if 'dhcp' in node.get('module',[]):
-      report_quirk(
-        text=f"netlab does not support DHCP functionality in Linux containers (node {node.name})",
-        node=node,
-        more_hints=[ "Use 'cumulus' for DHCP client or 'dnsmasq' for DHCP server" ],
-        category=log.IncorrectType,
-        quirk='clab_dhcp_client')
+    if devices.get_provider(node,topology) == 'clab':
+      check_clab_modules(node,topology)
+    else:
+      check_vm_modules(node,topology)
