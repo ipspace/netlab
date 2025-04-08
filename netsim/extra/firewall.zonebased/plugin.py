09838e2f00a2b4f401009cfeaa01936de1e0614a
Ivan Pepelnjak <ip@ipspace.net>
2025-04-08 15:48:00 +0200

    Added "none" device, streamlined the code a bit
    Also: reduced clutter generated when using an unsupported device

--- netsim/extra/firewall.zonebased/plugin.py
+++ netsim/extra/firewall.zonebased/plugin.py
@@ -14,24 +14,29 @@ def post_transform(topology: Box) -> None:
     for node in topology.nodes.values():
         features = devices.get_device_features(node,topology.defaults)
         zbf_supported = 'firewall.zonebased' in features
+        OK = True
+
         for intf in node.get('interfaces',[]):
-            # if link has a firewall zone, apply extra config to this node
             fw_zone = intf.get('firewall.zone', '')
-            if not zbf_supported and fw_zone:
+            if not fw_zone:                                     # No zone defined on the interface, move on
+                continue
+            if not zbf_supported:                               # Not supported, print error and move on
                 log.error( f"Node {node.name}({node.device}) does not support 'firewall.zone' used on {intf.name}",
                         category=log.IncorrectAttr,module=_config_name)
-            elif fw_zone:
-                # add zone to list of zones
-                node.firewall.zonebased._zones[fw_zone] = {}
-                # add config template
-                api.node_config(node,_config_name)
+                OK = False
+                continue
+
+            node.firewall.zonebased._zones[fw_zone] = {}        # Add zone to the list of zones
+            api.node_config(node,_config_name)                  # ... and remember we have to do extra config
+
+        if not OK:                                              # Errors? Move on
+            continue
+
         # now check that zones defined in rules are defined
-        rule_idx = 0
-        for def_rule in node.get('firewall.zonebased.default_rules', []):
+        for rule_idx,def_rule in enumerate(node.get('firewall.zonebased.default_rules', [])):
             for z_direction in [ 'from_zone', 'to_zone' ]:
                 z = def_rule[z_direction]
                 if z not in node.firewall.zonebased._zones:
-                    log.error( f"Node {node.name} does not have zone '{z}' defined in default zone rule '{rule_idx}' direction '{z_direction}'",
+                    log.error( f"Node {node.name} does not have zone '{z}' used in firewall.zonebased.default_rules[{rule_idx}].{z_direction}",
                             category=log.IncorrectAttr,module=_config_name)
-            rule_idx = rule_idx + 1
     return
