467e96b6866d3c2ad6236e57764984195112c958
J vanBemmel <jvb127@gmail.com>
2025-05-06 11:33:51 -0500

    SRv6: Reworked, adding FRR support with IS-IS and BGP L3VPN (#2224)

--- docs/module/srv6.md
+++ docs/module/srv6.md
@@ -1,57 +1,66 @@
 # Segment Routing over IPv6 (srv6) Configuration Module
 
-This configuration module configures SRv6 over IS-IS and (optionally) BGP on Nokia SR OS.
+This configuration module configures SRv6 over IS-IS with IBGP to advertise SID reachability
 
 Supported SRv6 features:
 
 * End SIDs (nodes) and End-x SIDs (links), all statically allocated
 * IS-IS routing protocol with SRv6 TLVs
-* Dynamic BGP Service SIDs
+* BGP L3VPN v4/v6 over SRv6
 
-The module currently depends on IS-IS module and will trigger a configuration error if the **isis** module is not enabled in the network topology.
+The module currently depends on IS-IS and will trigger a configuration error if the **isis** module is not enabled in the network topology. It is initially focused on the L3VPN use case; IPv4 islands over SRv6 or IPv6 transport between SRv6 endpoints will be added once FRRouting supports them.
 
-## Parameters
+## Platform Support
+The following table describes the per-platform support of individual router-level SRv6 features:
 
-* Locator: an IPv6 address range to allocate to SRv6 nodes; each node is assigned a unique /64 prefix from this range
+| Operating system         | IS-IS | OSPFv3 | BGP v4/v6 | Transit only |
+| ------------------------ |:-----:|:------:|:---------:|:-------------:
+| FRR                      |   ✅  |   ❌   |    ❌     |      ❌      | 
+| Nokia SR OS              |   ✅  |   ❌   |    ✅     |      ✅      |
 
-## Example
+(srv6-l3vpn-supported-platforms)=
+### BGP/SRv6 L3VPN
 
-We want to create a four-router SRv6 network.
+| Operating system      | VPNv4 | VPNv6 |
+| ----------------------| :---: | :---: |
+| FRR                   |   ✅  |   ✅ [❗️](caveats-frr)  |
+| Nokia SR OS           |   ❌  |   ❌  |
 
-All devices run IS-IS and SRv6:
-```
-module: [ isis, srv6 ]
-```
+**Notes**
+* VPNv4 and VPNv6 address families are enabled on IPv6 IBGP sessions
 
-We want to use unnumbered P2P interfaces and dual-stack deployment with IPv6 configured on loopback addresses:
+## Configurable Global and Node Parameters
 
-```
-addressing:
-  p2p:
-    unnumbered: true
-  loopback:
-    ipv6: 2001:db8:cafe::/48
+* **addressing.srv6_locator** -- global address pool[^poolname] for allocation of SRv6 locator prefixes, the default prefix is defined in `topology.defaults.srv6.locator_pool` (5F00::/16, the IANA reserved range defined by [RFC9602](https://datatracker.ietf.org/doc/rfc9602/)
+* **srv6.allocate_loopback** -- global flag (default: `True`) to replace the IPv6 loopback address of each SRv6-enabled node with an IP allocated from the locator range
+* **srv6.bgp** -- enable BGP with IPv4 and IPv6 address families over SRv6, default IPv4 + IPv6 over iBGP.
+* **srv6.vpn** -- enable BGP with VPNv4 and VPNv6 address families over SRv6. BGP/SRv6 L3VPN is disabled by default.
+* **srv6.igp** -- list of IGP protocols for which to enable SRv6, default `[isis]`
 
-srv6.locator: 2001:db8:aaaa::/48  # Must not overlap with interfaces
-```
+[^poolname]: You can change the name of the default SRv6 locator pool with the `topology.defaults.const.srv6.locator_pool.name` parameter
 
-```
-provider: clab
-defaults.device: sros # Type must support FP4 chipset
+(srv6-node-parameters)=
+## Node Parameters
 
-nodes:
-  e1:
-  e2:
-  c1:
-  c2:
-```
+* **srv6.locator**: an optional IPv6 address prefix to allocate to a given SRv6 node; by default, each node is assigned a unique /48 prefix from the global pool
+* **srv6.transit_only**: an optional Boolean flag to optimize resource usage and only allocate transit behaviors, not endpoint behaviors
 
-The devices are connected into a square topology:
+## Configurable BGP/SRv6 IPv4/6 and L3VPN Parameters
 
-```
-links:
-- e1-c1
-- e1-c2
-- e2-c1
-- e2-c2
-```
+This module provides 2 parameters that are identical in structure, controlling different BGP address families:
+
+* **srv6.bgp** -- Controls IPv4 and IPv6 address families
+* **srv6.vpn** -- Controls VPNv4 and VPNv6 address families
+
+Each parameter could be a boolean (*True* to enable both IP address families on IBGP sessions), or a dictionary of parameters:
+
+* **ipv4** -- enable IPv4/VPNv4 address family
+* **ipv6** -- enable IPv6/VPNv6 address family
+
+**ipv4** and **ipv6** parameters could be:
+
+* Boolean value *False* to disable the address family within node data (overwriting global defaults)
+* Boolean value *True* to enable the address family on IBGP sessions
+* A string or a list of *ibgp/ebgp* keywords
+
+For a tested example, see the [IPv4 L3VPN over SRv6 topology](https://github.com/ipspace/netlab/tree/dev/tests/integration/srv6/02-isis-ipv4-bgp-vpn.yml).
--- netsim/modules/srv6.py
+++ netsim/modules/srv6.py
@@ -1,29 +1,127 @@
 #
 # SRv6 transformation module
 #
+import typing
+
 from box import Box
+from ..augment import addressing,devices
 
 from . import _Module
 from ..utils import log
+from .. import data
 import ipaddress
+from ..data.global_vars import get_const
+
+# Defaults used for both srv6.bgp and srv6.vpn
+DEFAULT_BGP_AF: typing.Final[dict] = {
+  'ipv4': [ 'ibgp' ],
+  'ipv6': [ 'ibgp' ]
+}
+
+"""
+Returns the name for the SRv6 locator address pool, default 'srv6_locator'
+"""
+def get_pool_name() -> str:
+  return get_const('srv6.locator_pool.name','srv6_locator')
+
+"""
+Configures BGP address families for neighbors, including extended nexthop where needed
+"""
+def configure_bgp_for_srv6(node: Box, topology: Box) -> None:
+  srv6_bgp = node.get('srv6.bgp',{})
+  srv6_vpn = node.get('srv6.vpn',{})
+  for nb in list(node.get('bgp.neighbors',[])):
+    if 'ipv6' not in nb:                               # Skip IPv4-only neighbors
+      continue
+
+    for af in DEFAULT_BGP_AF.keys():
+      if nb.type in srv6_bgp.get(af,[]) or nb.type in srv6_vpn.get(af,[]):
+# If anything, deactivating BGP AFs would break SR-OS and not achieve anything on FRR
+#        nb.activate[af] = False                        # Disable regular BGP activation
+        pass
+      else:
+        continue                                       # Skip if neither AF is activated
+
+# The following code is untested and thus commented out
+#      if nb.type=='ebgp':                              # Set next hop unchanged for EBGP peers, to get end-2-end SID routing
+#        nb.next_hop_unchanged = True
+      if af=='ipv4' and 'ipv4' not in nb:
+        nb.extended_nexthop = True                     # Enable extended next hops when IPv4 AF is used without IPv4 transport
 
 class SRV6(_Module):
-
-  def node_post_transform(self, node: Box, topology: Box) -> None:
-
-      # Could model this as another addressing pool too
-      locator = f'{topology.defaults.srv6.locator}:{node.id:x}::/64'
-      locator_net = ipaddress.IPv6Network(locator)
-
-      if 'ipv6' not in node.loopback:
-        log.error(
-          f"SRv6 requires an ipv6 loopback address on node {node.name}",
+  """
+  module_pre_default - create the default SRv6 locator address pool
+  """
+  def module_pre_default(self, topology: Box) -> None:
+    # Defining this as _top addressing includes it in *every* topology
+    POOL_NAME = get_pool_name()
+    if POOL_NAME not in topology.defaults.addressing:
+      topology.defaults.addressing[ POOL_NAME ] = {
+        'ipv6': topology.defaults.srv6.locator_pool,
+        'prefix6': 48
+      }
+    elif 'ipv6' not in topology.defaults.addressing[ POOL_NAME ]:
+      log.error(
+          f"Custom SRv6 addressing pool '{POOL_NAME}' must provide IPv6 prefixes",
           category=log.MissingValue,
           module='srv6')
-      elif ipaddress.IPv6Interface(node.loopback.ipv6).network.subnet_of(locator_net):
+
+  def node_pre_transform(self, node: Box, topology: Box) -> None:
+    mods = node.get('module',[])
+    d_features = devices.get_device_features(node,topology.defaults)
+    for igp in node.get('srv6.igp',[]):
+      if igp not in mods:
         log.error(
-          f"Node {node.name} ipv6 loopback address {node.loopback.ipv6} overlaps with locator {locator}",
+          f"Node {node.name} does not have the {igp} IGP module enabled to run SRv6",
+          category=log.MissingDependency,
+          module='srv6')
+      if not d_features.srv6.get(igp):
+        log.error(
+          f"Node {node.name} (device {node.device}) does not support {igp} as IGP for SRv6",
           category=log.IncorrectValue,
           module='srv6')
 
-      node.srv6.locator = locator
+    data.bool_to_defaults(node.srv6,'bgp',DEFAULT_BGP_AF)
+    if node.srv6.get('bgp'):
+      if not d_features.srv6.get('bgp'):
+        log.error(
+          f"Node {node.name} (device {node.device}) does not support BGP v4/v6 with SRv6",
+          category=log.IncorrectValue,
+          module='srv6')
+      if 'bgp' not in mods:
+        log.error(
+          f"Node {node.name} does not have the BGP module enabled to support BGP v4/v6",
+          category=log.MissingDependency,
+          module='srv6')
+    data.bool_to_defaults(node.srv6,'vpn',DEFAULT_BGP_AF)    # Typically used with the vrf module, but not only
+    if node.srv6.get('vpn') and not d_features.srv6.get('vpn'):
+      log.error(
+        f"Node {node.name} (device {node.device}) does not support L3VPN BGP v4/v6 with SRv6",
+        category=log.IncorrectValue,
+        module='srv6')
+
+    locator = node.get('srv6.locator')
+    if not locator:
+       prefix = addressing.get(topology.pools,[get_pool_name()])['ipv6']
+       locator = str(prefix)
+       node.srv6.locator = locator
+    locator_net = ipaddress.IPv6Network(locator)
+    if node.get('srv6.allocate_loopback'):                   # Auto-assign a loopback from locator range
+      first_host = next(locator_net.hosts())                 # Use first usable address
+      prefix6 = topology.pools['loopback'].get('prefix6',64) # Use loopback.prefix6, default /64
+      node.loopback.ipv6 = ipaddress.IPv6Interface((first_host, prefix6)).with_prefixlen
+
+  def node_post_transform(self, node: Box, topology: Box) -> None:
+    if 'ipv6' not in node.loopback:
+        log.error(
+          f"Node {node.name} does not have an IPv6 loopback required for SRv6, and auto-allocation is disabled",
+          category=log.MissingValue,
+          module='srv6')
+    mods = node.get('module',[])
+    for igp in node.get('srv6.igp',[]):                      # Check if the IGP module is still active, it may have been removed
+      if igp not in mods:
+        log.warning(
+          text=f"The IGP module for {igp} on node {node.name} has been removed, SRv6 will likely not work",
+          module='srv6')
+    if 'bgp' in node:
+      configure_bgp_for_srv6(node,topology)
