f4969ce25b81987307701e19a7b624d24a375d0c
Jeroen van Bemmel <jvb127@gmail.com>
2025-06-10 23:56:06 -0500

    - add 'range' (only applies to 'normal' area kind) - add 'normal' kind

--- netsim/extra/ospf.stub/plugin.py
+++ netsim/extra/ospf.stub/plugin.py
@@ -7,6 +7,10 @@ from netsim.utils import log
 _config_name = "ospf.stub"
 _require = [ "ospf" ]
 
+def validate_area( id: int, area: Box ) -> bool:
+  if area.get('kind','normal') != 'normal' and 'range' in area:
+    log.error(f"OSPF area {id} is of type {area.kind} and cannot support 'range' summarization")
+
 def post_transform(topology: Box) -> None:
   for ndata in topology.nodes.values():
     if not 'ospf' in ndata.get('module',[]):
@@ -18,8 +22,11 @@ def post_transform(topology: Box) -> None:
     if 'stub' not in features.ospf:
       log.error(f"Node {ndata.name} (device {ndata.device}) not supported by the ospf.stub plugin")
       continue
-    if ndata.get('ospf.area','0.0.0.0') != '0.0.0.0':  # Check if node is an ABR
+    for k,v in ospf_areas.items():
+      validate_area(k,v)
+    if ndata.get('ospf.area','0.0.0.0') != '0.0.0.0':  # Check if node is not an ABR
       for _,area in ospf_areas.items():
-        area.pop('no_summary',None)
+        for att in [ 'no_summary', 'range' ]:          # Only applied at ABR
+          area.pop(att,None)
     global _config_name
     api.node_config(ndata,_config_name)
