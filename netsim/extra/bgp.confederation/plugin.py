8e9248b2a8a59389e45519d8647aa2b4540495cf
Jeroen van Bemmel <jvb127@gmail.com>
2025-06-30 18:26:42 -0500

    Use new bgp structures

--- netsim/extra/bgp.confederation/plugin.py
+++ netsim/extra/bgp.confederation/plugin.py
@@ -7,6 +7,18 @@ from netsim.augment import devices
 _config_name = 'bgp.confederation'
 _requires    = [ 'bgp' ]
 
+CONFED_EBGP = 'confed_ebgp'
+
+def init(topology: Box) -> None:
+  topology.defaults.attributes.bgp_session_type.valid_values[ CONFED_EBGP ] = None
+  topology.defaults.bgp.attributes._inherit_community[ CONFED_EBGP ] = 'ibgp'
+
+  for af in log.AF_LIST:
+    if CONFED_EBGP in topology.get(f'bgp.sessions.{af}',{}):
+      log.error( f"Cannot use {CONFED_EBGP} in bgp.sessions", category=Warning )
+    if CONFED_EBGP in topology.get(f'bgp.activate.{af}',{}):
+      log.error( f"Cannot use {CONFED_EBGP} in bgp.activate", category=Warning )
+
 def post_transform(topology: Box) -> None:
   global _config_name
   confed = topology.get('bgp.confederation')
@@ -33,13 +45,15 @@ def post_transform(topology: Box) -> None:
         api.node_config(ndata,_config_name)                 # Remember that we have to do extra configuration
 
         for nb in ndata.get('bgp.neighbors',[]):            # Update remote AS used by ebgp peers
+          if nb.type != 'ebgp':                             # Only applies to EBGP peers
+            continue
           neighbor = topology.nodes[ nb.name ]
           if neighbor.get('bgp.as') in members:             # Skip members of the same confederation
             continue
+          nb.type = CONFED_EBGP
           for nb2 in neighbor.get('bgp.neighbors',[]):
             if nb2.name == n and nb2.type=='ebgp':
               nb2['as'] = casn                              # Update peer to use the confederation AS instead
-
-        # TODO: Apply ibgp type community exchange, confed is really a new type of ebgp peer
+              # nb2.type = CONFED_EBGP                      # DO NOT update BGP session type - peer device may not support this plugin
 
         break
