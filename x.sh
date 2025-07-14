4e7d4d3f06e4283b4afd4da9c8b5e89a5583e51a
CD/CD tests <ip@ipspace.net>
2025-07-14 10:20:36 +0000

    Pre-release tests for Cisco IOS and IOS/XE devices

--- x.sh
+++ x.sh
@@ -1,7 +1,2 @@
-./run-tests.py -d arubacx -p libvirt -t vxlan --limit 04
-./run-tests.py -d arubacx -p libvirt -t routing --limit 03
-./run-tests.py -d arubacx -p libvirt -t bgp_policy --limit 31
-./run-tests.py -d arubacx -p libvirt -t bgp_policy --limit 41
-./run-tests.py -d arubacx -p libvirt -t vrf --limit 15
-./run-tests.py -d arubacx -p libvirt -t vrf --limit 21
-./run-tests.py -d arubacx -p libvirt -t vrf --limit 23
+./run-tests.py -d iosvl2 -p libvirt -t bgp_session --limit 12
+./run-tests.py -d iosvl2 -p libvirt -t dhcp --limit 21
