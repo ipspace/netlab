9145d14f0931c590905115866284066a9c754ae2
Stefano Sasso <852093+ssasso@users.noreply.github.com>
2025-08-06 20:24:10 +0200

    vJunos-switch: support for explicit EVPN vlan-aware bundle (#2576)

--- netsim/devices/vjunos-switch.py
+++ netsim/devices/vjunos-switch.py
@@ -5,6 +5,7 @@ from box import Box
 from .junos import JUNOS as _JUNOS
 from . import report_quirk
 from ..utils import log
+from ..modules import vrf
 
 def check_evpn_vlan_trunks(node: Box, topology: Box) -> None:
   if 'evpn' not in node.get('module',[]):
@@ -30,10 +31,26 @@ def check_evpn_vlan_trunks(node: Box, topology: Box) -> None:
           more_hints=['The node is using VLAN-based EVPN service which cannot be used with enterprise-style VLAN trunks'],
           quirk='evpn_macvrf')
 
+def macvrf_unique_rd_for_vlan_bundle(node: Box, topology: Box) -> None:
+  if 'evpn' not in node.get('module',[]):
+    return
+  if 'vrf' not in node.get('module',[]):
+    return
+  asn = vrf.get_rd_as_number(node, topology)
+  if asn is None:
+    return
+
+  for vname,vdata in node.vrfs.items():
+    if vdata.get('evpn.bundle'):
+      # in that case we need to generate a new RD for the mac-vrf (cannot be the same of L3 VRF)
+      free_vrf_idx = vrf.get_next_vrf_id(asn)
+      vdata._junos_l2vrf_rd = free_vrf_idx[1]
+
 class Junos_switch(_JUNOS):
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
     if log.debug_active('quirks'):
       print(f"*** DEVICE QUIRKS FOR vJunos-switch {node.name}")
     check_evpn_vlan_trunks(node,topology)
+    macvrf_unique_rd_for_vlan_bundle(node,topology)
     super().device_quirks(node,topology)
