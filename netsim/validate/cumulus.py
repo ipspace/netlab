fb2a3b83cb9c033ba1f7723220f0b43b5743dd92
Ivan Pepelnjak <ip@ipspace.net>
2025-08-26 10:04:37 +0200

    Import FRR ospf validation tests into Cumulus plugins

--- netsim/validate/cumulus.py
+++ netsim/validate/cumulus.py
@@ -3,9 +3,10 @@ Top-level Cumulus Linux validation plugin
 
 Import:
 
-* FRRouting BGP checks
+* FRRouting BGP and OSPF checks
 * Linux ping/route checks
 """
 
 from netsim.validate.bgp.frr import *
 from netsim.validate.linux import *
+from netsim.validate.ospf.frr import *
