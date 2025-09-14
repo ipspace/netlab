61d1b0612a0e1eba0948245c76eac7a14a144d7e
Ivan Pepelnjak <ip@ipspace.net>
2025-09-14 11:10:52 +0200

    Release 25.09-post1 (fixing Ansible-induced crash)
    Fixes #2683

--- netsim/__init__.py
+++ netsim/__init__.py
@@ -2,18 +2,30 @@
 
 import sys
 
-__version__ = "25.09"
+__version__ = "25.09-post1"
 
 abort = False
 
 try:
   import box
-
-  if box.__version__ < '7.2.0':                 # netlab needs Python Box version 7.2.0 or higher
+  if box.__version__ < '7.2.0':               # netlab needs Python Box version 7.2.0 or higher
     print("FATAL ERROR: python-box version 7.2.0 or higher required, use 'pip3 install --upgrade python-box' to install")
     abort = True
-
 except:                                       # box is not installed, something else is bound to fail ;)
+  pass
+
+try:
+  import ansible  # type: ignore
+  if ansible.__version__ >= '2.19':           # Ansible core 2.19 contains significant templating changes
+    print(f"""
+FATAL ERROR: Ansible core version 2.19 or higher breaks netlab templating. See
+https://github.com/ipspace/netlab/issues/2683 for details.
+
+Downgrade ansible to version 11.10 or lower, preferably using "netlab install
+ansible" command.
+""")
+    abort = 'install' not in sys.argv         # We should abort unless the user is trying to reinstall ansible
+except:
   pass
 
 if abort:
--- netsim/cli/version.py
+++ netsim/cli/version.py
@@ -7,6 +7,43 @@ import typing
 
 import netsim
 
+from ..utils import read as _read
+from . import NETLAB_SCRIPT
+
+MODULES: dict = {
+  'box': 'python-box',
+  'jinja2': 'Jinja2',
+  'yaml': 'PyYAML',
+  'netaddr': 'netaddr',
+  'ruamel.yaml': 'ruamel.yaml',
+  'ansible': 'ansible-core'
+}
+
+MISSING_OK = ['ruamel.yaml']
+
+def print_default_locations() -> None:
+  try:
+    topology = _read.load('package:cli/empty.yml')
+    print(f"  user defaults: {[ src for src in topology.input if 'package:' not in src ]}")
+  except Exception as ex:
+    print(f"  cannot load default topology: {str(ex)}")
 
 def run(args: typing.List[str]) -> None:
+  global MODULES, MISSING_OK
+
   print(f"netlab version {netsim.__version__}")
+  print(f"  executable location: {NETLAB_SCRIPT}")
+  print(f"  package location: {netsim.__path__}")
+  print_default_locations()
+  print("\nRequired packages:")
+  for module,package in MODULES.items():
+    try:
+      exec(f'import {module}')
+      try:
+        version = eval(f'{module}.__version__')
+        print(f'  {package}: {version}')
+      except:
+        print(f'  Cannot find {package} version')
+    except Exception as ex:
+      if package not in MISSING_OK:
+        print(f'  {package}: not installed {str(ex)}')
