7ef6d0fd4a856b3e5c415f52ba244f8ba7bcfafe
Ivan Pepelnjak <ip@ipspace.net>
2025-10-31 08:23:44 +0100

    SRv6 implementation on IOS-XE (IOL, IOLL2, Cat8000v) (#2771)
    Implemented features:
    
    * SRv6 with IS-IS
    * L3VPN (IPv4 and IPv6) over SRv6
    
    Other changes:
    
    * srv6.allocate_loopback default value is changed to 'False'
      It makes no sense to use a default value that a majority of
      supported devices cannot work with.
    * Integration tests were adjusted to use a loopback pool that is
      outside of SRv6 SID

--- netsim/devices/iol.py
+++ netsim/devices/iol.py
@@ -3,11 +3,22 @@
 #
 from box import Box
 
-from . import _Quirks
+from ..utils import log
+from . import _Quirks, report_quirk
 from .iosv import check_ripng_passive
 
+
+def check_srv6_sid(node: Box, topology: Box) -> None:
+  if node.get('srv6.allocate_loopback',False):
+    report_quirk(
+      f'SRv6 SID on Cisco IOS-XE cannot overlap with the loopback address',
+      node,
+      quirk='srv6.sid',
+      more_hints=['Set the srv6.allocate_loopback parameter to False'],
+      category=log.IncorrectValue)
 
 class IOSXE(_Quirks):
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
     check_ripng_passive(node,topology)
+    check_srv6_sid(node,topology)
