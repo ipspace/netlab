a4828b2a45ba290056281633c6acd06374cbbf65
Ivan Pepelnjak <ip@ipspace.net>
2025-12-06 11:30:54 +0100

    Refactor 'find config template' code (#2897)
    * The 'find config template' functions have been moved into the
      utils.files module as they are not provider-specific
    * The provider 'config_templates' functions use a cache of template
      paths to avoid double lookups (which will get more expensive as we
      add Ansible-like lookups)
    * The template cache is removed from the validation test results
      because it depends on the development environment

--- tests/utils.py
+++ tests/utils.py
@@ -4,14 +4,21 @@
 
 from netsim.augment.nodes import ghost_buster
 
+"""
+Return the results of topology transformation YAML format
 
+* Remove elements that are not relevant for comparison
+* Create YAML text out of the remaining dictionary
+"""
 def transformation_results_yaml(topology):
-  """
-  Return the results of topology transformation YAML format
+  # Remove template caches, which are specific to the directory in which the
+  # tests were executed, from the test results
+  #
+  for n_data in topology.nodes.values():
+    for p in topology.defaults.providers:
+      if p in n_data:
+        n_data[p].pop('_template_cache',None)
 
-  * Remove elements that are not relevant for comparison
-  * Create YAML text out of the remaining dictionary
-  """
   ignore:list = topology.defaults.tests.ignore or ['addressing','defaults','nodes_map','includes']
   for k in ignore:
     topology.pop(k,None)
