08318979b0f6db1aa9b14f6a8cd5276ee3f8b75a
Ivan Pepelnjak <ip@ipspace.net>
2025-12-11 10:42:42 +0100

    Improved 'netlab version' printout
    Reports:
    * All modules/packages used by netlab core (the list is still updated
      manually)
    * All core ansible components
    
    Resolves #2910

--- netsim/cli/version.py
+++ netsim/cli/version.py
@@ -4,6 +4,7 @@
 #
 
 import importlib
+import importlib.metadata
 import typing
 
 import netsim
@@ -17,8 +18,13 @@ MODULES: dict = {
   'yaml': 'PyYAML',
   'netaddr': 'netaddr',
   'ruamel.yaml': 'ruamel.yaml',
-  'ansible': 'ansible-core'
+  'requests': 'requests',
+  'filelock': 'filelock'
 }
+
+PACKAGES: list = [ 'rich' ]
+
+ANSIBLE_PACKAGES: list = [ 'ansible', 'ansible-core', 'ansible-pylibssh' ]
 
 MISSING_OK = ['ruamel.yaml']
 
@@ -29,22 +35,40 @@ def print_default_locations() -> None:
   except Exception as ex:
     print(f"  cannot load default topology: {str(ex)}")
 
+def package_version(package: str) -> None:
+  try:
+    print(f'  {package}: {importlib.metadata.version(package)}')
+  except importlib.metadata.PackageNotFoundError:
+    print(f'  {package}: not installed')
+  except Exception as ex:
+    print(f'  Cannot figure out {package} version: {str(ex)}')
+
+def module_version(module: str, package: str) -> None:
+  try:
+    py_mod = importlib.import_module(module)
+    try:
+      version = getattr(py_mod,'__version__')
+      print(f'  {package}: {version}')
+    except Exception as ex:
+      print(f'  Cannot find {package} version: {str(ex)}')
+  except Exception as ex:
+    if package not in MISSING_OK:
+      print(f'  {package}: not installed {str(ex)}')
+
 def run(args: typing.List[str]) -> None:
-  global MODULES, MISSING_OK
+  global MODULES, PACKAGES, ANSIBLE_PACKAGES, MISSING_OK
 
   print(f"netlab version {netsim.__version__}")
   print(f"  executable location: {NETLAB_SCRIPT}")
   print(f"  package location: {netsim.__path__}")
   print_default_locations()
+
   print("\nRequired packages:")
   for module,package in MODULES.items():
-    try:
-      py_mod = importlib.import_module(module)
-      try:
-        version = getattr(py_mod,'__version__')
-        print(f'  {package}: {version}')
-      except Exception as ex:
-        print(f'  Cannot find {package} version: {str(ex)}')
-    except Exception as ex:
-      if package not in MISSING_OK:
-        print(f'  {package}: not installed {str(ex)}')
+    module_version(module,package)
+  for package in PACKAGES:
+    package_version(package)
+
+  print("\nAnsible components:")
+  for package in ANSIBLE_PACKAGES:
+    package_version(package)
