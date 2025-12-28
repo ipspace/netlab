77cf8defe0b04cac84074a397fc40b0ee895b9e9
Ivan Pepelnjak <ip@ipspace.net>
2025-12-28 14:01:12 +0100

    Use paramiko for network_cli on Cisco IOS with new ansible-pylibssh (#2975)
    This commit uses the device quirks to check the version of the
    'ansible-pylibssh' library, decide whether to use paramiko as
    network_cli connection with Cisco IOS/IOS-XE devices, and modifies
    the device group vars, allowing users to override this behavior
    on device-, device+provider, or node level.
    
    Resolves #2966

--- netsim/devices/csr.py
+++ netsim/devices/csr.py
@@ -2,8 +2,15 @@
 # Cisco IOS-XE quirks
 #
 
+from box import Box
+
 from .iol import IOSXE as _IOSXE
+from .iosv import use_paramiko
 
 
 class CSR(_IOSXE):
-  pass
+
+  @classmethod
+  def device_quirks(self, node: Box, topology: Box) -> None:
+    super().device_quirks(node,topology)
+    use_paramiko(node,topology)
