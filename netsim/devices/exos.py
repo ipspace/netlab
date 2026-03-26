fa985f1a1a895eecb3486724540b492a8a1bbe3b
Seb d'Argoeuves <seb.dargoeuves@gmail.com>
2026-03-26 01:32:47 +0000

    vrrp support

---
+++ netsim/devices/exos.py
@@ -0,0 +1,26 @@
+from box import Box
+
+from ..utils import log
+from . import _Quirks, report_quirk
+
+
+def check_vrrp_address_families(node: Box) -> None:
+  for intf in node.interfaces:
+    if intf.get('gateway.protocol',None) != 'vrrp':
+      continue
+
+    if 'ipv4' in intf.gateway and 'ipv6' in intf.gateway:
+      report_quirk(
+        text=f'Extreme EXOS cannot configure IPv4 and IPv6 virtual addresses in the same VRRP instance ({node.name} {intf.ifname})',
+        node=node,
+        quirk='vrrp_mixed_af',
+        category=log.IncorrectType)
+      return
+
+
+class EXOS(_Quirks):
+
+  @classmethod
+  def device_quirks(cls, node: Box, topology: Box) -> None:
+    if 'gateway' in node.get('module',[]):
+      check_vrrp_address_families(node)
