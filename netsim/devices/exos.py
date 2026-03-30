16b5499ea6a0c6f9807f11c87d7ee1088efb07b3
Seb d'Argoeuves <seb.dargoeuves@gmail.com>
2026-03-30 07:22:24 +0000

    attempt at vlan1 quirk for EXOS

--- netsim/devices/exos.py
+++ netsim/devices/exos.py
@@ -3,6 +3,8 @@ from box import Box
 from ..utils import log
 from . import _Quirks, need_ansible_collection, report_quirk
 
+EXOS_DEFAULT_VLAN_NAME = 'Default'
+
 
 def check_vrrp_address_families(node: Box) -> None:
   for intf in node.interfaces:
@@ -18,10 +20,56 @@ def check_vrrp_address_families(node: Box) -> None:
       return
 
 
+def rename_vlan_references(intf: Box, old_name: str, new_name: str) -> None:
+  if intf.get('vlan.access',None) == old_name:
+    intf.vlan.access = new_name
+  if intf.get('vlan.native',None) == old_name:
+    intf.vlan.native = new_name
+  if intf.get('vlan.name',None) == old_name:
+    intf.vlan.name = new_name
+  if intf.get('vlan_name',None) == old_name:
+    intf.vlan_name = new_name
+  if intf.get('_vlan_native',None) == old_name:
+    intf._vlan_native = new_name
+
+  trunk = intf.get('vlan.trunk',None)
+  if trunk and old_name in trunk:
+    trunk[new_name] = trunk.pop(old_name)
+
+
+def rename_vlan_1_to_default(node: Box) -> None:
+  if 'vlans' not in node:
+    return
+
+  vlan_1_name = next(
+    (vname for vname,vdata in node.vlans.items() if vdata.get('id',None) == 1 and vname != EXOS_DEFAULT_VLAN_NAME),
+    None)
+  if not vlan_1_name:
+    return
+
+  if EXOS_DEFAULT_VLAN_NAME in node.vlans:
+    node.vlans[EXOS_DEFAULT_VLAN_NAME] = node.vlans[EXOS_DEFAULT_VLAN_NAME] + node.vlans[vlan_1_name]
+    node.vlans.pop(vlan_1_name,None)
+  else:
+    node.vlans[EXOS_DEFAULT_VLAN_NAME] = node.vlans.pop(vlan_1_name)
+
+  for intf in node.interfaces:
+    rename_vlan_references(intf,vlan_1_name,EXOS_DEFAULT_VLAN_NAME)
+
+  report_quirk(
+    text='Extreme EXOS reserves VLAN ID 1 for the built-in Default VLAN',
+    more_data=f'Renaming VLAN {vlan_1_name} to {EXOS_DEFAULT_VLAN_NAME} on node {node.name}',
+    node=node,
+    quirk='vlan.default_1',
+    category=Warning)
+
+
 class EXOS(_Quirks):
 
   @classmethod
   def device_quirks(cls, node: Box, topology: Box) -> None:
+    if 'vlan' in node.get('module',[]):
+      rename_vlan_1_to_default(node)
     if 'gateway' in node.get('module',[]):
       check_vrrp_address_families(node)
 
