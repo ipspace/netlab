1dd0538dc0dace0b734617d05401e3efcd51157f
leec-666 <leercalder@gmail.com>
2026-04-29 08:35:54 +0000

    remove reference to Junos quirks

--- netsim/devices/csrx.py
+++ netsim/devices/csrx.py
@@ -4,10 +4,10 @@
 from box import Box
 
 from ..utils import log
-from . import _Quirks, report_quirk
+from . import _Quirks
 
 class CSRX(_Quirks):
 
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
-    from . import junos
+    return
