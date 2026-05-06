eb19bd84bdea335c18414cac0ecf13b91b0c94ac
Ivan Pepelnjak <ip@ipspace.net>
2026-05-06 18:10:50 +0200

    Fix: Make CI/CD tests work with Ruamel
    This is more of a workaround than a complete solution. The tests pass
    even when Ruamel is installed and you can generate new transformation
    test results. Error test results cannot be generated with Ruamel due
    to Ruamel-specific scalar types

--- tests/utils.py
+++ tests/utils.py
@@ -2,9 +2,43 @@
 # Test utilities
 #
 
+import typing
+
+import yaml
 from box import Box
 
 from netsim.augment.nodes import ghost_buster
+
+try:
+  import ruamel  # type: ignore # noqa: F401 -- dear ruff, please do not complain, we need to know what we're dealing with
+  HAS_RUAMEL = True
+except:
+  HAS_RUAMEL = False
+
+"""
+Ruamel loves to use data types derived from int/str/float. If we want to
+create YAML via PyYAML (see #3345), we need to go through the whole data
+structure and clean it up. Yeah, it will be slow. Hooray for consistency!
+"""
+def clean_ruamel(data: typing.Any) -> typing.Any:
+  global HAS_RUAMEL
+  if not HAS_RUAMEL:
+    return data
+  
+  if isinstance(data,bool):
+    return data
+  elif isinstance(data,int):
+    return int(data)
+  elif isinstance(data,str):
+    return str(data)
+  elif isinstance(data,float):
+    return float(data)
+  elif isinstance(data,list):
+    return [ clean_ruamel(v) for v in data ]
+  elif isinstance(data,dict):
+    return { k:clean_ruamel(v) for k,v in data.items() }
+  else:
+    return data
 
 """
 Return the results of topology transformation YAML format
@@ -20,4 +54,4 @@ def transformation_results_yaml(topology: Box) -> str:
   if 'unmanaged' in ignore:
     topology = ghost_buster(topology)
 
-  return topology.to_yaml()
+  return yaml.dump(topology.to_dict(),width=120)
