cb88b74e9a7cfe567c5172aee10dcc7571bdd7b1
Ivan Pepelnjak <ip@ipspace.net>
2026-06-06 10:26:36 +0200

    Fix: Implement checks on routing.prefix min/max attributes
    The min/max attributes specified in a prefix-list entry are checked to
    be greater than (or equal) to the specified prefix length and less than
    the maximum allowed by the address family. Furthermore, the max value
    must be greater than or equal than the min value.
    
    The checks also enforce stricter min/max requirements for Cisco IOS
    where the min/max value must be strictly greater than the prefix len.

--- netsim/modules/routing/prefix.py
+++ netsim/modules/routing/prefix.py
@@ -5,10 +5,12 @@
 # * Routing filters (prefixes, communities, as-paths)
 # * Static routes
 #
+import ipaddress
 import typing
 
 from box import Box
 
+from ...augment import devices as a_devices
 from ...data import get_new_box
 from ...utils import log
 
@@ -46,10 +48,26 @@ def expand_prefix_entry(p_entry: Box, topology: Box) -> Box:
 
   return p_entry
 
-"""
-adjust_pfx_min_max: Adjust prefix list entry min/max keywords
-"""
-def adjust_pfx_min_max(p_entry: Box, m_kw: str, af: str, p_name: str, node: Box) -> None:
+def adjust_pfx_min_max(
+      p_entry: Box,
+      m_kw: str,
+      af: str,
+      p_name: str,
+      node: Box,
+      min_pfx_len: int,
+      max_pfx_len: int) -> None:
+  """
+  adjust_pfx_min_max: Adjust prefix list entry min/max keywords
+
+  Arguments:
+  - p_entry: the prefix we're checking
+  - m_kw: the parameter we're checking (min/max)
+  - af: the address family we're checking
+  - p_name: prefix list name for error messages
+  - node: node data (needed for error messages)
+  - min_pfx_len: minimum prefix length we can match (depends on prefix)
+  - max_pfx_len: maximum prefix length we can match (depends on address family)
+  """
   if m_kw not in p_entry:
     return
 
@@ -58,34 +76,59 @@ def adjust_pfx_min_max(p_entry: Box, m_kw: str, af: str, p_name: str, node: Box)
     return                                        # ... so we can just pop the unnecessary entry
 
   m_value = p_entry[m_kw].get(af,None)
-  if m_value < 0:                                 
+  if m_value < min_pfx_len:                                 
     log.error(
-      f'Prefix filter {m_kw} value should be >= 0 (policy {p_name}#{p_entry.sequence} on node {node.name})',
+      f'Prefix filter {af}.{m_kw} value should be >= {min_pfx_len} (limited by {p_entry[af]})',
+      more_data=[f'Node {node.name} (device {node.device}) policy {p_name} sequence# {p_entry.sequence}'],
       category=log.IncorrectValue,
       module='routing')
-    return
 
-  m_max = 32 if af == 'ipv4' else 128
-  if m_value > m_max:
+  if m_value > max_pfx_len:
     log.error(
-      f'Prefix filter {af}.{m_kw} value should be <= {m_max} (policy {p_name}#{p_entry.sequence} on node {node.name})',
+      f'Prefix filter {af}.{m_kw} value should be <= {max_pfx_len} (limited by address family)',
+      more_data=[f'Node {node.name} (device {node.device}) policy {p_name} sequence# {p_entry.sequence}'],
       category=log.IncorrectValue,
       module='routing')
-    return
 
   p_entry[m_kw] = m_value                         # Replace the min/max dict with per-AF value
 
-"""
-create_af_entry: create AF-specific prefix-list entry
-"""
-def create_pfx_af_entry(p_entry: Box, af: str, p_name: str, node: Box) -> Box:
+def create_pfx_af_entry(p_entry: Box, af: str, p_name: str, node: Box, min_strict: bool = False) -> Box:
+  """
+  create_af_entry: create AF-specific prefix-list entry
+
+  Arguments:
+  - p_entry: Original prefix list entry
+  - af: Address family we're focusing on
+  - p_name: Prefix list name (for error messages)
+  - node: Node data (needed for error messages)
+  - min_strict: Is device enforcing strict min/max rules (min/max have to be greater than prefix length)?
+  """
   af_p_entry = get_new_box(p_entry)                         # Create a copy of the current p_entry
   for af_x in ('ipv4','ipv6'):                              # ... remove all other address families
     if af_x in p_entry and af_x != af:
       af_p_entry.pop(af_x,None)
 
+  pfx_max = 32 if af == 'ipv4' else 128
+  pfx_len = ipaddress.ip_network(p_entry[af]).prefixlen
+
   for m_kw in ('min','max'):
-    adjust_pfx_min_max(af_p_entry,m_kw,af,p_name,node)
+    if pfx_len == pfx_max:
+      log.error(
+        f'Cannot use prefix filter {m_kw} keyword with prefix {p_entry[af]}',
+        more_data=[f'Node {node.name} (device {node.device}) policy {p_name} sequence# {p_entry.sequence}'],
+        category=log.IncorrectValue,
+        module='routing')
+      return af_p_entry
+    else:
+      pfx_min = pfx_len + 1 if min_strict else pfx_len
+      adjust_pfx_min_max(af_p_entry,m_kw,af,p_name,node,min_pfx_len=pfx_min,max_pfx_len=pfx_max)
+
+  if 'min' in af_p_entry and 'max' in af_p_entry and af_p_entry.min > af_p_entry.max:
+    log.error(
+      f'Prefix filter {af}.min should be <= {af}.max',
+      more_data=[f'Node {node.name} (device {node.device}) policy {p_name} sequence# {p_entry.sequence}'],
+      category=log.IncorrectValue,
+      module='routing')
 
   return af_p_entry
 
@@ -108,12 +151,14 @@ def expand_prefix_list(p_name: str,o_name: str,node: Box,topology: Box) -> typin
     node.routing[o_name][p_name][p_idx] = expand_prefix_entry(node.routing[o_name][p_name][p_idx],topology)
 
   node.routing[o_name][p_name] = sorted(node.routing[o_name][p_name],key=lambda O: O.get('sequence',10))
+  features = a_devices.get_device_features(node,topology.defaults)
+  min_strict = features.get('routing.prefix.strict',False)
   af_prefix: dict = {}                                      # Prepare dictionary of per-AF prefix lists
   for af in ('ipv4','ipv6'):                                # Iterate over address families (sorry, no CLNS or IPX)
     af_prefix[af] = []                                      # Start with an emtpy per-AF list
     for p_entry in node.routing[o_name][p_name]:            # Iterate over prefix list entries
       if af in p_entry:                                     # Is the current AF in the prefix list entry?
-        af_p_entry = create_pfx_af_entry(p_entry,af,p_name,node)
+        af_p_entry = create_pfx_af_entry(p_entry,af,p_name,node,min_strict=min_strict)
         af_prefix[af].append(af_p_entry)                    # create af-specific entry and append it to per-AF list
 
     if af_prefix[af]:                                       # Do we have a non-empty per-AF prefix list?
