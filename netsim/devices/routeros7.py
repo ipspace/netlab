5b5c6e7a9649f09268fadde129bfabf0251d5420
snuffy22 <22233310+snuffy22@users.noreply.github.com>
2026-07-10 23:42:45 +1000

    Mikrotik RouterOS7 version uplift to 7.21.4 (#3485)
    * Use BGP instances
    * Use the new keyword for AFI activation
    * Create a default BGP instance to support VRF route leaking
    * Add a device quirk to disable VPNv6 AF
    
    Also:
    * Use BGP filters to implement next-hop-self handling and
      community propagation
    
    ---------
    
    Co-authored-by: Ivan Pepelnjak <ip@ipspace.net>

---
+++ netsim/devices/routeros7.py
@@ -0,0 +1,25 @@
+#
+# Mikrotik RouterOS7 quirks
+#
+from box import Box
+
+from ..utils import log
+from . import _Quirks, report_quirk
+
+
+def check_vpnv6_af(node: Box, topology: Box) -> None:
+  for ngb in node.get('bgp.neighbors',[]):
+    if 'vpnv6' in ngb:
+      report_quirk(
+        f'We could not get VPNv6 AF to work on Mikrotik RouterOS7 (node {node.name})',
+        node=node,
+        quirk='vpnv6',
+        category=log.IncorrectValue,
+        module='quirks')
+      return
+
+class RouterOS7(_Quirks):
+  @classmethod
+  def device_quirks(self, node: Box, topology: Box) -> None:
+    if node.get('mpls.vpn',False):
+      check_vpnv6_af(node,topology)
