7ae38848651fd9ac679af798edee62fffcc7a9f0
Ivan Pepelnjak <ip@ipspace.net>
2026-07-11 08:43:57 +0200

    RouterOS7: Add BGP per-AF activation
    The RouterOS7 BGP configuration template can activate individual
    address families using bgp.neighbor.activate parameter. The BGP
    session is disabled if it has no usable AFs.
    
    To implement this feature, the device quirk builds per-transport-AF
    list of activated AFs. That list is then used to either activate the
    collected AFs or disable the transport session. The parameters of
    the bgpcfg.neighbor macro were changed to pass neighbor AF (not IP)
    so we can directly refer to the _afi_list.
    
    The AFs in the per-transport-AF AFI list can include vpn and evpn
    address families, making the 'mpls.vpn' configuration template
    obsolete.

--- netsim/devices/routeros7.py
+++ netsim/devices/routeros7.py
@@ -4,10 +4,14 @@
 from box import Box
 
 from ..utils import log
+from ..utils import routing as _routing
 from . import _common, _Quirks, report_quirk
 
 
 def check_vpnv6_af(node: Box, topology: Box) -> None:
+  '''
+  VPNv6 does not work on RouterOS7
+  '''
   for ngb in node.get('bgp.neighbors',[]):
     if 'vpnv6' in ngb:
       report_quirk(
@@ -18,9 +22,31 @@ def check_vpnv6_af(node: Box, topology: Box) -> None:
         module='quirks')
       return
 
+def build_afi_lists(node: Box) -> None:
+  '''
+  Adding AFIs to RouterOS7 BGP connections is non-trivial. This helper function
+  builds per-transport-connection AFI lists that can be used directly in the
+  configuration templates.
+
+  Please note that the AFI lists are built only for global BGP neighbors. The
+  VRF BGP neighbors use only the "native" AF.
+  '''
+  for ngb in _routing.neighbors(node,vrf=False):
+    for t_af in ['ipv4','ipv6']:                              # Iterate over potential transport AFs
+      if t_af not in ngb:                                     # Neighbor not using this AF? I'm fine with that...
+        continue
+      ngb._afi_list[t_af] = []
+      if 'activate' not in ngb or ngb.activate.get(t_af,False):
+        ngb._afi_list[t_af].append(t_af)                      # Append default AF if needed
+      for bgp_af in ['vpnv4','vpnv6','evpn']:                 # Then append service AFs
+        if bgp_af in ngb and (ngb[bgp_af] == ngb[t_af] or ngb[bgp_af] == t_af):
+          ngb._afi_list[t_af].append(bgp_af)
+
 class RouterOS7(_Quirks):
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
     if node.get('mpls.vpn',False):
       check_vpnv6_af(node,topology)
     _common.check_tagged_vlan_1(node)
+    if node.get('bgp.neighbors'):
+      build_afi_lists(node)
