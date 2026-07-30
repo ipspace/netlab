ca90f89b1a3297f4db93d34b7bd480e59b4e5496
Ivan Pepelnjak <ip@ipspace.net>
2026-07-30 12:55:06 +0200

    Add 'prefix_or_host' use case to IPv4/IPv6 validators (#3718)
    The ACLs accept either a prefix or an IP address (= host), but not
    the additional interface values (true/false/int). No existing 'use'
    value matched the requirements.
    
    Also: prettify the "int/bool is not valid here" hint display

--- netsim/data/types.py
+++ netsim/data/types.py
@@ -704,6 +704,7 @@ Common IP address validation functionality. Both tests recognize these use cases
 * 'subnet_prefix' -- an IP prefix that can be used on a subnet, including bool (unnum/LLA).
   Host bits must be zero, and it cannot be in the multicast range.
 * 'prefix' -- any IP prefix, including multicast prefixes. Use case: prefix lists
+* 'prefix_or_host' -- any IP prefix or host (IP address without a prefix). Use case: ACLs
 
 Valid types per use case:
 
@@ -712,6 +713,7 @@ Use case      | str (w/) | str (no/) | bool | int |
 address       |          |    OK     |      |     |
 prefix        |    OK    |           |      |     |
 host_prefix   |    OK    |           |      |     |
+prefix_or_host|    OK    |    OK     |      |     |
 interface     |    OK    |    OK     |  OK  | OK  |
 subnet_prefix |    OK    |           |  OK  |     |
 id            |          |    OK     |      | OK  |
@@ -763,9 +765,19 @@ def common_addr_parse(
 
     return None
 
+  if use not in ['address','prefix','host_prefix','prefix_or_host','interface','subnet_prefix','id']:
+    return {'_value': f'INTERNAL ERROR: invalid use parameter: {use}'}
+
+  exp_type = f'{af} address or prefix' if use == 'prefix_or_host' \
+                else f'{af} prefix' if 'prefix' in use  \
+                else f'{af} address'
+
   if isinstance(value,bool):                      # bool values are valid only on interfaces and subnets
     if use not in ('interface','subnet_prefix'):
-      return { '_value' : f'an {af} address (boolean value is valid only on an interface)' }
+      return {
+        '_value' : exp_type,
+        '_hint_id': 'ip_bool_value',
+        '_hint': 'A boolean value is valid only on an interface or as a subnet prefix' }
     else:
       return { '_valid': True }
 
@@ -773,11 +785,14 @@ def common_addr_parse(
     if use == 'id' and af == 'IPv4':
       return check_int_value(value,xform_int)
     if use != 'interface':
-      return { '_value': f'an {af} address or prefix (integer value is only valid as interface offset or a 32-bit ID)' }
+      return {
+        '_value' : exp_type,
+        '_hint_id': 'ip_int_value',
+        '_hint': 'An integer value is only valid as interface offset or a 32-bit ID' }
     return check_int_value(value,None)
 
   if not isinstance(value,str):
-    return { '_type': f'{af} prefix' if 'prefix' in use else f'{af} address' }
+    return { '_type': exp_type }
 
   if named and xform_pfx is not None:             # Check whether we can use a named prefix
     topology = global_vars.get_topology()
@@ -789,7 +804,7 @@ def common_addr_parse(
         if 'ipv4' in pfxs[value]:
           return { '_valid': True, '_transform': xform_pfx }
 
-  if use in ['id','address'] or (use in ['interface'] and '/' not in value):
+  if use in ['id','address'] or (use in ['interface','prefix_or_host'] and '/' not in value):
     try:
       p_addr = addr_parse(value)
     except Exception as Ex:
@@ -802,7 +817,7 @@ def common_addr_parse(
     except Exception as Ex:
       return { '_value': f'{af} prefix ({Ex})' }
 
-  if use not in ['prefix','id']:
+  if use not in ['prefix','prefix_or_host','id']:
     r_hit = check_reserved_range(RESERVED_PREFIXES[af],p_addr)
     if r_hit:
       return {'_value': f'{af} {"prefix" if "prefix" in use else "address"}'+
