5e2886b032c4957fb635657b5a20d0c14c329b33
Ivan Pepelnjak <ip@ipspace.net>
2026-07-30 12:06:58 +0200

    Rewrite routing.acl functionality (part 1)
    * Make ACLs AF-agnostic and generate per-AF entries in the _acl.af
      dictionaries
    * Change most src/dst attributes to lists to allow the user to
      specify multiple values (addresses, pools...). Also, the attributes
      are no longer exclusive as we collect their values into ipv4/ipv6
      lists
    * Simplify interface ACL settings -- AF dict is optional and created
      based on interface AFs
    * Replace 'any' attribute with 'prefix: any'
    * Accept addresses or prefixes in ipv4/ipv6 attribute
    * Remove 'host' attribute and create it based on whether an IP address
      has a slash (not that we would need it anyway)
    * Set default 'prefix: [ any ]' value for src/dst
    
    While generating the per-AF ACL entries for the target devices, the
    src/dst IP lists are expanded into a cartesian product, generating
    as many ACL entries as needed.
    
    Also:
    * Renumber integration tests to have a distinct first number (3)
    
    Still missing:
    * node/interface/role logic is broken
    * Documentation
    
    Out-of-scope: port matching logic

--- netsim/modules/routing/acl.py
+++ netsim/modules/routing/acl.py
@@ -9,64 +9,53 @@ from box import Box
 
 from netsim.utils import log
 
-from ...data import get_box
-from .normalize import import_routing_object
-
-"""
-expand_acl_address_entry:
-* Transform 'pool' and 'prefix' keywords into 'ipv4' and 'ipv6'
-* Resolve node inteface and node-role tupples into 'ipv4' and 'ipv6'
-"""
+from ...data import append_to_list, get_box
+from .normalize import check_routing_object, import_routing_object
 
 
 def expand_acl_address_entry(p_entry: Box, topology: Box) -> Box:
-  extra_data = None
-  if "pool" in p_entry:
-    extra_data = topology.addressing[p_entry.pool]
-    p_entry.pop("pool", None)
+  """
+  expand_acl_address_entry:
+  * Transform 'pool' and 'prefix' keywords into 'ipv4' and 'ipv6'
+  * Resolve node-inteface and node-role tuples into 'ipv4' and 'ipv6'
+  """
 
-  if "prefix" in p_entry:
-    extra_data = topology.prefix[p_entry.prefix]
-    p_entry.pop("prefix", None)
+  def add_acl_prefixes(p_entry: Box, data: Box) -> None:
+    for af in log.AF_LIST:
+      if af in data:
+        append_to_list(p_entry,af,data[af])
 
-  if "node" in p_entry:
-    node_name = p_entry.node
+  for p_name in p_entry.get('pool',[]):
+    add_acl_prefixes(p_entry,topology.addressing[p_name])
+
+  for p_name in p_entry.get('prefix',[]):
+    add_acl_prefixes(p_entry,topology.prefix[p_name])
+
+  for node_name in p_entry.get('node',[]):
     node_data = topology.nodes[node_name]
-    intf_list = node_data.get("interfaces", [])
+    intf_list = node_data.get("interfaces", []) + node_data.get("loopback",[])
 
     if "interface" in p_entry:
       ifname = p_entry.interface
-      match = [intf for intf in intf_list if intf.get("ifname") == ifname]
-      if not match:
+      intf_list = [intf for intf in intf_list if intf.get("ifname") in ifname]
+      if not intf_list:
         log.error(f"Node {node_name} has no interface {ifname} referenced in ACL entry", log.IncorrectValue, "acl")
-        return p_entry
-      extra_data = match[0]
-      p_entry.pop("interface", None)
-
+        continue
     elif "role" in p_entry:
       role = p_entry.role
-      match = [intf for intf in intf_list if intf.get("role") == role]
+      match = [intf for intf in intf_list if intf.get("role") in role]
       if not match:
         log.error(
-          f"Node {node_name} has no interface with role {role} referenced in ACL entry", log.IncorrectValue, "acl"
-        )
-        return p_entry
-      if len(match) > 1:
-        log.error(
-          f"Node {node_name} has multiple interfaces with role {role}, ACL entry is ambiguous",
+          f"Node {node_name} has no interface with role {role} referenced in ACL entry",
           log.IncorrectValue,
-          "acl",
-        )
-        return p_entry
-      extra_data = match[0]
-      p_entry.pop("role", None)
+          "acl")
+        continue
 
-    p_entry.pop("node", None)
+    add_acl_prefixes(p_entry,intf_list)
 
-  if extra_data:
-    for af in ("ipv4", "ipv6"):
-      if af in extra_data:
-        p_entry[af] = extra_data[af]
+  for kw in ('pool','prefix','node','interface','role'):
+    p_entry.pop(kw,None)
+
   return p_entry
 
 
@@ -105,7 +94,6 @@ def validate_acl_address_entry(p_entry: Box, ctx: validation_context) -> None:
 
   UDP = 17
   TCP = 6
-  af = "ipv4" if ctx.af_ipv4 else "ipv6"
   port_keys = ("port", "port_range")
 
   if ctx.protocol < 0 or ctx.protocol > 255:
@@ -127,19 +115,12 @@ def validate_acl_address_entry(p_entry: Box, ctx: validation_context) -> None:
 
   for direction in ("src", "dst"):
     entry = p_entry[direction]
-    if af not in entry and "any" not in entry:
-      log.error(
-        f"ACL {ctx.p_name} entry {ctx.idx} requires a valid {af}/any address in {direction} address",
-        category=log.IncorrectAttr,
-      )
-
     if any(k in entry for k in port_keys) and ctx.protocol not in (TCP, UDP):
       log.error(
         f"ACL {ctx.p_name} entry {ctx.idx} cannot use a port or "
         f"port range in {direction} address with this protocol. Use UDP/TCP",
         category=log.IncorrectAttr,
       )
-
     if "port_range" in entry and entry.port_range.min >= entry.port_range.max:
       log.error(
         f"ACL {ctx.p_name} entry {ctx.idx} has an invalid {direction} port range: min greater or equal to max",
@@ -167,7 +148,8 @@ def expand_acl_description(entry: Box, expansion: list) -> None:
   expansion.append(description_entry)
 
 
-def expand_acl_portop(entry: Box, node: Box, topology: Box, expansion: list) -> None:
+def expand_acl_portop(entry: Box) -> list:
+  expansion = []
 
   for addr_key in ("src", "dst"):
     addr_entry = entry.get(addr_key)
@@ -190,19 +172,13 @@ def expand_acl_portop(entry: Box, node: Box, topology: Box, expansion: list) -> 
     upper_entry[addr_key].port_op = "gt"
     upper_entry[addr_key].pop("port_range", None)
     expansion.append(upper_entry)
-  return None
 
-
-# Entry points below
+  return expansion
 
 
 def expand_acl(p_name: str, o_name: str, node: Box, topology: Box) -> typing.Optional[list]:
   ctx = validation_context(p_name=p_name, idx=0, af_ipv4=True, protocol=0, established=False)
-  acl_def = node.routing[o_name][p_name]
-  acl_list = acl_def.value
-  ctx.af_ipv4 = acl_def.af == "ipv4"
-  expansion: list = []
-
+  acl_list = node.routing[o_name][p_name]
   for idx, entry in enumerate(list(acl_list)):
     ctx.protocol = resolve_protocol(entry.protocol)
     ctx.established = entry.get("established", False)
@@ -211,61 +187,94 @@ def expand_acl(p_name: str, o_name: str, node: Box, topology: Box) -> typing.Opt
       if addr_key in entry:
         entry[addr_key] = expand_acl_address_entry(entry[addr_key], topology)
     validate_acl_address_entry(entry, ctx)
-    # Synthesize new acl entries, when needed
-    expand_acl_description(entry, expansion)
-    expand_acl_portop(entry, node, topology, expansion)
     acl_list[idx] = entry
 
-  acl_list.extend(expansion)
   acl_list.sort(key=lambda e: e.get("sequence", 10))
   return None
 
 
-"""
-Ensure ACLs referenced on interfaces are merged
-at node level. This needs to be called early in
-post-transform, before ACLs are processed at node
-level.
-"""
+def expand_af_acl(acl_list: list,acl_af: str, acl_name: str, node_name: str) -> list:
+  """
+  Expands the ACL entries for a single address family. Lists of source/
+  destination IP addresses are expanded as a cartesian product
+  """
+  if not acl_list:                                          # Nothing to do 
+    return acl_list
+  acl_sequence = 100
+  acl_result: list = []
+  for acl_idx,acl_entry in enumerate(acl_list,1):           # Iterate over all ACL entries
+    src_list = acl_entry.src.get(acl_af,[])                 # Get source/destination AF-specific entries
+    dst_list = acl_entry.dst.get(acl_af,[])
+    acl_rest = { k:v for k,v in acl_entry.items() if k not in ['src','dst'] }
+    src_data = { k:v for k,v in acl_entry.src.items() if k not in log.AF_LIST }
+    dst_data = { k:v for k,v in acl_entry.dst.items() if k not in log.AF_LIST }
+    acl_data = get_box(acl_rest) + { 'src': src_data } + {'dst': dst_data }
+
+    if not src_list and not dst_list:                       # No usable AF entries? Move on...
+      continue
+    if not src_list or not dst_list:                        # Otherwise we need both source and destination entries
+      log.warning(
+        text=f'ACL {acl_name}[{acl_idx}] on node {node_name} does not have {acl_af} entries in source and destination fields',
+        flag='acl_af_src_dst',module='routing')
+      continue
+
+    if 'description' in acl_data:
+      acl_result.append(get_box({'description': acl_data.description, 'sequence': acl_sequence}))
+      acl_sequence += 10
+      acl_data.pop('description',None)
+
+    for src_ip in src_list:
+      for dst_ip in dst_list:
+        acl_item = acl_data + {'src': { acl_af: src_ip }} + {'dst': { acl_af: dst_ip }}
+        for kw in ['src','dst']:
+          if '/' not in acl_item[kw][acl_af]:
+            acl_item[kw].host = True
+          if acl_item[kw][acl_af].endswith('/0'):
+            acl_item[kw].any = True
+
+        acl_item.sequence = acl_sequence
+        acl_result.append(acl_item)
+        extra_items = expand_acl_portop(acl_item)
+        if extra_items:
+          acl_result.extend(extra_items)
+
+        acl_sequence += 10
+
+  return acl_result
+
+#
+# Entry points:
+#
+# - interface_acl_references is called to trigger import of ACLs used on interfaces
+# - create_af_acls is called during the cleanup phase to create per-AF ACLs
+#
+
+def interface_acl_references(node: Box,topology: Box) -> None:
+  """
+  Ensure ACLs referenced on interfaces are merged at node level. This needs to
+  be called early in post-transform, before ACLs are processed at node level.
+  """
+  for intf in node.get("interfaces",[]):
+    intf_acl = intf.get("routing.acl",{})
+    if not intf_acl:
+      continue
+    for acl_dir,acl_data in intf_acl.items():
+      if isinstance(acl_data,str):
+        intf.routing.acl[acl_dir] = { acl_af: acl_data for acl_af in log.AF_LIST if acl_af in intf }
+        acl_list = [ acl_data ]
+      else:
+        acl_list = acl_data.values()
+      for acl_name in acl_list:
+        if import_routing_object(acl_name,'acl',node,topology):
+          check_routing_object(acl_name,'acl',node,topology)
 
 
-def resolve_interface_acl_references(node: Box, topology: Box) -> None:
-  routing = topology.get("routing")
-  if routing is None:
-    return
-
-  global_acls = routing.get("acl", {})
-  node_acls = node.get("routing", {}).get("acl", {})
-
-  for intf in node.get("interfaces", []):
-    intf_acl = intf.get("routing", {}).get("acl", {})
-
-    for direction in ("in", "out"):
-      direction_acl = intf_acl.get(direction, {})
-
-      for af in ("ipv4", "ipv6"):
-        acl_id = direction_acl.get(af)
-        if not acl_id:
-          continue
-
-        acl = global_acls.get(acl_id)
-        if acl is None:
-          log.error(
-            f"Interface '{intf.ifname}' on '{node.name}' "
-            f"references non-existent ACL '{acl_id}' "
-            f"in 'routing.acl.{direction}.{af}'",
-            category=log.IncorrectAttr,
-          )
-          continue
-
-        if acl.get("af") != af:
-          log.error(
-            f"Interface '{intf.ifname}' on '{node.name}' "
-            f"references {acl.get('af')} ACL '{acl_id}' "
-            f"as {af} in 'routing.acl.{direction}.{af}'",
-            category=log.IncorrectAttr,
-          )
-          continue
-
-        if acl_id not in node_acls:
-          import_routing_object(acl_id, "acl", node, topology)
+def create_af_acls(node_pdata: Box,o_type: str,node: Box,topology: Box) -> None:
+  """
+  Create per-AF node ACLs
+  """
+  node_af_list = node.get('af',{}).keys() or log.AF_LIST
+  for acl_name,acl_list in node_pdata.items():
+    for acl_af in node_af_list:
+      acl_data = expand_af_acl(acl_list,acl_af,acl_name,node.name)
+      node.routing['_'+o_type][acl_af][acl_name] = acl_data
