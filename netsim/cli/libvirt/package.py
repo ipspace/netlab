edebb1e3c2a40f249bebe39d46a4c6fc5673af7d
snuffy22 <22233310+snuffy22@users.noreply.github.com>
2026-08-29 00:50:03 +1000

    Ensure Vagrant provider is 'libvirt' when trying to package our own boxes (#3832)
    WSL needs the "VAGRANT_DEFAULT_PROVIDER" environment variable to work.

--- netsim/cli/libvirt/package.py
+++ netsim/cli/libvirt/package.py
@@ -430,9 +430,9 @@ preceded by "netlab/".
 
   strings.print_colored_text('[IMPORT]  ','green',None)
   print(f"Importing Vagrant box {boxname} version {version}")
-  if not external_commands.run_command(f"vagrant box add {json_name}"):
+  if not external_commands.run_command(["vagrant","box","add","--provider","libvirt",json_name]):
     error_and_exit(
-      f'Failed to add Vagrant box. Fix the error(s) and use "vagrant box add {json_name}" to add it.',
+      f'Failed to add Vagrant box. Fix the error(s) and use "vagrant box add --provider libvirt {json_name}" to add it.',
       module='libvirt',
       category=log.FatalError,
       more_hints=[
