a9dce117e7ef54f883fc4273b8e3f232b45fe871
snuffy22 <22233310+snuffy22@users.noreply.github.com>
2026-09-18 22:27:09 +1000

    LAG on RouterOS7 (#3851)
    * Device configuration templates
    * Router quirk (increased MTU on member intf)

--- netsim/devices/routeros7.py
+++ netsim/devices/routeros7.py
@@ -42,6 +42,47 @@ def build_afi_lists(node: Box) -> None:
         if bgp_af in ngb and (ngb[bgp_af] == ngb[t_af] or ngb[bgp_af] == t_af):
           ngb._afi_list[t_af].append(bgp_af)
 
+def adjust_lag_vlan_mtu(node: Box) -> None:
+  '''
+  RouterOS includes the 802.1Q header in the MTU of a VLAN carried over a bond.
+  Increase the physical LAG member MTU to the LAG MTU plus four bytes.
+  '''
+  lag_by_ifindex: dict[int,Box] = {}
+  lag_mtu: dict[int,int] = {}
+
+  # Find LAGs and VLAN configuration stored directly on them.
+  for lag_intf in node.interfaces:
+    if lag_intf.get('type') != 'lag':
+      continue
+
+    lag_by_ifindex[lag_intf.ifindex] = lag_intf
+
+    if 'vlan' in lag_intf:
+      lag_mtu[lag_intf.lag.ifindex] = lag_intf.get('mtu',1500) + 4
+
+  # Find VLAN subinterfaces attached to a LAG.
+  for vlan_intf in node.interfaces:
+    if vlan_intf.get('type') != 'vlan_member':
+      continue
+
+    parent_lag_intf = lag_by_ifindex.get(vlan_intf.get('parent_ifindex'))
+
+    if parent_lag_intf is None:
+      continue
+
+    lag_mtu[parent_lag_intf.lag.ifindex] = (parent_lag_intf.get('mtu',1500) + 4)
+
+  # Raise each physical member's MTU, preserving larger values.
+  for lag_member in node.interfaces:
+    required_mtu = lag_mtu.get(lag_member.get('lag._parentindex'))
+
+    if required_mtu is None:
+      continue
+
+    lag_member.mtu = max(lag_member.get('mtu',1500),required_mtu,)
+
+
+
 class RouterOS7(_Quirks):
   @classmethod
   def device_quirks(self, node: Box, topology: Box) -> None:
@@ -50,3 +91,5 @@ class RouterOS7(_Quirks):
     _common.check_tagged_vlan_1(node)
     if node.get('bgp.neighbors'):
       build_afi_lists(node)
+    if 'lag' in node.get('module',[]) and 'vlan' in node.get('module',[]):
+      adjust_lag_vlan_mtu(node)
